83381abaeb77c773699576055235ade7a9feed08
Chris Hanna <cdhannaphone@gmail.com>
2021-09-17 10:23:34 -0400

    copying microservice base from p4v

---
+++ microservice/microservice/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace microservice.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpperRestToLower(this string input) =>
+            input switch
+            {
+                null => throw new ArgumentNullException(nameof(input)),
+                "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+                _ => input.ToLower().First().ToString().ToUpper() + input.Substring(1).ToLower()
+            };
+    }
+}
