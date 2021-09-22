30232dc8f7654b39296b6c8f7b33fecff749f012
Tomasz Kaczmarz <tomasz@beamable.com>
2021-09-22 20:09:10 +0200

    Remove unnecessary using

--- client/Packages/com.beamable/Common/Runtime/Promise.cs
+++ client/Packages/com.beamable/Common/Runtime/Promise.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using Beamable.Common.Runtime.Collections;
-using UnityEngine;
 
 #if !DISABLE_BEAMABLE_ASYNCMETHODBUILDER
 
