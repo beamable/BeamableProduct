04000fea76fd70f6ef5bdf35d8cd6f279c4a762b
Gleuq <arturmankowski22@gmail.com>
2021-09-29 20:57:24 +0200

    [BEAM-1668] C#MS - Create base class for service models (#60)

---
+++ client/Packages/com.beamable.server/Editor/UI/Model/Interfaces/IBeamableMicroservice.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Beamable.Server.Editor;
+using Beamable.Server.Editor.ManagerClient;
+
+namespace Beamable.Editor.UI.Model
+{
+    public interface IBeamableMicroservice : IBeamableService
+    {
+        MicroserviceDescriptor Descriptor { get; }
+        MicroserviceBuilder Builder { get; }
+        ServiceStatus RemoteStatus { get; }
+        MicroserviceConfigurationEntry Config { get; }
+
+        Task Build();
+        Task BuildAndStart();
+        Task BuildAndRestart();
+        void OpenLocalDocs();
+        void EnrichWithRemoteReference(ServiceReference remoteReference);
+        void EnrichWithStatus(ServiceStatus status);
+    }
+}
--- client/Packages/com.beamable.server/Editor/UI/Model/MicroservicesDataModel.cs
+++ client/Packages/com.beamable.server/Editor/UI/Model/MicroservicesDataModel.cs
@@ -50,8 +50,8 @@ namespace Beamable.Editor.UI.Model
       }
 
       public List<IBeamableService> AllServices = new List<IBeamableService>();
-      public List<MicroserviceModel> Services => AllServices.Where(service => service.GetServiceType() == ServiceType.MicroService).Select(service => service as MicroserviceModel).ToList();
-      public List<MongoStorageModel> Storages => AllServices.Where(service => service.GetServiceType() == ServiceType.StorageObject).Select(service => service as MongoStorageModel).ToList();
+      public List<MicroserviceModel> Services => AllServices.Where(service => service.GetServiceType == ServiceType.MicroService).Select(service => service as MicroserviceModel).ToList();
+      public List<MongoStorageModel> Storages => AllServices.Where(service => service.GetServiceType == ServiceType.StorageObject).Select(service => service as MongoStorageModel).ToList();
       public ServiceManifest ServerManifest = new ServiceManifest();
       public GetStatusResponse Status = new GetStatusResponse();
 
@@ -115,8 +115,8 @@ namespace Beamable.Editor.UI.Model
 
       public void AddLogMessage(IDescriptor descriptor, LogMessage message)
       {
-         AllServices.FirstOrDefault(r => r.GetDescriptor().Name.Equals(descriptor.Name))
-            ?.GetLogs().AddMessage(message);
+         AllServices.FirstOrDefault(r => r.GetDescriptor.Name.Equals(descriptor.Name))
+            ?.Logs.AddMessage(message);
       }
 
       public ServiceStatus GetStatus(MicroserviceDescriptor descriptor)
@@ -155,18 +155,18 @@ namespace Beamable.Editor.UI.Model
       public ServiceType GetModelServiceType(string name)
       {
          var service = AllServices
-            .FirstOrDefault(s => s.GetDescriptor().Name.Equals(name));
-         return service?.GetServiceType() ?? ServiceType.MicroService;
+            .FirstOrDefault(s => s.GetDescriptor.Name.Equals(name));
+         return service?.GetServiceType ?? ServiceType.MicroService;
       }
 
-      public bool ContainsModel(string serviceName) => AllServices?.Any(s => s.GetDescriptor().Name.Equals(serviceName)) ?? false;
+      public bool ContainsModel(string serviceName) => AllServices?.Any(s => s.GetDescriptor.Name.Equals(serviceName)) ?? false;
 
       public T GetModel<T>(IDescriptor descriptor) where T : IBeamableService =>
          GetModel<T>(descriptor.Name);
 
       public T GetModel<T>(string serviceName) where T : IBeamableService
       {
-         return (T)AllServices?.FirstOrDefault(s => s.GetDescriptor().Name.Equals(serviceName));
+         return (T)AllServices?.FirstOrDefault(s => s.GetDescriptor.Name.Equals(serviceName));
       }
 
       public MicroserviceModel GetMicroserviceModel(IDescriptor descriptor) => GetModel<MicroserviceModel>(descriptor);
