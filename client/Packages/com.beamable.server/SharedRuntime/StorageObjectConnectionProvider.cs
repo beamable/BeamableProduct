82924de4407412ac44ad500c796e71c907da05d8
tomekwwo <tomasz@beamable.com>
2021-09-29 17:25:47 +0200

    [BEAM-1403] Connection Provider Service (#39)
    * Get MongoDB client by storage and microservice types
    
    * Fixed connection provider class and injected it to microservice
    
    * Add an interface for the connection provider

---
+++ client/Packages/com.beamable.server/SharedRuntime/StorageObjectConnectionProvider.cs
@@ -0,0 +1,51 @@
+using System;
+using Beamable.Common;
+using MongoDB.Driver;
+
+namespace Beamable.Server
+{
+    public interface IStorageObjectConnectionProvider
+    {
+        MongoClient GetClient<TStorage>() where TStorage : MongoStorageObject;
+    }
+    
+    public class StorageObjectConnectionProvider : IStorageObjectConnectionProvider
+    {
+        private const string CONNSTR_VAR_NAME_FORMAT = "STORAGE_CONNSTR_{0}";
+        
+        public MongoClient GetClient<TStorage>() where TStorage : MongoStorageObject
+        {
+            string storageName = string.Empty;
+            var attributes = typeof(TStorage).GetCustomAttributes(true);
+            foreach (var attribute in attributes)
+            {
+                if (attribute is StorageObjectAttribute storageAttr)
+                {
+                    storageName = storageAttr.StorageName;
+                    break;
+                }
+            }
+            
+            if (string.IsNullOrEmpty(storageName))
+            {
+                BeamableLogger.LogError($"Cannot find storage name for type {typeof(TStorage)} ");
+                return null;
+            }
+            
+            return new MongoClient(GetConnectionString(storageName));
+        }
+
+        private string GetConnectionString(string storageName)
+        {
+            string connStringName = string.Format(CONNSTR_VAR_NAME_FORMAT, storageName);
+            string connectionString = Environment.GetEnvironmentVariable(connStringName);
+            
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                BeamableLogger.LogError($"Connection string to storage '{storageName}' is empty or not present in environment variables.");
+            }
+            
+            return connectionString;
+        }
+    }
+}
