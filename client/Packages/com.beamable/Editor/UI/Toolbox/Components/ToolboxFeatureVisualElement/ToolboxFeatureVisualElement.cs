d6bb8c2c50054ba7a92969bbfea92b970d26b54d
Piotr Siuszko <piotr@beamable.com>
2021-10-06 15:55:33 +0200

    Wrap description in any Unity version

--- client/Packages/com.beamable/Editor/UI/Toolbox/Components/ToolboxFeatureVisualElement/ToolboxFeatureVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Toolbox/Components/ToolboxFeatureVisualElement/ToolboxFeatureVisualElement.cs
@@ -57,9 +57,7 @@ namespace Beamable.Editor.Toolbox.Components
 
             Label despLabel = Root.Q<Label>("description");
             despLabel.text = WidgetModel.Description;
-#if UNITY_2018
             despLabel.AddTextWrapStyle();
-#endif
 
             Image functionImage = Root.Q<Image>("functionIcon");
             functionImage.image = WidgetModel.Icon;
