c017852a08bf3ef6630663847313359e5e6f4647
juna8001 <juna8001@gmail.com>
2021-10-07 16:36:48 +0200

    beam-1688 - archiving namespaces ui fixes (#94)

--- client/Packages/com.beamable/Editor/UI/Content/Components/ArchiveManifestsVisualElement/ArchiveManifestsVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Content/Components/ArchiveManifestsVisualElement/ArchiveManifestsVisualElement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Beamable.Editor.UI.Buss.Components;
+using Beamable.Editor.UI.Common;
 using Beamable.Editor.UI.Common.Models;
 using Beamable.Editor.UI.Components;
 using UnityEditor;
@@ -23,11 +24,12 @@ namespace Beamable.Editor.Content.Components
         private VisualElement _listRoot;
         private List<Entry> _entries = new List<Entry>();
         private PrimaryButtonVisualElement _archiveBtn;
+        private FormConstraint _buttonGatekeeper;
 
         private ArchiveManifestsVisualElement() : base(nameof(ArchiveManifestsVisualElement)) { }
 
         public static BeamablePopupWindow OpenAsUtilityWindow() => OpenAsUtilityWindow(null, out var _);
-
+        
         public static BeamablePopupWindow OpenAsUtilityWindow(EditorWindow parent, out ArchiveManifestsVisualElement content) {
             content = new ArchiveManifestsVisualElement();
             var window = BeamablePopupWindow.ShowUtility(ContentManagerConstants.ArchiveManifests, content, parent, ContentManagerConstants.WindowSizeMinimum);
@@ -55,17 +57,20 @@ namespace Beamable.Editor.Content.Components
                 }
             });
 
+            Root.Q<Label>("manifestWarningMessage").AddTextWrapStyle();
+            
             _archiveBtn = Root.Q<PrimaryButtonVisualElement>("archiveBtn");
             _archiveBtn.Button.clickable.clicked += ArchiveButton_OnClicked;
+            _buttonGatekeeper = FormConstraint.Logical("No namespace selected.", () => _entries.Count(e => e.IsSelected) == 0);
+            _archiveBtn.AddGateKeeper(_buttonGatekeeper);
             UpdateArchiveButtonInteractivity();
-
+            
             var cancelBtn = Root.Q<Button>("cancelBtn");
             cancelBtn.clickable.clicked += CancelButton_OnClicked;
         }
 
         private void UpdateArchiveButtonInteractivity() {
-            var enabled = _entries.Count(e => e.IsSelected) > 0;
-            _archiveBtn.SetEnabled(enabled);
+            _buttonGatekeeper.Check();
         }
 
         private void CancelButton_OnClicked()
