5bede033012de942d3bfe9d040fffd958f85fe92
Tomasz Kaczmarz <tomasz@beamable.com>
2021-10-13 12:22:53 +0200

    Added summaries to ConvertIfNotDoneAlready methods

--- client/Packages/com.beamable/Common/Runtime/Content/ContentEnums.cs
+++ client/Packages/com.beamable/Common/Runtime/Content/ContentEnums.cs
@@ -24,12 +24,30 @@ namespace Beamable.Common.Content
             return defaultValue;
         }
 
+        /// <summary>
+        /// Converts given <see cref="OptionalString"/> value to target enum value.
+        /// Conversion process sets the OptionalString to custom value to mark it as converted.
+        /// This way it is going to be converted only once after first call.
+        /// </summary>
+        /// <param name="conversionTarget">Target enum variable to convert to</param>
+        /// <param name="value">Value to be converted</param>
+        /// <param name="defaultValue">Value to be set if conversion fails</param>
+        /// <typeparam name="T">Target enum type</typeparam>
         public static void ConvertIfNotDoneAlready<T>(ref T conversionTarget, ref OptionalString value,
             T defaultValue = default) where T : Enum
         {
             ConvertIfNotDoneAlready(ref conversionTarget, ref value.Value, defaultValue);
         }
         
+        /// <summary>
+        /// Converts given <see cref="string"/> value to target enum value.
+        /// Conversion process sets the OptionalString to custom value to mark it as converted.
+        /// This way it is going to be converted only once after first call.
+        /// </summary>
+        /// <param name="conversionTarget">Target enum variable to convert to</param>
+        /// <param name="value">Value to be converted</param>
+        /// <param name="defaultValue">Value to be set if conversion fails</param>
+        /// <typeparam name="T">Target enum type</typeparam>
         public static void ConvertIfNotDoneAlready<T>(ref T conversionTarget, ref string value, T defaultValue = default) where T : Enum
         {
             if (value != VALUE_CONVERTED_MARKER)
