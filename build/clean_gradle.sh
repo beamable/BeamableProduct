1dd93d942470761d231dc441b4d32f907dd3c013
Chris Hanna <cdhannaphone@gmail.com>
2021-10-20 11:35:57 -0400

    use mv not rename

--- build/clean_gradle.sh
+++ build/clean_gradle.sh
@@ -5,8 +5,8 @@ echo "Unity=[${UNITY_VERSION}]. Checking to see which mainTemplate.gradle file w
 if [ "$UNITY_VERSION" == "2020.3.19f1" ]
 then
     echo "Using the 2020 gradle templates, and deleting the other ones..."
-    rename -f ./client/Assets/Plugins/Android/gradleTemplate.2020.gradle ./client/Assets/Plugins/Android/gradleTemplate.gradle
-    rename -f ./client/Assets/Plugins/Android/gradleTemplate.2020.properties ./client/Assets/Plugins/Android/gradleTemplate.properties
+    mv -f ./client/Assets/Plugins/Android/gradleTemplate.2020.gradle ./client/Assets/Plugins/Android/gradleTemplate.gradle
+    mv -f ./client/Assets/Plugins/Android/gradleTemplate.2020.properties ./client/Assets/Plugins/Android/gradleTemplate.properties
     rm -f ./client/Assets/Plugins/Android/gradleTemplate.2020.gradle.meta
     rm -f ./client/Assets/Plugins/Android/gradleTemplate.2020.properties.meta
 else
