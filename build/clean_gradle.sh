45081ea627f0fdca62ce7b07bb962ac68977600f
Chris Hanna <cdhannaphone@gmail.com>
2021-10-20 11:48:38 -0400

    fix gradle script

--- build/clean_gradle.sh
+++ build/clean_gradle.sh
@@ -5,10 +5,10 @@ echo "Unity=[${UNITY_VERSION}]. Checking to see which mainTemplate.gradle file w
 if [ "$UNITY_VERSION" == "2020.3.19f1" ]
 then
     echo "Using the 2020 gradle templates, and deleting the other ones..."
-    mv -f ./client/Assets/Plugins/Android/gradleTemplate.2020.gradle ./client/Assets/Plugins/Android/gradleTemplate.gradle
-    mv -f ./client/Assets/Plugins/Android/gradleTemplate.2020.properties ./client/Assets/Plugins/Android/gradleTemplate.properties
-    rm -f ./client/Assets/Plugins/Android/gradleTemplate.2020.gradle.meta
-    rm -f ./client/Assets/Plugins/Android/gradleTemplate.2020.properties.meta
+    mv -f ./client/Assets/Plugins/Android/mainTemplate.2020.gradle ./client/Assets/Plugins/Android/mainTemplate.gradle
+    mv -f ./client/Assets/Plugins/Android/mainTemplate.2020.properties ./client/Assets/Plugins/Android/mainTemplate.properties
+    rm -f ./client/Assets/Plugins/Android/mainTemplate.2020.gradle.meta
+    rm -f ./client/Assets/Plugins/Android/mainTemplate.2020.properties.meta
 else
     echo "Leaving gradle files alone"
 fi
