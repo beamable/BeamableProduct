cdb03336e4c60f444f9d88ca860c344040d835f1
Chris Hanna <cdhannaphone@gmail.com>
2021-10-21 10:56:22 -0400

    beam 1704 - device builds (#116)
    many commits
    # Conflicts:
    #	.github/workflows/main.yml
    #	client/Assets/TestingTool/Scripts/Editor/TestScenariosSettingsBuilder.cs

---
+++ build/clean_gradle.sh
@@ -0,0 +1,14 @@
+
+#!/bin/sh
+echo "Unity=[${UNITY_VERSION}]. Checking to see which mainTemplate.gradle file we should use based on Unity version."
+
+if [ "$UNITY_VERSION" == "2020.3.19f1" ]
+then
+    echo "Using the 2020 gradle templates, and deleting the other ones..."
+    mv -f ./client/Assets/Plugins/Android/mainTemplate.2020.gradle ./client/Assets/Plugins/Android/mainTemplate.gradle
+    mv -f ./client/Assets/Plugins/Android/gradleTemplate.2020.properties ./client/Assets/Plugins/Android/gradleTemplate.properties
+    rm -f ./client/Assets/Plugins/Android/mainTemplate.2020.gradle.meta
+    rm -f ./client/Assets/Plugins/Android/gradleTemplate.2020.properties.meta
+else
+    echo "Leaving gradle files alone"
+fi
