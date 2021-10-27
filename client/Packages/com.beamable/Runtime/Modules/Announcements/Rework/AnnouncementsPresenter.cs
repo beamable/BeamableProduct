7e59db8874b504c563cccf5e548a2a0c8db10eb1
Tomasz Kaczmarz <tomasz@beamable.com>
2021-10-27 21:06:13 +0200

    Added collection for announcements and created base for the new prefab

---
+++ client/Packages/com.beamable/Runtime/Modules/Announcements/Rework/AnnouncementsPresenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Modules.Inventory.Prototype;
+
+namespace Modules.Content
+{
+    public class AnnouncementsPresenter : CollectionPresenter<AnnouncementsCollection>
+    {
+        private void OnEnable()
+        {
+            Collection = new AnnouncementsCollection(OnCollectionUpdated);
+        }
+
+        private void OnDisable()
+        {
+            Collection?.Unsubscribe();
+            Collection = null;
+        }
+
+        private void OnCollectionUpdated()
+        {
+            
+        }
+    }
+}
