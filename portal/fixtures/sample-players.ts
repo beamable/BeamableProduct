ffcec26dc1f738fcbdef037b6742e9d8f71cd2d2
Chris Hanna <cdhannaphone@gmail.com>
2021-10-28 15:31:59 -0400

    fixes svelte tests

--- portal/fixtures/sample-players.ts
+++ portal/fixtures/sample-players.ts
@@ -4,6 +4,7 @@ export const playerWithFacebook: PlayerData = new PlayerData({
     email: 'test@beamable.com',
     createdTimeMillis: 400000,
     id: 1,
+    deviceId: 'abc',
     updatedTimeMillis: 400001,
     gamerTags: [{
         gamerTag: 123,
@@ -22,6 +23,7 @@ export const anonymousPlayer: PlayerData = new PlayerData({
     email: 'test@beamable.com',
     createdTimeMillis: 400000,
     id: 1,
+    deviceId: '',
     updatedTimeMillis: 400001,
     gamerTags: [{
         gamerTag: 123,
