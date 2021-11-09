f3dd5b9a1ddee94f8b3b3948a0f3c491991b8a26
Lukasz Wysk <lukaszw@beamable.com>
2021-11-09 22:33:58 +0100

    BEAM-1832

--- client/Packages/com.beamable/Runtime/UI/Materials/SDF/TextMeshBUSSElement.cs
+++ client/Packages/com.beamable/Runtime/UI/Materials/SDF/TextMeshBUSSElement.cs
@@ -19,9 +19,39 @@ namespace Beamable.UI.BUSS
 
             if (Style == null) return;
 
+            // BASE
+
             _text.font = BUSSStyle.Font.Get(Style).FontAsset;
             _text.fontSize = BUSSStyle.FontSize.Get(Style).FloatValue;
             _text.color = BUSSStyle.BackgroundColor.Get(Style).ColorRect.TopLeftColor;
+
+            // OUTLINE
+
+            float borderWidth = BUSSStyle.BorderWidth.Get(Style).FloatValue;
+
+            if (borderWidth > 0)
+                _text.fontMaterial.EnableKeyword(ShaderUtilities.Keyword_Outline);
+            else
+                _text.fontMaterial.DisableKeyword(ShaderUtilities.Keyword_Underlay);
+
+            _text.fontMaterial.SetFloat(ShaderUtilities.ID_OutlineWidth, borderWidth);
+            _text.fontMaterial.SetColor(ShaderUtilities.ID_OutlineColor, BUSSStyle.BorderColor.Get(Style).ColorRect.TopLeftColor);
+            _text.fontMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, BUSSStyle.ShadowColor.Get(Style).ColorRect.TopLeftColor);
+
+            // SHADOW
+
+            Vector2 shadowOffset = BUSSStyle.ShadowOffset.Get(Style).Vector2Value;
+
+            if (shadowOffset != Vector2.zero)
+                _text.fontMaterial.EnableKeyword(ShaderUtilities.Keyword_Underlay);
+            else
+                _text.fontMaterial.DisableKeyword(ShaderUtilities.Keyword_Underlay);
+
+            _text.fontMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetX, shadowOffset.x);
+            _text.fontMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetY, shadowOffset.y);
+            _text.fontMaterial.SetFloat(ShaderUtilities.ID_UnderlaySoftness, BUSSStyle.ShadowSoftness.Get(Style).FloatValue);
+            _text.fontMaterial.SetFloat(ShaderUtilities.ID_UnderlayDilate, BUSSStyle.ShadowThreshold.Get(Style).FloatValue);
+
         }
     }
 }
