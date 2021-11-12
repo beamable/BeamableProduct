6c35dca2fd72c81fc6a16af561f3d9c74b9e3e07
Lukasz Wysk <lukaszw@beamable.com>
2021-11-12 16:19:14 +0100

    BEAM-1612 unity 2018 fix

--- client/Packages/com.beamable/Editor/UI/Common/Components/GenericButtonVisualElement/GenericButtonVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Common/Components/GenericButtonVisualElement/GenericButtonVisualElement.cs
@@ -1,7 +1,14 @@
 using System;
 using System.Collections.Generic;
 using Beamable.Editor.UI.Buss;
+
+#if UNITY_2018
+using UnityEngine.Experimental.UIElements;
+using UnityEditor.Experimental.UIElements;
+#elif UNITY_2019_1_OR_NEWER
 using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+#endif
 
 namespace Beamable.Editor.UI.Components
 {
