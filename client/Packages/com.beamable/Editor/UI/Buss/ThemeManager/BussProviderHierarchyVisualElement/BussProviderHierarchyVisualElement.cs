3655bbb9904db5e3dae50a8eb47bf843acfcbb51
Łukasz Kundys <lukaszkundys@gmail.com>
2021-11-22 17:21:08 +0100

    Missing statements for 2018

--- client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/BussProviderHierarchyVisualElement/BussProviderHierarchyVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/BussProviderHierarchyVisualElement/BussProviderHierarchyVisualElement.cs
@@ -1,10 +1,16 @@
 ﻿using Beamable.Editor.UI.Buss;
 using UnityEngine.UI;
+#if UNITY_2018
+using UnityEngine.Experimental.UIElements;
+using UnityEditor.Experimental.UIElements;
+#elif UNITY_2019_1_OR_NEWER
 using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+#endif
 
 namespace Beamable.Editor.UI.Components
 {
-	// TODO: change generic type from Text to BussProvider before final merge
+	// TODO: change generic type from Text to BussElement before final merge
 	public class BussProviderHierarchyVisualElement : ComponentBasedHierarchyVisualElement<Text>	
 	{
 		public new class UxmlFactory : UxmlFactory<BussProviderHierarchyVisualElement, UxmlTraits>
