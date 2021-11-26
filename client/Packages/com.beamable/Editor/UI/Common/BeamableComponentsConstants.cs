cbde8425112ed8ac82f93cbde40f875572fc0750
Gleuq <arturmankowski22@gmail.com>
2021-11-26 16:58:18 +0100

    Update BeamableComponentsConstants.cs

--- client/Packages/com.beamable/Editor/UI/Common/BeamableComponentsConstants.cs
+++ client/Packages/com.beamable/Editor/UI/Common/BeamableComponentsConstants.cs
@@ -9,7 +9,7 @@ namespace Beamable.Editor.UI.Buss
       public const string COMP_PATH = UI_PACKAGE_PATH  +"/Common/Components";
       public const string COMMON_USS_PATH = UI_PACKAGE_PATH + "/Common/Common.uss";
       public const string BUSS_PACKAGE_PATH = UI_PACKAGE_PATH + "/Buss";
-      public const string BUSS_COMPONENTS_PATH = BUSS_PACKAGE_PATH + "/Buss";
+      public const string BUSS_COMPONENTS_PATH = BUSS_PACKAGE_PATH + "/Components";
       public const string BUSS_THEME_MANAGER_PATH = BUSS_PACKAGE_PATH + "/ThemeManager";
 
       // Schedules
