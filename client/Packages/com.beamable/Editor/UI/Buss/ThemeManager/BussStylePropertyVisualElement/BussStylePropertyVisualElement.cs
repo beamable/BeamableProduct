3d7099c3c61ca1a56998ead50f198ebdc04320e9
Łukasz Kundys <lukaszkundys@gmail.com>
2021-12-06 09:10:07 +0100

    BussStylePropertyVisualElement added

---
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/BussStylePropertyVisualElement/BussStylePropertyVisualElement.cs
@@ -0,0 +1,200 @@
+﻿using Beamable.Editor.UI.Buss;
+using Beamable.Editor.UI.Validation;
+using Beamable.UI.Buss;
+using Beamable.UI.Sdf;
+using Beamable.UI.Sdf.MaterialManagement;
+using Beamable.UI.Tweening;
+using System;
+using UnityEngine;
+#if UNITY_2018
+using UnityEngine.Experimental.UIElements;
+using UnityEditor.Experimental.UIElements;
+#elif UNITY_2019_1_OR_NEWER
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+#endif
+
+namespace Beamable.Editor.UI.Components
+{
+    public class BussStylePropertyVisualElement : ValidableVisualElement<string>
+    {
+	    public new class UxmlFactory : UxmlFactory<BussStylePropertyVisualElement, UxmlTraits>
+        {
+        }
+
+	    public BussStylePropertyVisualElement() : base(
+            $"{BeamableComponentsConstants.BUSS_THEME_MANAGER_PATH}/{nameof(BussStylePropertyVisualElement)}/{nameof(BussStylePropertyVisualElement)}")
+        {
+        }
+
+	    private BussPropertyProvider _property;
+	    private VisualElement _valueParent;
+
+	    public override void Refresh()
+        {
+            base.Refresh();
+
+            _valueParent = Root.Q<VisualElement>("value");
+
+            Label labelComponent = Root.Q<Label>("label");
+            labelComponent.text = _property.Key;
+
+            SetupEditableField(_property.GetProperty());
+        }
+
+	    public void Setup(BussPropertyProvider property)
+        {
+            _property = property;
+            Refresh();
+        }
+
+	    private void SetupEditableField(IBussProperty propertyType)
+	    {
+		    if (propertyType is SingleColorBussProperty singleColorBussProperty)
+		    {
+			    ColorField field = new ColorField();
+			    _valueParent.Add(field);
+		    }
+
+		    if (propertyType is VertexColorBussProperty vertexColorBussProperty)
+		    {
+			    // TODO: complex
+		    }
+
+		    if (propertyType is FloatBussProperty floatBussProperty)
+		    {
+			    FloatField field = new FloatField();
+			    field.value = floatBussProperty.FloatValue;
+			    _valueParent.Add(field);
+		    }
+
+		    if (propertyType is FractionFloatBussProperty fractionFloatBussProperty)
+		    {
+			    Vector2Field field = new Vector2Field();
+			    field.value = new Vector2(fractionFloatBussProperty.Fraction, fractionFloatBussProperty.Offset);
+			    _valueParent.Add(field);
+		    }
+
+		    if (propertyType is FontBussAssetProperty fontBussProperty)
+		    {
+			    ObjectField field = new ObjectField();
+			    field.allowSceneObjects = false;
+			    field.objectType = typeof(Font);
+			    field.value = fontBussProperty.FontAsset;
+			    _valueParent.Add(field);
+		    }
+
+		    if (propertyType is SpriteBussProperty spriteBussProperty)
+		    {
+			    ObjectField field = new ObjectField();
+			    field.allowSceneObjects = false;
+			    field.objectType = typeof(Sprite);
+			    field.value = spriteBussProperty.SpriteValue;
+			    _valueParent.Add(field);
+		    }
+
+		    if (propertyType is SdfModeBussProperty sdfModeBussProperty)
+		    {
+			    ToolbarMenu menu = new ToolbarMenu();
+			    menu.AddToClassList("dropdown");
+
+			    Array values = Enum.GetValues(typeof(SdfImage.SdfMode));
+
+			    foreach (var value in values)
+			    {
+				    menu.menu.AppendAction(value.ToString(), action => {
+					    menu.text = action.name;
+				    });
+			    }
+			    _valueParent.Add(menu);
+		    }
+
+		    if (propertyType is ImageTypeBussProperty imageTypeBussProperty)
+		    {
+			    ToolbarMenu menu = new ToolbarMenu();
+			    menu.AddToClassList("dropdown");
+
+			    Array values = Enum.GetValues(typeof(SdfImage.ImageType));
+
+			    foreach (var value in values)
+			    {
+				    menu.menu.AppendAction(value.ToString(), action => {
+					    menu.text = action.name;
+				    });
+			    }
+			    _valueParent.Add(menu);
+		    }
+
+		    if (propertyType is BackgroundModeBussProperty backgroundModeBussProperty)
+		    {
+			    ToolbarMenu menu = new ToolbarMenu();
+			    menu.AddToClassList("dropdown");
+
+			    Array values = Enum.GetValues(typeof(SdfBackgroundMode));
+
+			    foreach (var value in values)
+			    {
+				    menu.menu.AppendAction(value.ToString(), action => {
+					    menu.text = action.name;
+				    });
+			    }
+			    _valueParent.Add(menu);
+		    }
+		    
+		    if (propertyType is BorderModeBussProperty borderModeBussProperty)
+		    {
+			    ToolbarMenu menu = new ToolbarMenu();
+			    menu.AddToClassList("dropdown");
+
+			    Array values = Enum.GetValues(typeof(SdfImage.BorderMode));
+
+			    foreach (var value in values)
+			    {
+				    menu.menu.AppendAction(value.ToString(), action => {
+					    menu.text = action.name;
+				    });
+			    }
+			    _valueParent.Add(menu);
+		    }
+		    
+		    if (propertyType is ShadowModeBussProperty shadowModeBussProperty)
+		    {
+			    ToolbarMenu menu = new ToolbarMenu();
+			    menu.AddToClassList("dropdown");
+
+			    Array values = Enum.GetValues(typeof(SdfShadowMode));
+
+			    foreach (var value in values)
+			    {
+				    menu.menu.AppendAction(value.ToString(), action => {
+					    menu.text = action.name;
+				    });
+			    }
+			    _valueParent.Add(menu);
+		    }
+		    
+		    if (propertyType is EasingBussProperty easingBussProperty)
+		    {
+			    ToolbarMenu menu = new ToolbarMenu();
+			    menu.AddToClassList("dropdown");
+
+			    Array values = Enum.GetValues(typeof(Easing));
+
+			    foreach (var value in values)
+			    {
+				    menu.menu.AppendAction(value.ToString(), action => {
+					    menu.text = action.name;
+				    });
+			    }
+			    _valueParent.Add(menu);
+		    }
+
+		    if (propertyType is Vector2BussProperty vectorBussProperty)
+		    {
+			    Vector2Field field = new Vector2Field();
+			    field.value = vectorBussProperty.Vector2Value;
+			    _valueParent.Add(field);
+		    }
+	    }
+    }
+}
