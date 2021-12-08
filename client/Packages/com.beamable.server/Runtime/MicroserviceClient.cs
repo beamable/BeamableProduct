3303822ebef58f9fe76f63d8b053c81759b3e837
MevLyshkin <piotr@beamable.com>
2021-12-08 14:16:36 +0100

    [BEAM-1803] Add support for Dictionary serialization in ClientCallable methods using SmallerJSON (#255)
    * [BEAM-1803] Support for dictionary serialization
    
    * Revert "[BEAM-1803] Support for dictionary serialization"
    
    This reverts commit f0499f6ff1dff5a617c6fb0da7e6b9dd8dfba8c6.
    
    * [BEAM-1803] Support for dictionary serialization
    
    * Format file with new settings
    
    * Format MicroserviceClient with new settings
    
    * Add tests.

--- client/Packages/com.beamable.server/Runtime/MicroserviceClient.cs
+++ client/Packages/com.beamable.server/Runtime/MicroserviceClient.cs
@@ -1,180 +1,245 @@
-
+using Beamable.Common;
+using Beamable.Common.Api;
+using Beamable.Serialization.SmallerJSON;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Beamable.Platform.SDK;
-using Beamable;
-using Beamable.Common;
-using Beamable.Common.Api;
+using System.Text;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
-using Beamable.Serialization.SmallerJSON;
 
 namespace Beamable.Server
 {
-   [Serializable]
-   public class MicroserviceClientDataWrapper<T> : ScriptableObject
-   {
-      public T Data;
-   }
+	[Serializable]
+	public class MicroserviceClientDataWrapper<T> : ScriptableObject
+	{
+		public T Data;
+	}
 
-   public class MicroserviceClient
-   {
-      protected async Promise<T> Request<T>(string serviceName, string endpoint, string[] serializedFields)
-      {
-         return await MicroserviceClientHelper.Request<T>(serviceName, endpoint, serializedFields);
-      }
+	public class MicroserviceClient
+	{
+		protected async Promise<T> Request<T>(string serviceName, string endpoint, string[] serializedFields)
+		{
+			return await MicroserviceClientHelper.Request<T>(serviceName, endpoint, serializedFields);
+		}
 
-      protected string SerializeArgument<T>(T arg) => MicroserviceClientHelper.SerializeArgument(arg);
+		protected string SerializeArgument<T>(T arg) => MicroserviceClientHelper.SerializeArgument(arg);
 
-      protected string CreateUrl(string cid, string pid, string serviceName, string endpoint)
-         => MicroserviceClientHelper.CreateUrl(cid, pid, serviceName, endpoint);
-   }
+		protected string CreateUrl(string cid, string pid, string serviceName, string endpoint) =>
+			MicroserviceClientHelper.CreateUrl(cid, pid, serviceName, endpoint);
+	}
 
+	public static class MicroserviceClientHelper
+	{
+		[System.Serializable]
+		public class ResponseObject
+		{
+			public string payload;
+		}
 
-   public static class MicroserviceClientHelper
-   {
-      [System.Serializable]
-      public class ResponseObject
-      {
-         public string payload;
-      }
+		[System.Serializable]
+		public class RequestObject
+		{
+			public string payload;
+		}
 
-      [System.Serializable]
-      public class RequestObject
-      {
-         public string payload;
-      }
+		static readonly StringBuilder _builder = new StringBuilder();
+		static string _prefix;
 
-      static string _prefix;
+		public static void SetPrefix(string prefix) => _prefix = prefix;
 
-      public static void SetPrefix(string prefix) => _prefix = prefix;
+		public static string SerializeArgument(object arg)
+		{
+			// JSONUtility will serialize objects correctly, but doesn't handle primitives well.
+			if (arg == null)
+			{
+				return "null";
+			}
 
-      public static string SerializeArgument(object arg)
-      {
-         // JSONUtility will serialize objects correctly, but doesn't handle primitives well.
-         if (arg == null)
-         {
-            return "null";
-         }
+			switch (arg)
+			{
+				case string prim:
+					return Json.IsValidJson(prim) ? "[" + prim + "]" : "\"" + prim + "\"";
+				case IDictionary dictionary:
+					return Json.Serialize(dictionary, _builder);
+				case IEnumerable enumerable:
+				{
+					var output = new List<string>();
+					foreach (var elem in enumerable)
+					{
+						output.Add(SerializeArgument(elem));
+					}
 
-         switch (arg)
-         {
-            case IEnumerable enumerable when !(enumerable is string):
-               var output = new List<string>();
-               foreach (var elem in enumerable)
-               {
-                  output.Add(SerializeArgument(elem));
-               }
+					var outputJson = "[" + string.Join(",", output) + "]";
+					return outputJson;
+				}
+				case bool prim:
+					return prim ? "true" : "false";
+				case long prim:
+					return prim.ToString();
+				case double prim:
+					return prim.ToString();
+				case float prim:
+					return prim.ToString();
+				case int prim:
+					return prim.ToString();
+				case Vector2Int prim:
+					return JsonUtility.ToJson(new Vector2IntEx(prim));
+				case Vector3Int prim:
+					return JsonUtility.ToJson(new Vector3IntEx(prim));
+			}
 
-               var outputJson = "[" + string.Join(",", output) + "]";
-               return outputJson;
+			return JsonUtility.ToJson(arg);
+		}
 
-            case bool prim:
-               return prim ? "true": "false";
-            case long prim:
-               return prim.ToString();
-            case string prim:
-               return Json.IsValidJson(prim) ? "[" + prim + "]" : "\"" + prim + "\"";
-            case double prim:
-               return prim.ToString();
-            case float prim:
-               return prim.ToString();
-            case int prim:
-               return prim.ToString();
-            case Vector2Int prim:
-               return JsonUtility.ToJson(new Vector2IntEx(prim));
-            case Vector3Int prim:
-               return JsonUtility.ToJson(new Vector3IntEx(prim));
-            }
-         return JsonUtility.ToJson(arg);
-      }
+		public static T DeserializeResult<T>(string json)
+		{
+			var type = typeof(T);
+			var defaultInstance = default(T);
 
-     public static T DeserializeResult<T>(string json)
-      {
-         var defaultInstance = default(T);
+			if (typeof(Unit).IsAssignableFrom(type))
+			{
+				return (T)(object)PromiseBase.Unit;
+			}
 
-         if (typeof(Unit).IsAssignableFrom(typeof(T)))
-         {
-            return (T)(object) PromiseBase.Unit;
-         }
+			if (type == typeof(string))
+			{
+				return (T)(object)json;
+			}
 
-         if (typeof(T).Equals(typeof(string)))
-         {
-            return (T)(object) json;
-         }
-         switch (defaultInstance)
-         {
-            case float _:
-               return (T) (object) float.Parse(json);
-            case long _:
-               return (T) (object) long.Parse(json);
-            case double _:
-               return (T) (object) double.Parse(json);
-            case bool _:
-               return (T) (object) bool.Parse(json);
-            case int _:
-               return (T) (object) int.Parse(json);
-            case Vector2Int _:
-               return (T)(object)Vector2IntEx.DeserializeToVector2(json);
-            case Vector3Int _:
-               return (T)(object)Vector3IntEx.DeserializeToVector3(json);
-            }
+			switch (defaultInstance)
+			{
+				case float _:
+					return (T)(object)float.Parse(json);
+				case long _:
+					return (T)(object)long.Parse(json);
+				case double _:
+					return (T)(object)double.Parse(json);
+				case bool _:
+					return (T)(object)bool.Parse(json);
+				case int _:
+					return (T)(object)int.Parse(json);
+				case Vector2Int _:
+					return (T)(object)Vector2IntEx.DeserializeToVector2(json);
+				case Vector3Int _:
+					return (T)(object)Vector3IntEx.DeserializeToVector3(json);
+			}
 
-         if (json.StartsWith("[") && json.EndsWith("]"))
-         {
-            json = $"{{\"items\": {json}}}";
-            var wrapped = JsonUtility.FromJson<JsonUtilityWrappedList<T>>(json);
-            return wrapped.items;
-         }
+			if (typeof(IDictionary).IsAssignableFrom(type))
+			{
+				var arrayDict = (ArrayDict)Json.Deserialize(json);
+				object result = default(T);
+				if (typeof(Dictionary<string, string>) == type)
+				{
+					result = ConvertArrayDictToDictionary<string>(arrayDict);
+				}
+				else if (typeof(Dictionary<string, double>) == type)
+				{
+					result = ConvertArrayDictToDictionary<double>(arrayDict);
+				}
+				else if (typeof(Dictionary<string, float>) == type)
+				{
+					result = ConvertArrayDictToDictionary<float>(arrayDict);
+				}
+				else if (typeof(Dictionary<string, int>) == type)
+				{
+					result = ConvertArrayDictToDictionary<int>(arrayDict);
+				}
+				else if (typeof(Dictionary<string, bool>) == type)
+				{
+					result = ConvertArrayDictToDictionary<bool>(arrayDict);
+				}
+				else if (typeof(Dictionary<string, long>) == type)
+				{
+					result = ConvertArrayDictToDictionary<long>(arrayDict);
+				}
+				else
+				{
+					Debug.LogWarning($"Cannot convert json to Dictionary:\n{json}");
+				}
 
-         return JsonUtility.FromJson<T>(json);
-      }
+				return (T)result;
+			}
 
-      private class JsonUtilityWrappedList<TList>
-      {
-         public TList items = default;
-      }
+			if (json.StartsWith("[") && json.EndsWith("]"))
+			{
+				json = $"{{\"items\": {json}}}";
+				var wrapped = JsonUtility.FromJson<JsonUtilityWrappedList<T>>(json);
+				return wrapped.items;
+			}
 
-      public static string CreateUrl(string cid, string pid, string serviceName, string endpoint)
-      {
-         var prefix = _prefix ?? (_prefix = MicroserviceIndividualization.GetServicePrefix(serviceName));
-         var path = $"{prefix}micro_{serviceName}/{endpoint}";
-         var url = $"/basic/{cid}.{pid}.{path}";
-         return url;
-      }
+			return JsonUtility.FromJson<T>(json);
+		}
 
-      public static async Promise<T> Request<T>(string serviceName, string endpoint, string[] serializedFields)
-      {
-         Debug.Log($"Client called {endpoint} with {serializedFields.Length} arguments");
-         var argArray = "[ " + string.Join(",", serializedFields) + " ]";
-         Debug.Log(argArray);
+		public static Dictionary<string, T> ConvertArrayDictToDictionary<T>(ArrayDict arrayDict)
+		{
+			var dictionary = new Dictionary<string, T>(arrayDict.Count);
+			if (typeof(T) == typeof(int))
+			{
+				foreach (var pair in arrayDict)
+				{
+					var intValue = (int)((long)pair.Value % Int32.MaxValue);
+					dictionary.Add(pair.Key, (T)(object)intValue);
+				}
+			}
+			else if (typeof(T) == typeof(float))
+			{
+				foreach (var pair in arrayDict)
+				{
+					var floatValue = (float)((double)pair.Value);
+					dictionary.Add(pair.Key, (T)(object)floatValue);
+				}
+			}
+			else
+			{
+				foreach (var pair in arrayDict)
+				{
+					dictionary.Add(pair.Key, (T)pair.Value);
+				}
+			}
 
-         T Parser(string json)
-         {
-            // TODO: Remove this in 0.13.0
-            if (!(json?.StartsWith("{\"payload\":\"") ?? false)) return DeserializeResult<T>(json);
+			return dictionary;
+		}
+
+		private class JsonUtilityWrappedList<TList>
+		{
+			public TList items = default;
+		}
+
+		public static string CreateUrl(string cid, string pid, string serviceName, string endpoint)
+		{
+			var prefix = _prefix ?? (_prefix = MicroserviceIndividualization.GetServicePrefix(serviceName));
+			var path = $"{prefix}micro_{serviceName}/{endpoint}";
+			var url = $"/basic/{cid}.{pid}.{path}";
+			return url;
+		}
+
+		public static async Promise<T> Request<T>(string serviceName, string endpoint, string[] serializedFields)
+		{
+			Debug.Log($"Client called {endpoint} with {serializedFields.Length} arguments");
+			var argArray = "[ " + string.Join(",", serializedFields) + " ]";
+			Debug.Log(argArray);
+
+			T Parser(string json)
+			{
+				// TODO: Remove this in 0.13.0
+				if (!(json?.StartsWith("{\"payload\":\"") ?? false))
+					return DeserializeResult<T>(json);
 
 #pragma warning disable 618
-            Debug.LogWarning($"Using legacy payload string. Redeploy the {serviceName} service without the UseLegacySerialization setting.");
+				Debug.LogWarning(
+					$"Using legacy payload string. Redeploy the {serviceName} service without the UseLegacySerialization setting.");
 #pragma warning restore 618
-            var responseObject = DeserializeResult<ResponseObject>(json);
-            var result = DeserializeResult<T>(responseObject.payload);
-            return result;
-         }
+				var responseObject = DeserializeResult<ResponseObject>(json);
+				var result = DeserializeResult<T>(responseObject.payload);
+				return result;
+			}
 
-         var b = await API.Instance;
-         var requester = b.Requester;
-         var url = CreateUrl(b.Token.Cid, b.Token.Pid, serviceName, endpoint);
-         var req = new RequestObject
-         {
-            payload = argArray
-         };
-         Debug.Log($"Sending Request uri=[{url}]");
-         return await requester.Request<T>(Method.POST, url, req, parser: Parser);
-      }
-
-
-   }
-
+			var b = await API.Instance;
+			var requester = b.Requester;
+			var url = CreateUrl(b.Token.Cid, b.Token.Pid, serviceName, endpoint);
+			var req = new RequestObject { payload = argArray };
+			Debug.Log($"Sending Request uri=[{url}]");
+			return await requester.Request<T>(Method.POST, url, req, parser: Parser);
+		}
+	}
 }
