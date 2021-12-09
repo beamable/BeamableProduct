c79672c6509d25aa25b8c3918b5541257950d4f4
Brandon Bateman <brandon@beamable.com>
2021-12-09 14:37:34 -0500

    Adding new build method to build xcodeproj with the correct settings

---
+++ client/Assets/Editor/Scripts/BuildConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using UnityEditor;
+
+public static class BuildSampleProject
+{
+   public readonly static string teamId = "A6C4565DLF";
+   private static string[] GetActiveScenes()
+   {
+      var scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(scene => scene.path).ToArray();
+      return scenes;
+   }
+
+   private static void BuildActiveTarget()
+   {
+      BuildPipeline.BuildPlayer(GetActiveScenes(), "dist/iOS/", BuildTarget.iOS, BuildOptions.None);
+   }
+   [MenuItem("Beamable/SampleBuild/Development")]
+   public static void Development()
+   {
+      PlayerSettings.iOS.appleDeveloperTeamID = teamId;
+      PlayerSettings.applicationIdentifier = "com.beamable.dev";
+      BuildActiveTarget();
+   }
+   [MenuItem("Beamable/SampleBuild/Staging")]
+   public static void Staging()
+   {
+      PlayerSettings.iOS.appleDeveloperTeamID = teamId;
+      PlayerSettings.applicationIdentifier = "com.beamable.staging";
+      BuildActiveTarget();
+   }
+   [MenuItem("Beamable/SampleBuild/Production")]
+   public static void Production()
+   {
+      PlayerSettings.iOS.appleDeveloperTeamID = teamId;
+      PlayerSettings.applicationIdentifier = "com.beamable.production";
+      BuildActiveTarget();
+   }
+}
