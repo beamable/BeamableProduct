6f974365e5be106e35b48d554d05211487a03331
Brandon Bateman <brandon@beamable.com>
2021-12-15 13:33:04 -0500

    Trying another method for setting the build number

--- client/Assets/Editor/Scripts/BuildConfiguration.cs
+++ client/Assets/Editor/Scripts/BuildConfiguration.cs
@@ -43,7 +43,7 @@ public class BuildSampleProject
    {
       PlayerSettings.iOS.appleDeveloperTeamID = teamId;
       PlayerSettings.applicationIdentifier = "com.beamable.dev";
-      PlayerSettings.iOS.buildNumber = "2000000";
+      PlayerSettings.iOS.buildNumber = Environment.GetEnvironmentVariable("ANDROID_VERSION_CODE") ?? "2";
       BuildActiveTarget();
    }
    [MenuItem("Beamable/SampleBuild/Staging")]
