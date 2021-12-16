dba5b92420952bfbf58a331d9245521128b15fa1
juna8001 <juna8001@gmail.com>
2021-12-16 14:20:21 +0100

    buss property visual elements

---
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/BussPropertyVisualElements/FloatBussPropertyVisualElement/FloatBussPropertyVisualElement.cs
@@ -0,0 +1,28 @@
+﻿using Beamable.UI.Buss;
+using Editor.UI.BUSS.ThemeManager.BussPropertyVisualElements;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Beamable.Editor.UI.Components
+{
+	public class FloatBussPropertyVisualElement : BussPropertyVisualElement
+	{
+		public FloatBussPropertyVisualElement(BussPropertyProvider propertyProvider) : base(propertyProvider) { }
+
+		public override void Refresh()
+		{
+			base.Refresh();
+			
+			FloatField field = new FloatField();
+			field.value = PropertyProvider.GetProperty<FloatBussProperty>().FloatValue;
+			_mainElement.Add(field);
+
+			field.RegisterValueChangedCallback(OnValueChange);
+		}
+
+		private void OnValueChange(ChangeEvent<float> evt)
+		{
+			PropertyProvider.GetProperty<FloatBussProperty>().FloatValue = evt.newValue;
+		}
+	}
+}
