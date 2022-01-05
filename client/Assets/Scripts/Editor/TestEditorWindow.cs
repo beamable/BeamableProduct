9a9d37b6ecab2bafcd6ae72820f69040c064a818
Piotr Siuszko <piotr@beamable.com>
2022-01-05 20:32:47 +0100

    Add TestEditorWindow class
    Thanks to existence of such class the issue in the future will be detected right away

---
+++ client/Assets/Scripts/Editor/TestEditorWindow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+/// <summary>
+/// <para>This is class created only for one purpose- make sure that issues like below are handled:</para>
+/// <c>error CS0118: 'Editor' is a namespace but is used like a type</c>
+/// </summary>
+public class TestEditorWindow : Editor
+{
+	
+}
