7c1a15e374c0c9d199cf1c7c5dc48ed91af2c47b
Piotr Siuszko <piotr@beamable.com>
2022-01-07 15:54:49 +0100

    [BEAM-2004] Make timeout for account management signals bigger

--- client/Packages/com.beamable.server/Editor/UI/Components/PublishPopup/PublishWindow.cs
+++ client/Packages/com.beamable.server/Editor/UI/Components/PublishPopup/PublishWindow.cs
@@ -4,10 +4,10 @@ using Beamable.Server.Editor.UI;
 using Beamable.Server.Editor.UI.Components;
 using System.Threading;
 using UnityEngine;
+using UnityEditor;
 #if UNITY_2018
 using UnityEngine.Experimental.UIElements;
 using UnityEditor.Experimental.UIElements;
-
 #elif UNITY_2019_1_OR_NEWER
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
