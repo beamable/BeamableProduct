c54b6643059da002ce5ef4293c174781dc9d055f
Chris Hanna <cdhannaphone@gmail.com>
2022-01-10 09:04:00 -0500

    beam 2015 - connect storage objects to remotes (#390)
    * playing with getting a microservice storage to deploy
    
    * adds remote connection provider
    
    * comments and minor changes
    
    * func name
    
    * changelog
    
    * puts some code back togehter
    
    * removes storage
    
    * removes popup warning
    
    * comment fixes

--- client/Packages/com.beamable.server/Editor/Microservices.cs
+++ client/Packages/com.beamable.server/Editor/Microservices.cs
@@ -252,7 +252,7 @@ namespace Beamable.Server.Editor
 
                var allStorages = new HashSet<string>();
 
-               foreach (var serverSideStorage in manifest.storages.Select(s => s.storageName))
+               foreach (var serverSideStorage in manifest.storages.Select(s => s.id))
                {
                    allStorages.Add(serverSideStorage);
                }
@@ -314,11 +314,20 @@ namespace Beamable.Server.Editor
          return env;
       }
 
-      public static Promise<string> GetConnectionString(StorageObjectDescriptor storage, MicroserviceDescriptor user)
+      public static async Promise<string> GetConnectionString(StorageObjectDescriptor storage, MicroserviceDescriptor user)
       {
-         // TODO: Check if the container is actually running. If it isn't, we need to get a connection string to the remote database.
-         var config = MicroserviceConfiguration.Instance.GetStorageEntry(storage.Name);
-         return Promise<string>.Successful($"mongodb://{config.LocalInitUser}:{config.LocalInitPass}@gateway.docker.internal:{config.LocalDataPort}");
+	      var storageCheck = new CheckImageReturnableCommand(storage);
+	      var isStorageRunning = await storageCheck.Start(null);
+	      if (isStorageRunning)
+	      {
+		      var config = MicroserviceConfiguration.Instance.GetStorageEntry(storage.Name);
+		      return
+			      $"mongodb://{config.LocalInitUser}:{config.LocalInitPass}@gateway.docker.internal:{config.LocalDataPort}";
+	      }
+	      else
+	      {
+		      return "";
+	      }
       }
 
       public static async Promise<bool> OpenLocalMongoTool(StorageObjectDescriptor storage)
@@ -504,19 +513,19 @@ namespace Beamable.Server.Editor
             return BitConverter.ToString(checksum).Replace("-", String.Empty);
          }
       }
-      
+
       public static event Action<ManifestModel, int> OnBeforeDeploy;
       public static event Action<ManifestModel, int> OnDeploySuccess;
       public static event Action<ManifestModel, string> OnDeployFailed;
-     
+
       public static async System.Threading.Tasks.Task Deploy(ManifestModel model, CommandRunnerWindow context, CancellationToken token, Action<IDescriptor> onServiceDeployed = null)
       {
          if (Descriptors.Count == 0) return; // don't do anything if there are no descriptors.
-         
+
          var descriptorsCount = Descriptors.Count;
 
          OnBeforeDeploy?.Invoke(model, descriptorsCount);
-         
+
          OnDeploySuccess -= HandleDeploySuccess;
          OnDeploySuccess += HandleDeploySuccess;
          OnDeployFailed -= HandleDeployFailed;
@@ -581,7 +590,7 @@ namespace Beamable.Server.Editor
                 serviceDependencies.Add(new ServiceDependency
                 {
                     id = storage.Name,
-                    type = "storage"
+                    storageType = "storage"
                 });
             }
             entryModel.Dependencies = serviceDependencies;
@@ -623,7 +632,7 @@ namespace Beamable.Server.Editor
          {
              return new ServiceStorageReference
              {
-                 storageName = kvp.Value.Name,
+                 id = kvp.Value.Name,
                  storageType = kvp.Value.Type,
                  templateId = kvp.Value.TemplateId,
                  enabled = kvp.Value.Enabled,
