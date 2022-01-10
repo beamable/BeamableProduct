31a75460594f3db6e42428dccca8281d1438dd39
Chris Hanna <cdhannaphone@gmail.com>
2022-01-10 12:54:50 -0500

    allow references, because they will be mocked

--- client/Packages/com.beamable.server/SharedRuntime/StorageDocument.cs
+++ client/Packages/com.beamable.server/SharedRuntime/StorageDocument.cs
@@ -1,8 +1,6 @@
 
 using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Bson.Serialization.Conventions;
 using System;
 using UnityEngine;
 
