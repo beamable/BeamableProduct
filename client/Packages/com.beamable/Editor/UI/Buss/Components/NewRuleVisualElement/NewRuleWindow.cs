ef7fb76ef433687c92d6c1620d6b2b46101e2983
Gleuq <arturmankowski22@gmail.com>
2022-01-14 16:33:26 +0100

    New rule window

---
+++ client/Packages/com.beamable/Editor/UI/Buss/Components/NewRuleVisualElement/NewRuleWindow.cs
@@ -0,0 +1,20 @@
+﻿using Beamable.Editor.Toolbox.Components;
+using Beamable.UI.Buss;
+using UnityEngine;
+
+namespace Beamable.Editor.UI.Buss.Components
+{
+	public class NewRuleWindow : BUSSWindowBase<NewRuleWindow, NewRuleVisualElement>
+	{
+		public void Init()
+		{
+			titleContent = new GUIContent("New Rule Window");
+			minSize = new Vector2(720, 400);
+			maxSize = minSize;
+			position = new Rect((Screen.width + minSize.x) * 0.5f, Screen.width * 0.5f, minSize.x, minSize.y);
+			
+			Refresh();
+		}
+		protected override NewRuleVisualElement GetVisualElement() => new NewRuleVisualElement();
+	}
+}
---
+++ client/Packages/com.beamable/Editor/UI/Buss/Components/RuleVisualElement/RuleVisualElement.cs
@@ -0,0 +1,38 @@
+﻿using Beamable.Editor.UI.Buss;
+using Beamable.Editor.UI.Buss.Components;
+using Beamable.Editor.UI.Components;
+using Beamable.UI.Buss;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Editor.UI.BUSS.ThemeManager.BussPropertyVisualElements;
+#if UNITY_2018
+using UnityEngine.Experimental.UIElements;
+
+#elif UNITY_2019_1_OR_NEWER
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+#endif
+namespace Beamable.Editor.Toolbox.Components
+{
+	public class RuleVisualElement : BeamableVisualElement
+	{
+		public RuleVisualElement() : base(
+			$"{BeamableComponentsConstants.BUSS_COMPONENTS_PATH}/{nameof(RuleVisualElement)}/{nameof(RuleVisualElement)}")
+		{
+		}
+		
+		public new class UxmlFactory : UxmlFactory<RuleVisualElement, UxmlTraits>
+		{
+		}
+
+		public override void Refresh()
+		{
+			base.Refresh();
+			
+			var lel = Root.Q<DropdownVisualElement>("propertyDropdown");
+			lel.Setup(BussStyle.Keys.ToList(), i => Debug.LogWarning(""));
+			lel.Refresh();
+		}
+	}
+}
