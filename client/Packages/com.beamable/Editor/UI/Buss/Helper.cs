05e79a296351b7f06934edf5e484d576db6cc43d
Gleuq <arturmankowski22@gmail.com>
2022-01-17 17:06:02 +0100

    Working add selector window

--- client/Packages/com.beamable/Editor/UI/Buss/Helper.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/Helper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 
 namespace Editor.UI.Buss
 {
@@ -13,5 +14,29 @@ namespace Editor.UI.Buss
 
 		public static List<string> GetAllClassesNamesInheritedFrom(Type baseClass) =>
 			GetAllClassesInheritedFrom(baseClass).Select(x => x.Name).ToList();
+
+		/// <summary>
+		/// Working same as AssetDatabase.FindAssets but returns collection of loaded assets instead of GUIDs.
+		/// </summary>
+		/// <param name="filter">The filter string can contain search data. See below for details about this string.</param>
+		/// <param name="searchInFolders">The folders where the search will start.</param>
+		/// <typeparam name="T">Asset type</typeparam>
+		/// <returns>Collection of loaded assets</returns>
+		/// <footer><a href="file:///C:/Program%20Files/Unity/Hub/Editor/2018.4.36f1/Editor/Data/Documentation/en/ScriptReference/AssetDatabase.html">External documentation for `AssetDatabase`</a></footer>
+		public static IEnumerable<T> FindAssets<T>(string filter, string[] searchInFolders) 
+			where T : UnityEngine.Object
+		{
+			var GUIDs = AssetDatabase.FindAssets(filter, searchInFolders);
+			if (GUIDs == null || GUIDs.Length == 0)
+				return null;
+			
+			var assets = new List<T>(); 
+			foreach (var GUID in GUIDs)
+			{
+				var path = AssetDatabase.GUIDToAssetPath(GUID);
+				assets.Add(AssetDatabase.LoadAssetAtPath<T>(path));
+			}
+			return assets;
+		}
 	}
 }
