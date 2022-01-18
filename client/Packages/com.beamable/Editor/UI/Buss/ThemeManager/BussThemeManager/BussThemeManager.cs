c2b106ae8575754469b630510521d2427a49237c
Gleuq <arturmankowski22@gmail.com>
2022-01-18 12:37:13 +0100

    Update

--- client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/BussThemeManager/BussThemeManager.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/BussThemeManager/BussThemeManager.cs
@@ -1,4 +1,5 @@
 ﻿using Beamable.Editor.UI.Buss;
+using Beamable.Editor.UI.Buss.Components;
 using Beamable.Editor.UI.Components;
 using Beamable.UI.Buss;
 using Editor.UI.BUSS;
@@ -108,6 +109,21 @@ namespace Beamable.UI.BUSS
 		{
 			ClearStyleCards();
 			CreateStyleCards();
+			AddSelectorButton();
+		}
+
+		private void AddSelectorButton()
+		{
+			var addSelectorButton = new VisualElement {name = "addSelectorButton"};
+			addSelectorButton.AddToClassList("button");
+			addSelectorButton.Add(new Label("Add Selector"));
+			addSelectorButton.RegisterCallback<MouseDownEvent>(_ =>
+			{
+				var window = AddSelectorWindow.ShowWindow();
+				window?.Init();
+			});
+			
+			_stylesGroup.Add(addSelectorButton);
 		}
 
 		private void ClearCurrentStyleSheet()
