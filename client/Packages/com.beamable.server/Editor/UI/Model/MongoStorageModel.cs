8462537efe67c556a092575a5832da8f90fc7fc5
MevLyshkin <siuszko@zoho.com>
2022-01-20 11:36:03 +0100

    [BEAM-2058] Autoformat files in Pull Requests (#414)
    * Add new version of job
    
    * Test editing one file
    
    * Automated dotnet-format update by  <+@users.noreply.github.com>
    
    * Test two other files
    
    * Automated dotnet-format update by  <+@users.noreply.github.com>
    
    * Preserve single line blocks
    
    * Automated dotnet-format update by  <+@users.noreply.github.com>
    
    * Job cleanup
    
    * Remove has-changes-action dep
    
    * Test one more file
    
    * Automated dotnet-format update by  <+@users.noreply.github.com>
    
    * [BEAM-2058] Use better command
    
    * Autoformat code
    
    * Autoformat code
    
    * Remove push dependency, run dotnet-format before everything
    
    * Restore MicroserviceModel from main
    
    * Autoformat code
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- client/Packages/com.beamable.server/Editor/UI/Model/MongoStorageModel.cs
+++ client/Packages/com.beamable.server/Editor/UI/Model/MongoStorageModel.cs
@@ -1,14 +1,14 @@
 ﻿
-using System;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 using Beamable.Common;
 using Beamable.Editor.Environment;
 using Beamable.Server;
 using Beamable.Server.Editor;
 using Beamable.Server.Editor.DockerCommands;
 using Beamable.Server.Editor.ManagerClient;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 #if UNITY_2018
@@ -22,60 +22,60 @@ using UnityEditor.UIElements;
 
 namespace Beamable.Editor.UI.Model
 {
-    [System.Serializable]
-    public class MongoStorageModel : ServiceModelBase, IBeamableStorageObject
-    {
-        public StorageObjectDescriptor ServiceDescriptor { get; private set; }
-        public MongoStorageBuilder ServiceBuilder { get; private set; }
-        public override IBeamableBuilder Builder => ServiceBuilder;
-        public override IDescriptor Descriptor => ServiceDescriptor;
-        public override bool IsRunning => ServiceBuilder?.IsRunning ?? false;
-        public StorageConfigurationEntry Config { get; private set; }
+	[System.Serializable]
+	public class MongoStorageModel : ServiceModelBase, IBeamableStorageObject
+	{
+		public StorageObjectDescriptor ServiceDescriptor { get; private set; }
+		public MongoStorageBuilder ServiceBuilder { get; private set; }
+		public override IBeamableBuilder Builder => ServiceBuilder;
+		public override IDescriptor Descriptor => ServiceDescriptor;
+		public override bool IsRunning => ServiceBuilder?.IsRunning ?? false;
+		public StorageConfigurationEntry Config { get; private set; }
 
-        public override event Action<Task> OnStart;
-        public override event Action<Task> OnStop;
-        
-        public static MongoStorageModel CreateNew(StorageObjectDescriptor descriptor)
-        {
-            return new MongoStorageModel
-            {
-                ServiceDescriptor = descriptor,
-                ServiceBuilder = Microservices.GetStorageBuilder(descriptor),
-                Config = MicroserviceConfiguration.Instance.GetStorageEntry(descriptor.Name)
-            };
-        }
+		public override event Action<Task> OnStart;
+		public override event Action<Task> OnStop;
 
-        public override Task Start()
-        {
-            OnLogsAttached?.Invoke();
-            var task = ServiceBuilder.TryToStart();
-            OnStart?.Invoke(task);
-            return task;
-        }
-        public override Task Stop()
-        {
-            var task = ServiceBuilder.TryToStop();
-            OnStop?.Invoke(task);
-            return task;
-        }
-        public override void PopulateMoreDropdown(ContextualMenuPopulateEvent evt)
-        {
-            // TODO - Implement copy connection strings
-            
-            evt.menu.BeamableAppendAction($"Erase data", _ => AssemblyDefinitionHelper.ClearMongo(ServiceDescriptor));
-            //evt.menu.BeamableAppendAction($"Copy connection strings", _ => Debug.Log("Not implemented!"));
-            evt.menu.BeamableAppendAction($"Goto data explorer", _ => AssemblyDefinitionHelper.OpenMongoExplorer(ServiceDescriptor));
-            evt.menu.BeamableAppendAction($"Create a snapshot", _ => AssemblyDefinitionHelper.SnapshotMongo(ServiceDescriptor));
-            evt.menu.BeamableAppendAction($"Download a snapshot", _ => AssemblyDefinitionHelper.RestoreMongo(ServiceDescriptor));
-            evt.menu.BeamableAppendAction($"Open C# Code", _ => OpenCode());
-        }
-        public override void Refresh(IDescriptor descriptor)
-        {
-            // reset the descriptor and statemachines; because they aren't system.serializable durable.
-            ServiceDescriptor = (StorageObjectDescriptor)descriptor;
-            var oldBuilder = ServiceBuilder;
-            ServiceBuilder = Microservices.GetStorageBuilder(ServiceDescriptor);
-            ServiceBuilder.ForwardEventsTo(oldBuilder);
-        }
-    }
+		public static MongoStorageModel CreateNew(StorageObjectDescriptor descriptor)
+		{
+			return new MongoStorageModel
+			{
+				ServiceDescriptor = descriptor,
+				ServiceBuilder = Microservices.GetStorageBuilder(descriptor),
+				Config = MicroserviceConfiguration.Instance.GetStorageEntry(descriptor.Name)
+			};
+		}
+
+		public override Task Start()
+		{
+			OnLogsAttached?.Invoke();
+			var task = ServiceBuilder.TryToStart();
+			OnStart?.Invoke(task);
+			return task;
+		}
+		public override Task Stop()
+		{
+			var task = ServiceBuilder.TryToStop();
+			OnStop?.Invoke(task);
+			return task;
+		}
+		public override void PopulateMoreDropdown(ContextualMenuPopulateEvent evt)
+		{
+			// TODO - Implement copy connection strings
+
+			evt.menu.BeamableAppendAction($"Erase data", _ => AssemblyDefinitionHelper.ClearMongo(ServiceDescriptor));
+			//evt.menu.BeamableAppendAction($"Copy connection strings", _ => Debug.Log("Not implemented!"));
+			evt.menu.BeamableAppendAction($"Goto data explorer", _ => AssemblyDefinitionHelper.OpenMongoExplorer(ServiceDescriptor));
+			evt.menu.BeamableAppendAction($"Create a snapshot", _ => AssemblyDefinitionHelper.SnapshotMongo(ServiceDescriptor));
+			evt.menu.BeamableAppendAction($"Download a snapshot", _ => AssemblyDefinitionHelper.RestoreMongo(ServiceDescriptor));
+			evt.menu.BeamableAppendAction($"Open C# Code", _ => OpenCode());
+		}
+		public override void Refresh(IDescriptor descriptor)
+		{
+			// reset the descriptor and statemachines; because they aren't system.serializable durable.
+			ServiceDescriptor = (StorageObjectDescriptor)descriptor;
+			var oldBuilder = ServiceBuilder;
+			ServiceBuilder = Microservices.GetStorageBuilder(ServiceDescriptor);
+			ServiceBuilder.ForwardEventsTo(oldBuilder);
+		}
+	}
 }
