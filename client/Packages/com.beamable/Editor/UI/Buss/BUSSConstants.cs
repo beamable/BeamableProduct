947d3972983c259d7ba3c5f120e3e9f88636edaa
Łukasz Kundys <lukaszkundys@gmail.com>
2022-01-21 12:21:58 +0100

    Confirmation popup while trying to delete whole style card

--- client/Packages/com.beamable/Editor/UI/Buss/BUSSConstants.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/BUSSConstants.cs
@@ -5,5 +5,8 @@ namespace Beamable.UI.BUSS
 	public static class BUSSConstants
 	{
 		public static readonly Vector2 ThemeManagerWindowSize = new Vector2(500, 300);
+		
+		public static readonly string StyleDeletionHeader = "Delete style";
+		public static readonly string StyleDeletionMessage = "Are You sure You want to delete this style?";
 	}
 }
