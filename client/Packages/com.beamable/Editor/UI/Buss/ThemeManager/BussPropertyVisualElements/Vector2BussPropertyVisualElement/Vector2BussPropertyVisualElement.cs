020286d4323b03f2dc2797aa75ed745abd6f7af6
Łukasz Kundys <lukaszkundys@gmail.com>
2022-01-24 16:32:09 +0100

    Missing statements

--- client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/BussPropertyVisualElements/Vector2BussPropertyVisualElement/Vector2BussPropertyVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/BussPropertyVisualElements/Vector2BussPropertyVisualElement/Vector2BussPropertyVisualElement.cs
@@ -1,7 +1,12 @@
 ﻿using Beamable.UI.Buss;
-using UnityEditor.Experimental.UIElements;
 using UnityEngine;
+#if UNITY_2018
 using UnityEngine.Experimental.UIElements;
+using UnityEditor.Experimental.UIElements;
+#elif UNITY_2019_1_OR_NEWER
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+#endif
 
 namespace Beamable.Editor.UI.Components
 {
