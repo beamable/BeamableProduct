51f368d49da49b6615c36e964154d04b1691e0ed
Chris Hanna <cdhannaphone@gmail.com>
2022-01-25 08:57:21 -0500

    only dotnet format when going into main (#445)
    * only dotnet format when going into main
    
    * poking the format tool
    
    * skip step not job
    
    * poke
    
    * poke 2
    
    * conditions
    
    * use branch protect
    
    * help me
    
    * final test
    
    * bad format
    
    * derp
    
    * Autoformat code
    
    * yarg
    
    * show git status
    
    * sily
    
    * Autoformat code
    
    * add permission
    
    * syntax
    
    * what r enums tho
    
    * all the juice
    
    * Autoformat code
    
    * github is a jekr
    
    * Autoformat code
    
    * dont use creds
    
    * pass creds to changedfiles
    
    * Autoformat code
    
    * autocancel
    
    * poke
    
    * Autoformat code
    
    * fast cancel
    
    * Autoformat code
    
    * meta file stuff
    
    * more stupid meta files
    
    * renaming buss constants file
    
    * rename buss
    
    * use latest 2018 lts
    
    * Autoformat code
    
    * fix clean iap
    
    * formatter die
    
    * meta file stuff
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- build/clean_iap.sh
+++ build/clean_iap.sh
@@ -2,7 +2,7 @@
 #!/bin/sh
 echo "Unity=[${UNITY_VERSION}]. Checking to see if we should destroy the Unity IAP Plugin, because it gets auto-generated in later versions of Unity."
 
-if [ "$UNITY_VERSION" != "2018.4.18f1" ]
+if [ "$UNITY_VERSION" != "2018.4.36f1" ]
 then
     echo "Deleting Unity IAP Plugin folder"
     rm -rf ./client/Assets/Plugins/UnityPurchasing
