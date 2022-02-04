6e6a671dd3fcd9301bc56dec3e4746ecce542191
Łukasz Kundys <lukaszkundys@gmail.com>
2022-02-04 16:53:26 +0100

    In progress

--- client/Packages/com.beamable/Runtime/Modules/AccountManagement/Scripts/AccountDisplayItem.cs
+++ client/Packages/com.beamable/Runtime/Modules/AccountManagement/Scripts/AccountDisplayItem.cs
@@ -15,236 +15,246 @@ using TMPro;
 
 namespace Beamable.AccountManagement
 {
-   public class AccountDisplayItem : MonoBehaviour
-   {
+	public class AccountDisplayItem : MonoBehaviour
+	{
+		public TextMeshProUGUI _emailTxt, _progressTxt;
+		public Image _statusImg;
+		public StyleBehaviour StatusSelectedBehaviour, StatusDeleteBehaviour;
+		public GameObject ThirdPartiesGroup;
+		public GameObject EmailIcon, FacebookIcon, AppleIcon, GoogleIcon;
+		public List<GameObject> RemoveAccountElements;
+		public Button AccountButton;
+		public TextReference AttachedEmailTextReference;
 
-      public TextMeshProUGUI _emailTxt, _progressTxt;
-      public Image _statusImg;
-      public StyleBehaviour StatusSelectedBehaviour, StatusDeleteBehaviour;
-      public GameObject EmailIcon, FacebookIcon, AppleIcon, GoogleIcon;
-      public List<GameObject> RemoveAccountElements;
-      public Button AccountButton;
-      public TextReference AttachedEmailTextReference;
+		private bool _isActiveAccount = false;
 
-      private bool _isActiveAccount = false;
+		public TokenReference TokenReference;
 
-      public TokenReference TokenReference;
+		public string SubTextLabel;
+		public StatBehaviour DisplayNameStat;
+		public StatBehaviour SubTextStat;
 
-      public string SubTextLabel;
-      public StatBehaviour DisplayNameStat;
-      public StatBehaviour SubTextStat;
+		public StatBehaviour AvatarStat;
+		public AccountAvatarBehaviour AvatarBehaviour;
+		public User User { get; private set; }
+		public bool IsCurrent { get; private set; }
 
-      public StatBehaviour AvatarStat;
-      public AccountAvatarBehaviour AvatarBehaviour;
-      public User User { get; private set; }
-      public bool IsCurrent { get; private set; }
+		public Promise<List<AccountManagementConfiguration.UserThirdPartyAssociation>> ThirdPartyAssociationPromise
+		{
+			get;
+			private set;
+		}
 
-      public Promise<List<AccountManagementConfiguration.UserThirdPartyAssociation>> ThirdPartyAssociationPromise { get; private set; }
-      public Promise<UserExtensions.StatCollection> StatPromise { get; private set; }
+		public Promise<UserExtensions.StatCollection> StatPromise { get; private set; }
 
-      public Promise<AccountDisplayItem> LoadingPromise { get; private set; }
+		public Promise<AccountDisplayItem> LoadingPromise { get; private set; }
 
-      private bool _initializedStatCallbacks = false;
+		private bool _initializedStatCallbacks = false;
 
-      void Awake()
-      {
-         InitializeStatCallbacks();
-      }
+		void Awake()
+		{
+			InitializeStatCallbacks();
+		}
 
-      void InitializeStatCallbacks()
-      {
-         if (_initializedStatCallbacks) return;
-         _initializedStatCallbacks = true;
-         DisplayNameStat.OnStatReceived.AddListener(OnAliasAvailable);
-         SubTextStat.OnStatReceived.AddListener(OnSubtextAvailable);
-      }
+		void InitializeStatCallbacks()
+		{
+			if (_initializedStatCallbacks) return;
+			_initializedStatCallbacks = true;
+			DisplayNameStat.OnStatReceived.AddListener(OnAliasAvailable);
+			SubTextStat.OnStatReceived.AddListener(OnSubtextAvailable);
+		}
 
-      public void Apply()
-      {
+		public void Apply()
+		{
+			// first, await the entire operation.
+			LoadingPromise.Then(_ =>
+			{
+				// apply the third party icons...
+				ThirdPartyAssociationPromise.Then(assocs =>
+				{
+					bool hasDBCredentials = User.HasDBCredentials();
+					bool hasApple = false;
+					bool hasFb = false;
 
-         // first, await the entire operation.
-         LoadingPromise.Then(_ =>
-         {
+					EmailIcon.SetActive(hasDBCredentials);
+					if (AttachedEmailTextReference != null)
+					{
+						AttachedEmailTextReference.Value = User.email;
+					}
 
-            // apply the third party icons...
-            ThirdPartyAssociationPromise.Then(assocs =>
-            {
-               EmailIcon.SetActive(User.HasDBCredentials());
-               if (AttachedEmailTextReference != null)
-               {
-                  AttachedEmailTextReference.Value = User.email;
-               }
+					foreach (var assoc in assocs)
+					{
+						switch (assoc.ThirdParty)
+						{
+							case AuthThirdParty.Apple:
+								AppleIcon.SetActive(assoc.ShouldShowIcon);
+								hasApple = assoc.ShouldShowIcon;
+								break;
+							case AuthThirdParty.Facebook:
+								FacebookIcon.SetActive(assoc.ShouldShowIcon);
+								hasFb = assoc.ShouldShowIcon;
+								break;
+							case AuthThirdParty.Google:
+								GoogleIcon.SetActive(assoc.ShouldShowIcon);
+								break;
+						}
+					}
 
-               foreach (var assoc in assocs)
-               {
-                  switch (assoc.ThirdParty)
-                  {
-                     case AuthThirdParty.Apple:
-                        AppleIcon.SetActive(assoc.ShouldShowIcon);
-                        break;
-                     case AuthThirdParty.Facebook:
-                        FacebookIcon.SetActive(assoc.ShouldShowIcon);
-                        break;
-                     case AuthThirdParty.Google:
-                        GoogleIcon.SetActive(assoc.ShouldShowIcon);
-                        break;
-                  }
-               }
+					if (ThirdPartiesGroup != null)
+					{
+						ThirdPartiesGroup.SetActive(hasDBCredentials || hasFb || hasApple);
+					}
 
-               LayoutCredentialIcons();
-            });
+					LayoutCredentialIcons();
+				});
 
-            // apply the stat data
-            StatPromise.Then(stats =>
-            {
-               InitializeStatCallbacks();
-               var config = AccountManagementConfiguration.Instance;
-               DisplayNameStat.SetStat(config.DisplayNameStat);
-               SubTextStat.SetStat(config.SubtextStat);
-               AvatarStat.SetStat(config.AvatarStat);
-               SubTextLabel = config.SubtextLabel;
+				// apply the stat data
+				StatPromise.Then(stats =>
+				{
+					InitializeStatCallbacks();
+					var config = AccountManagementConfiguration.Instance;
+					DisplayNameStat.SetStat(config.DisplayNameStat);
+					SubTextStat.SetStat(config.SubtextStat);
+					AvatarStat.SetStat(config.AvatarStat);
+					SubTextLabel = config.SubtextLabel;
 
-               DisplayNameStat.SetForUser(User);
-               SubTextStat.SetForUser(User);
-               AvatarStat.SetForUser(User);
+					DisplayNameStat.SetForUser(User);
+					SubTextStat.SetForUser(User);
+					AvatarStat.SetForUser(User);
 
-               var alias = stats.Get(DisplayNameStat.Stat);
-               var subtext = stats.Get(SubTextStat.Stat);
-               var avatar = stats.Get(AvatarStat.Stat);
+					var alias = stats.Get(DisplayNameStat.Stat);
+					var subtext = stats.Get(SubTextStat.Stat);
+					var avatar = stats.Get(AvatarStat.Stat);
 
-               var aliasValue = alias ?? (User.email ?? "Anonymous");
-               var subtextValue = subtext ?? (User.id.ToString());
-               SubTextStat.DefaultValueOverride = subtextValue;
-               AvatarStat.DefaultValueOverride = avatar;
-               DisplayNameStat.SetCurrentValue(aliasValue);
-               SubTextStat.SetCurrentValue(subtextValue);
-               AvatarStat.SetCurrentValue(avatar);
-               DisplayNameStat.RefreshOnStart = true;
-               SubTextStat.RefreshOnStart = true;
-               AvatarStat.RefreshOnStart = true;
-            });
+					var aliasValue = alias ?? (User.email ?? "Anonymous");
+					var subtextValue = subtext ?? (User.id.ToString());
+					SubTextStat.DefaultValueOverride = subtextValue;
+					AvatarStat.DefaultValueOverride = avatar;
+					DisplayNameStat.SetCurrentValue(aliasValue);
+					SubTextStat.SetCurrentValue(subtextValue);
+					AvatarStat.SetCurrentValue(avatar);
+					DisplayNameStat.RefreshOnStart = true;
+					SubTextStat.RefreshOnStart = true;
+					AvatarStat.RefreshOnStart = true;
+				});
 
-            SetActiveAccount(IsCurrent);
-            SetRemoveAccountElements(false);
-            gameObject.SetActive(true);
-         });
+				SetActiveAccount(IsCurrent);
+				SetRemoveAccountElements(false);
+				gameObject.SetActive(true);
+			});
+		}
 
-      }
+		void LayoutCredentialIcons()
+		{
+			var parent = EmailIcon.transform.parent as RectTransform;
 
-      void LayoutCredentialIcons()
-      {
-         var parent = EmailIcon.transform.parent as RectTransform;
+			/*
+		  * we want to layout icons right-justified, based on the gameobject's enabled state
+		  */
 
-         /*
-       * we want to layout icons right-justified, based on the gameobject's enabled state
-       */
+			var iconWidth = 20;
+			var activeCount = 0;
+			for (var i = 0; i < parent.childCount; i++)
+			{
+				var child = parent.GetChild(i) as RectTransform;
+				if (child.gameObject.activeInHierarchy)
+				{
+					child.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, activeCount * iconWidth, iconWidth);
+					activeCount += 1;
+				}
+			}
+		}
 
-         var iconWidth = 20;
-         var activeCount = 0;
-         for (var i = 0; i < parent.childCount; i++)
-         {
-            var child = parent.GetChild(i) as RectTransform;
-            if (child.gameObject.activeInHierarchy)
-            {
-               child.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, activeCount * iconWidth, iconWidth);
-               activeCount += 1;
-            }
-         }
-      }
+		public void SetRemoveAccountElements(bool active)
+		{
+			if (AccountButton == null) return;
 
-      public void SetRemoveAccountElements(bool active)
-      {
-         if (AccountButton == null) return;
+			AccountButton.interactable = !active;
+			foreach (var element in RemoveAccountElements)
+			{
+				element?.SetActive(active);
+			}
+		}
 
-         AccountButton.interactable = !active;
-         foreach (var element in RemoveAccountElements)
-         {
-            element?.SetActive(active);
-         }
-      }
+		public void SetActiveAccount(bool active)
+		{
+			if (StatusSelectedBehaviour != null)
+			{
+				StatusSelectedBehaviour.enabled = active;
+			}
 
-      public void SetActiveAccount(bool active)
-      {
-         if (StatusSelectedBehaviour != null)
-         {
-            StatusSelectedBehaviour.enabled = active;
-         }
+			if (StatusDeleteBehaviour != null)
+			{
+				StatusDeleteBehaviour.enabled = !active;
+			}
+		}
 
-         if (StatusDeleteBehaviour != null)
-         {
-            StatusDeleteBehaviour.enabled = !active;
-         }
-      }
+		public void OnSubtextAvailable(string subText)
+		{
+			_progressTxt.text = $"{SubTextLabel}{subText}";
+		}
 
-      public void OnSubtextAvailable(string subText)
-      {
-         _progressTxt.text = $"{SubTextLabel}{subText}";
-      }
+		public void OnAliasAvailable(string alias)
+		{
+			_emailTxt.text = alias;
+		}
 
-      public void OnAliasAvailable(string alias)
-      {
-         _emailTxt.text = alias;
-      }
+		public void OnStatusClicked()
+		{
+			if (!_isActiveAccount)
+			{
+				SetRemoveAccountElements(true);
+			}
+		}
 
-      public void OnStatusClicked()
-      {
-         if (!_isActiveAccount)
-         {
-            SetRemoveAccountElements(true);
-         }
-      }
+		public void Destroy()
+		{
+			GameObject.Destroy(gameObject);
+		}
 
-      public void Destroy()
-      {
-         GameObject.Destroy(gameObject);
-      }
+		public Promise<AccountDisplayItem> StartLoading(AccountDisplayItem clone)
+		{
+			User = clone.User;
+			IsCurrent = clone.IsCurrent;
+			if (TokenReference != null)
+			{
+				TokenReference.Bundle = clone.TokenReference.Bundle;
+			}
 
-      public Promise<AccountDisplayItem> StartLoading(AccountDisplayItem clone)
-      {
-         User = clone.User;
-         IsCurrent = clone.IsCurrent;
-         if (TokenReference != null)
-         {
-            TokenReference.Bundle = clone.TokenReference.Bundle;
-         }
+			StatPromise = clone.StatPromise;
+			ThirdPartyAssociationPromise = clone.ThirdPartyAssociationPromise;
+			LoadingPromise = clone.LoadingPromise;
 
-         StatPromise = clone.StatPromise;
-         ThirdPartyAssociationPromise = clone.ThirdPartyAssociationPromise;
-         LoadingPromise = clone.LoadingPromise;
+			return LoadingPromise;
+		}
 
-         return LoadingPromise;
-      }
+		public Promise<AccountDisplayItem> StartLoading(User user, bool isCurrent, TokenResponse token)
+		{
+			User = user;
+			IsCurrent = isCurrent;
+			if (TokenReference != null && token != null)
+			{
+				TokenReference.Bundle = new UserBundle {User = user, Token = token};
+			}
 
-      public Promise<AccountDisplayItem> StartLoading(User user, bool isCurrent, TokenResponse token)
-      {
-         User = user;
-         IsCurrent = isCurrent;
-         if (TokenReference != null && token != null)
-         {
-            TokenReference.Bundle = new UserBundle
-            {
-               User = user,
-               Token = token
-            };
-         }
+			StatPromise = user.GetStats(
+				AccountManagementConfiguration.Instance.DisplayNameStat,
+				AccountManagementConfiguration.Instance.SubtextStat,
+				AccountManagementConfiguration.Instance.AvatarStat);
 
-         StatPromise = user.GetStats(
-            AccountManagementConfiguration.Instance.DisplayNameStat,
-            AccountManagementConfiguration.Instance.SubtextStat,
-            AccountManagementConfiguration.Instance.AvatarStat);
+			ThirdPartyAssociationPromise =
+				AccountManagementConfiguration.Instance.GetAllEnabledThirdPartiesForUser(User);
 
-         ThirdPartyAssociationPromise = AccountManagementConfiguration.Instance.GetAllEnabledThirdPartiesForUser(User);
+			LoadingPromise = Promise.Sequence(StatPromise.ToUnit(), ThirdPartyAssociationPromise.ToUnit())
+			                        .Map(_ => this);
 
-         LoadingPromise = Promise.Sequence(StatPromise.ToUnit(), ThirdPartyAssociationPromise.ToUnit()).Map(_ => this);
+			return LoadingPromise;
+		}
+	}
 
-         return LoadingPromise;
-      }
-
-   }
-
-   struct IconEnableData
-   {
-      public bool HasThirdParty;
-      public GameObject IconGameObject;
-   }
+	struct IconEnableData
+	{
+		public bool HasThirdParty;
+		public GameObject IconGameObject;
+	}
 }
