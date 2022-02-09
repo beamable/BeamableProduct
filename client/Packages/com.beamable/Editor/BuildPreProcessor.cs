86ec85445c4c95d1608df95109e25e5c58162d9b
Chris Hanna <cdhannaphone@gmail.com>
2022-02-09 17:04:40 -0500

    Feature/beam 2238 2 (#630)
    * beam 2238 - fix various build issues
    
    # Conflicts:
    #	client/Assets/Beamable/Common/Unity.Beamable.Customer.Common.asmdef
    #	client/Packages/com.beamable.server/Editor/CommonAreaService.cs
    #	client/Packages/com.beamable/Editor/BuildPreProcessor.cs
    
    * run link gen always

--- client/Packages/com.beamable/Editor/BuildPreProcessor.cs
+++ client/Packages/com.beamable/Editor/BuildPreProcessor.cs
@@ -12,13 +12,22 @@ namespace Beamable.Editor
 		public int callbackOrder { get; }
 
 #if !UNITY_STANDALONE
-		public void OnPreprocessBuild(BuildReport report) { }
+		public void OnPreprocessBuild(BuildReport report) {
+			if (CoreConfiguration.Instance.PreventCodeStripping)
+            {
+				BeamableLinker.GenerateLinkFile();
+            }
+ }
 #else
         public async void OnPreprocessBuild(BuildReport report)
         {
             if (ContentConfiguration.Instance.BakeContentOnBuild)
             {
-                await ContentIO.BakeContent();    
+                await ContentIO.BakeContent();
+            }
+			if (CoreConfiguration.Instance.PreventCodeStripping)
+            {
+				BeamableLinker.GenerateLinkFile();
             }
         }
 #endif
