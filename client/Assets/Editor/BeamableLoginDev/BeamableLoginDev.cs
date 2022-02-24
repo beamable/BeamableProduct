6707865ec620c0164f0bae1926d3f4c8ee33555d
Artur Mankowski <arturmankowski22@gmail.com>
2022-02-24 13:49:11 +0100

    Quicker login for beamable devs

---
+++ client/Assets/Editor/BeamableLoginDev/BeamableLoginDev.cs
@@ -0,0 +1,53 @@
+﻿#if BEAMABLE_DEVELOPER
+
+using Beamable.Editor;
+using System;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public class BeamableLoginDev
+{
+	private const string PATH_TO_LOGIN_DATA = "Assets/Editor/BeamableLoginDev/login_data.json";
+
+	[MenuItem("Beamable/Login with dev credits", false, priority = 0)]
+	public static async void Login()
+	{
+		var editorAPI = await EditorAPI.Instance;
+		var isLoggedIn = editorAPI.User != null;
+
+		if (isLoggedIn)
+		{
+			Debug.LogWarning($"You are already logged in!");
+			return;
+		}
+
+		var json = File.ReadAllText(PATH_TO_LOGIN_DATA);
+		var data = JsonUtility.FromJson<BeamableDevLoginData>(json);
+
+		if (data.IsDataMissing)
+		{
+			Debug.LogError($"Login data is missing! Check {PATH_TO_LOGIN_DATA}");
+			return;
+		}
+
+		var password = Encoding.UTF8.GetString(Convert.FromBase64String(data.password));
+		await editorAPI.LoginCustomer(data.aliasOrCid, data.email, password);
+	}
+}
+
+[Serializable]
+public class BeamableDevLoginData
+{
+	public bool IsDataMissing =>
+		string.IsNullOrWhiteSpace(aliasOrCid) ||
+		string.IsNullOrWhiteSpace(email) ||
+		string.IsNullOrWhiteSpace(password);
+
+	public string aliasOrCid;
+	public string email;
+	public string password;
+}
+
+#endif
