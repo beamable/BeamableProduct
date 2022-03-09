1987c4714510c620dc4b4d6444c7ae8bdfd3af76
Piotr Siuszko <piotr@beamable.com>
2022-03-09 16:23:44 +0100

    [BEAM-2328] Add input system that fits settings

--- client/Packages/com.beamable/Runtime/Modules/Input/BeamableInput.cs
+++ client/Packages/com.beamable/Runtime/Modules/Input/BeamableInput.cs
@@ -1,3 +1,13 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+#if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
+#if UNITY_2018
+using UnityEngine.Experimental.Input.UI;
+#else
+using UnityEngine.InputSystem.UI;
+#endif
+#endif
+
 namespace Beamable.InputManagerIntegration
 {
 	public static class BeamableInput
@@ -5,6 +15,18 @@ namespace Beamable.InputManagerIntegration
 		public static bool IsActionTriggered(InputActionArg arg)
 		{
 			return arg?.IsTriggered() ?? false;
+		}
+
+		public static void AddInputSystem()
+		{
+			
+			var eventSystem = new GameObject("EventSystem");
+#if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
+			eventSystem.AddComponent<InputSystemUIInputModule>();
+#else
+				eventSystem.AddComponent<StandaloneInputModule>();
+#endif
+			eventSystem.AddComponent<EventSystem>();
 		}
 	}
 }
