da4e73dd652627630c1274c1529b5eefd4dc08a8
MevLyshkin <piotr@beamable.com>
2022-03-15 21:31:35 +0100

    [BEAM-2348][BEAM-2347] MS play button refactor (#783)
    * [BEAM-2348] Microservices play button refactor
    
    * [BEAM-2347] Center align play button
    
    * [BEAM-2347] Better approach to centering
    
    * [BEAM-2348] Code simplify
    
    * [BEAM-2347] Additional fixes for creating services versions
    
    * Last fixes

--- client/Packages/com.beamable.server/Editor/UI/Components/MicroserviceVisualElement/MicroserviceVisualElement.cs
+++ client/Packages/com.beamable.server/Editor/UI/Components/MicroserviceVisualElement/MicroserviceVisualElement.cs
@@ -23,10 +23,6 @@ namespace Beamable.Editor.Microservice.UI.Components
 		{ }
 		protected override string ScriptName => nameof(MicroserviceVisualElement);
 
-		private Action _defaultBuildAction;
-
-		private VisualElement _buildDefaultLabel;
-		private Button _buildBtn;
 		private MicroserviceModel _microserviceModel;
 
 		protected override void OnDestroy()
@@ -45,15 +41,14 @@ namespace Beamable.Editor.Microservice.UI.Components
 		protected override void QueryVisualElements()
 		{
 			base.QueryVisualElements();
-			_buildBtn = Root.Q<Button>("buildBtn");
-			_buildDefaultLabel = Root.Q<VisualElement>("buildImage");
 			_microserviceModel = (MicroserviceModel)Model;
 		}
 		protected override void UpdateVisualElements()
 		{
 			base.UpdateVisualElements();
-			_buildBtn.clickable.clicked -= HandleBuildButtonClicked;
-			_buildBtn.clickable.clicked += HandleBuildButtonClicked;
+			Root.Q("leftArea")?.RemoveFromHierarchy();
+			_startButton.clickable.clicked -= HandleStartButtonClicked;
+			_startButton.clickable.clicked += HandleStartButtonClicked;
 			_microserviceModel.OnBuildAndStart -= SetupProgressBarForBuildAndStart;
 			_microserviceModel.OnBuildAndStart += SetupProgressBarForBuildAndStart;
 			_microserviceModel.OnBuildAndRestart -= SetupProgressBarForBuildAndRestart;
@@ -118,26 +113,24 @@ namespace Beamable.Editor.Microservice.UI.Components
 		{
 			new StepLogParser(_loadingBar, Model, task);
 		}
-		private void HandleBuildButtonClicked() => _defaultBuildAction?.Invoke();
+		private void HandleStartButtonClicked()
+		{
+			if (_microserviceModel.IsRunning)
+			{
+				_microserviceModel.Stop();
+			}
+			else
+			{
+				_microserviceModel.BuildAndStart();
+			}
+		}
 
 		protected override void UpdateButtons()
 		{
 			base.UpdateButtons();
-			_stopButton.visible = Model.IsRunning;
-			_buildBtn.tooltip = GetBuildButtonString(_microserviceModel.IncludeDebugTools,
-													 _microserviceModel.IsRunning ? BUILD_RESET : BUILD_START);
-			_buildDefaultLabel.EnableInClassList("running", _microserviceModel.IsRunning);
-
-			if (_microserviceModel.IsRunning)
-			{
-				_defaultBuildAction = () => _microserviceModel.BuildAndRestart();
-			}
-			else
-			{
-				_defaultBuildAction = () => _microserviceModel.BuildAndStart();
-			}
-			_stopButton.SetEnabled(_microserviceModel.ServiceBuilder.HasImage && !_microserviceModel.IsBuilding);
-			_buildBtn.SetEnabled(!_microserviceModel.IsBuilding);
+			_startButton.tooltip = GetBuildButtonString(_microserviceModel.IncludeDebugTools,
+													 _microserviceModel.IsRunning ? STOP : BUILD_START);
+			_startButton.SetEnabled(!_microserviceModel.IsBuilding);
 		}
 	}
 }
