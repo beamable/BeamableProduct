3c4498c3f6f78ac82be7fdec7f373ac7e2c94ce0
PedroRauizBeamable <92586258+PedroRauizBeamable@users.noreply.github.com>
2022-03-22 07:20:54 -0300

    upon deserialization after domain reload, set the deserialized C#MS model as it's _instance variable; Also fixes BEAM-2405 (#820)
    - This has to do with a current issue we have with the way our Editor-time data models are initialized.
    - The architectural fix for this type of problem will happen at a later release (1.2 being the most likely).
    - This does fix the immediate problem though

--- client/Packages/com.beamable.server/Editor/UI/Model/MicroservicesDataModel.cs
+++ client/Packages/com.beamable.server/Editor/UI/Model/MicroservicesDataModel.cs
@@ -300,6 +300,8 @@ namespace Beamable.Editor.UI.Model
 
 		public void OnAfterDeserialize()
 		{
+			_instance = this;
+			
 			void AddModels<T>(List<T> models, List<IBeamableService> listToPopulate) where T : ServiceModelBase
 			{
 				foreach (var service in models.ToArray())
@@ -343,6 +345,7 @@ namespace Beamable.Editor.UI.Model
 			}
 			AddModels(_localMicroserviceModels, AllLocalServices);
 			AddModels(_localStorageModels, AllLocalServices);
+			
 		}
 	}
 
