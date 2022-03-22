a4cb6d4768554591c19e82b4541478b54840c249
Chris Hanna <cdhannaphone@gmail.com>
2022-03-22 09:22:40 -0400

    beam 2424- assume user without admin creds (#823)

--- client/Packages/com.beamable.server/CHANGELOG.md
+++ client/Packages/com.beamable.server/CHANGELOG.md
@@ -18,6 +18,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - When exiting Unity, all related Microservices and Microstorage containers are closed
 - Microservice client code is generated in a dockerized dotnet runtime instead of Unity
 - Added docstrings to `StatsService.SearchStats` to clarify correct usage of the `Criteria` parameter.
+- `AssumeUser` takes an optional boolean parameter to disable the Admin access token check
 
 ### Fixed
 - Fixed issue that caused the `ReflectionCache` to run an extra unnecessary time when a `.cs` or `.asmdef` file were changed.
---
+++ microservice/microserviceTests/microservice/dbmicroservice/BeamableMicroServiceTests/AssumeUserTests.cs
@@ -0,0 +1,122 @@
+using System.Threading.Tasks;
+using Beamable.Common;
+using Beamable.Common.Api;
+using Beamable.Common.Api.Content;
+using Beamable.Microservice.Tests.Socket;
+using Beamable.Server;
+using microserviceTests.microservice.Util;
+using NUnit.Framework;
+
+namespace microserviceTests.microservice.dbmicroservice.BeamableMicroServiceTests;
+
+[TestFixture]
+public class AssumeUserTests
+{
+	[SetUp]
+	[TearDown]
+	public void ResetContentInstance()
+	{
+		ContentApi.Instance = new Promise<IContentApi>();
+	}
+
+
+	[TestCase(true)]
+	[TestCase(false)]
+	[NonParallelizable]
+	public async Task AssumeUserAsAdmin(bool forceCheck)
+	{
+		LoggingUtil.Init();
+		TestSocket testSocket = null;
+		var ms = new BeamableMicroService(new TestSocketProvider(socket =>
+		{
+			testSocket = socket;
+			socket.AddStandardMessageHandlers()
+				.AddMessageHandler(
+					MessageMatcher
+						.WithReqId(-5)
+						.WithFrom(2),
+					MessageResponder.Success(new EmptyResponse()),
+					MessageFrequency.OnlyOnce())
+				.AddMessageHandler(
+					MessageMatcher
+						.WithReqId(1)
+						.WithStatus(200),
+					MessageResponder.NoResponse(),
+					MessageFrequency.OnlyOnce()
+				);
+		}));
+
+		await ms.Start<SimpleMicroservice>(new TestArgs());
+		Assert.IsTrue(ms.HasInitialized);
+
+		testSocket.SendToClient(ClientRequest.ClientCallableAsAdmin("micro_simple", "TestAssumeUser", 1, 0, 2, forceCheck));
+
+		// simulate shutdown event...
+		await ms.OnShutdown(this, null);
+		Assert.IsTrue(testSocket.AllMocksCalled());
+	}
+
+	[Test]
+	[NonParallelizable]
+	public async Task AssumeUserAsNormal_WithNoCheck()
+	{
+		LoggingUtil.Init();
+		TestSocket testSocket = null;
+		var ms = new BeamableMicroService(new TestSocketProvider(socket =>
+		{
+			testSocket = socket;
+			socket.AddStandardMessageHandlers()
+				.AddMessageHandler(
+					MessageMatcher
+						.WithReqId(-5)
+						.WithFrom(2),
+					MessageResponder.Success(new EmptyResponse()),
+					MessageFrequency.OnlyOnce())
+				.AddMessageHandler(
+					MessageMatcher
+						.WithReqId(1)
+						.WithStatus(200),
+					MessageResponder.NoResponse(),
+					MessageFrequency.OnlyOnce()
+				);
+		}));
+
+		await ms.Start<SimpleMicroservice>(new TestArgs());
+		Assert.IsTrue(ms.HasInitialized);
+
+		testSocket.SendToClient(ClientRequest.ClientCallable("micro_simple", "TestAssumeUser", 1, 0, 2, false));
+
+		// simulate shutdown event...
+		await ms.OnShutdown(this, null);
+		Assert.IsTrue(testSocket.AllMocksCalled());
+	}
+
+	[Test]
+	[NonParallelizable]
+	public async Task AssumeUserAsNormal_WithCheck()
+	{
+		LoggingUtil.Init();
+		TestSocket testSocket = null;
+		var ms = new BeamableMicroService(new TestSocketProvider(socket =>
+		{
+			testSocket = socket;
+			socket.AddStandardMessageHandlers()
+				.AddMessageHandler(
+					MessageMatcher
+						.WithReqId(1)
+						.WithStatus(403),
+					MessageResponder.NoResponse(),
+					MessageFrequency.OnlyOnce()
+				);
+		}));
+
+		await ms.Start<SimpleMicroservice>(new TestArgs());
+		Assert.IsTrue(ms.HasInitialized);
+
+		testSocket.SendToClient(ClientRequest.ClientCallable("micro_simple", "TestAssumeUser", 1, 0, 2, true));
+
+		// simulate shutdown event...
+		await ms.OnShutdown(this, null);
+		Assert.IsTrue(testSocket.AllMocksCalled());
+	}
+}
