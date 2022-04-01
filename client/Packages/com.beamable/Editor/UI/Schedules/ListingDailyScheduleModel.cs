091d7920029e691298c27edc7995901c2b7e4fdf
Artur Mankowski <arturmankowski22@gmail.com>
2022-04-01 13:59:00 +0200

    Small change

--- client/Packages/com.beamable/Editor/UI/Schedules/ListingDailyScheduleModel.cs
+++ client/Packages/com.beamable/Editor/UI/Schedules/ListingDailyScheduleModel.cs
@@ -44,19 +44,20 @@ namespace Beamable.Editor.Models.Schedules
 			ScheduleParser.PrepareGeneralData(newSchedule, _descriptionComponent.Value, _neverExpiresComponent.Value,
 				$"{_activeToDateComponent.SelectedDate}{_activeToHourComponent.SelectedHour}");
 
+			int fromHour = 0;
+			int toHour = 0;
+			int fromMinute = 0;
+			int toMinute = 0;
+			
 			if (!_allDayComponent.Value)
 			{
-				var fromHour = int.Parse(_periodFromHourComponent.Hour);
-				var toHour = int.Parse(_periodToHourComponent.Hour);
-				var fromMinute = int.Parse(_periodFromHourComponent.Minute);
-				var toMinute = int.Parse(_periodToHourComponent.Minute);
-				ScheduleParser.PrepareListingDailyModeData(newSchedule, fromHour, toHour, fromMinute, toMinute);
+				fromHour = int.Parse(_periodFromHourComponent.Hour);
+				toHour = int.Parse(_periodToHourComponent.Hour);
+				fromMinute = int.Parse(_periodFromHourComponent.Minute);
+				toMinute = int.Parse(_periodToHourComponent.Minute);
 			}
-			else
-			{
-				ScheduleParser.PrepareListingDailyModeData(newSchedule, 0, 0, 0, 0);
-			}
-			
+
+			ScheduleParser.PrepareListingDailyModeData(newSchedule, fromHour, toHour, fromMinute, toMinute);
 			return newSchedule;
 		}
 	}
