5ad72c8d64091d20d44e9b5ca884a4a14d60791a
LukaszwBeamable <LukaszwBeamable@users.noreply.github.com>
2022-04-04 14:25:03 +0000

    Formatting: [BEAM-2435] 1.1 Blocker - restore deleted classes (#870)

--- client/Packages/com.beamable/Runtime/Modules/AccountManagement/Scripts/AccountManagementConfiguration.cs
+++ client/Packages/com.beamable/Runtime/Modules/AccountManagement/Scripts/AccountManagementConfiguration.cs
@@ -113,13 +113,13 @@ namespace Beamable.AccountManagement
 				case AuthThirdParty.Google:
 					// On non-iOS platforms, just honor the Google checkbox.
 					return Google;
-				
+
 #if UNITY_EDITOR
 				case AuthThirdParty.Apple:
 					return Apple;	
-#endif	
+#endif
 #endif // UNITY_IOS
-				
+
 				default:
 					return false;
 			}
