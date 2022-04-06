0c4242c2a5ddffff7cdb383a9b64713399677ae5
Artur Mankowski <arturmankowski22@gmail.com>
2022-04-06 17:27:02 +0200

    So we need it

--- client/Packages/com.beamable.server/Editor/UI/Model/MicroserviceModel.cs
+++ client/Packages/com.beamable.server/Editor/UI/Model/MicroserviceModel.cs
@@ -6,6 +6,7 @@ using Beamable.Server.Editor.ManagerClient;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
