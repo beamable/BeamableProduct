928952207a292f7d65834da9c3757a6c1d459677
Łukasz Kundys <lukaszkundys@gmail.com>
2022-04-12 12:46:42 +0200

    Generic menu for style related commands (copy, delete)

--- client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/BussStyleCardVisualElement/BussSelectorLabelVisualElement/BussSelectorLabelVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/BussStyleCardVisualElement/BussSelectorLabelVisualElement/BussSelectorLabelVisualElement.cs
@@ -1,7 +1,10 @@
-﻿using Beamable.Editor.UI.Common;
+﻿using Beamable.Editor.Common;
+using Beamable.Editor.UI.Buss;
+using Beamable.Editor.UI.Common;
 using Beamable.UI.Buss;
 using System;
-using System.Text;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 #if UNITY_2018
 using UnityEngine.Experimental.UIElements;
@@ -18,35 +21,51 @@ namespace Beamable.Editor.UI.Components
 		private TextField _editableLabel;
 		private BussStyleRule _styleRule;
 		private BussStyleSheet _styleSheet;
+		private List<GenericMenuCommand> _commands;
 
-		public event Action OnChangeSubmit;
+		public event Action ChangeSubmit;
 
 		public BussSelectorLabelVisualElement() : base(
-			$"{BUSS_THEME_MANAGER_PATH}/BussStyleCardVisualElement/BussSelectorLabelVisualElement/BussSelectorLabelVisualElement.uss")
-		{ }
+			$"{BUSS_THEME_MANAGER_PATH}/BussStyleCardVisualElement/BussSelectorLabelVisualElement/BussSelectorLabelVisualElement.uss") { }
 
-		public void Setup(BussStyleRule styleRule, BussStyleSheet styleSheet, bool editMode)
+		public void Setup(BussStyleRule styleRule, BussStyleSheet styleSheet, bool editMode, List<GenericMenuCommand> commands)
 		{
 			base.Init();
 
 			_styleRule = styleRule;
 			_styleSheet = styleSheet;
+			_commands = commands;
 
 			if (!editMode)
 			{
 				TextElement textLabel = new TextElement();
 				textLabel.name = "styleId";
+				textLabel.text = styleRule.SelectorString;
+				Root.Add(textLabel);
 
-				StringBuilder label = new StringBuilder();
-				label.Append($"{styleRule.SelectorString} ({styleSheet.name})");
+				TextElement separator01 = new TextElement();
+				separator01.name = "separator";
+				separator01.text = "|";
+				Root.Add(separator01);
+
+				TextElement styleSheetLabel = new TextElement();
+				styleSheetLabel.name = "styleSheetLabel";
+				styleSheetLabel.text = $"{styleSheet.name}";
+				styleSheetLabel.RegisterCallback<MouseDownEvent>(OnStyleSheetClicked);
+				Root.Add(styleSheetLabel);
 
 				if (styleSheet.IsReadOnly)
 				{
-					label.Append(" - readonly");
-				}
+					TextElement separator02 = new TextElement();
+					separator02.name = "separator";
+					separator02.text = "|";
+					Root.Add(separator02);
 
-				textLabel.text = label.ToString();
-				Root.Add(textLabel);
+					TextElement readonlyLabel = new TextElement();
+					readonlyLabel.name = "readonlyLabel";
+					readonlyLabel.text = "readonly";
+					Root.Add(readonlyLabel);
+				}
 			}
 			else
 			{
@@ -58,11 +77,29 @@ namespace Beamable.Editor.UI.Components
 				{
 					if (evt.keyCode == KeyCode.Return)
 					{
-						OnChangeSubmit?.Invoke();
+						ChangeSubmit?.Invoke();
 					}
 				});
 				Root.Add(_editableLabel);
 			}
+		}
+
+		private void OnStyleSheetClicked(MouseDownEvent evt)
+		{
+			if (_styleSheet.IsReadOnly)
+			{
+				return;
+			}
+			
+			GenericMenu context = new GenericMenu();
+
+			foreach (GenericMenuCommand command in _commands)
+			{
+				GUIContent label = new GUIContent(command.Name);
+				context.AddItem(new GUIContent(label), false, () => { command.Invoke(); });
+			}
+
+			context.ShowAsContext();
 		}
 
 		protected override void OnDestroy()
