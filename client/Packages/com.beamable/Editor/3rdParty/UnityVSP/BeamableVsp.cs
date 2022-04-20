6f5d856904c8daba51dca54a0ce64dd4bb9ae907
Chris Hanna <cdhannaphone@gmail.com>
2022-04-20 09:10:03 -0400

    VSP - Feature/beam 2480 - support whats new  (#959)
    * version check
    
    * support whatsnew banner for vsp

--- client/Packages/com.beamable/Editor/3rdParty/UnityVSP/BeamableVsp.cs
+++ client/Packages/com.beamable/Editor/3rdParty/UnityVSP/BeamableVsp.cs
@@ -1,4 +1,8 @@
 using Beamable;
+using Beamable.Common;
+using Beamable.Common.Api;
+using Beamable.Editor;
+using System;
 
 namespace UnityEditor.VspAttribution.Beamable
 {
@@ -14,6 +18,37 @@ namespace UnityEditor.VspAttribution.Beamable
 				action,
 				"beamable",
 				cid);
+		}
+
+		public static async Promise<VspMetadata> GetLatestVersion()
+		{
+			var api = await EditorAPI.Instance;
+			var res = await api.Requester.ManualRequest<VspVersionResponse>(Method.GET, "http://beamable-vsp.beamable.com/vsp-meta.json");
+			var metadata = new VspMetadata {storeUrl = res.storeUrl};
+			try
+			{
+				PackageVersion version = res.version;
+				metadata.version = version;
+			}
+			catch
+			{
+				metadata.version = new PackageVersion(0,0,0);
+			}
+
+			return metadata;
+		}
+
+		[Serializable]
+		public class VspVersionResponse
+		{
+			public string version;
+			public string storeUrl;
+		}
+
+		public class VspMetadata
+		{
+			public PackageVersion version;
+			public string storeUrl;
 		}
 	}
 }
