664b89baa7c4f564c7f4b9692645b74002d5f17d
Chris Hanna <cdhannaphone@gmail.com>
2022-04-22 13:12:42 -0400

    beam 2458 - adds docs to IAuthApi, IAccessToken, AccessToken, and DocsUtils (#984)
    * adds docs to IAuthApi, IAccessToken, AccessToken, and DocsUtils
    
    * reference credentials from user class
    
    * reference credential methdos
    
    * auth pr comments

---
+++ client/Packages/com.beamable/Common/Runtime/DocUtils.cs
@@ -0,0 +1,12 @@
+namespace Beamable.Common.Docs
+{
+	/// <summary>
+	/// [img beamable-logo]: https://landen.imgix.net/7udgo2lvquge/assets/xgh89bz1.png?w=400 "Beamable Logo"
+	///
+	/// ![img beamable-logo]
+	/// </summary>
+	public class Logo
+	{
+
+	}
+}
