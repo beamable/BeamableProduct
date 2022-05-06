0aa6c1610cc4fb0f7f7d4f97989380cadd599ff4
Piotr Siuszko <piotr@beamable.com>
2022-05-06 14:31:58 +0200

    Print login info

--- client/Assets/GpgsSecondTest.cs
+++ client/Assets/GpgsSecondTest.cs
@@ -10,8 +10,21 @@ public class GpgsSecondTest : MonoBehaviour
 		var beamAPI = Beamable.BeamContext.Default;
 		await beamAPI.OnReady;
 		var GPGS = new SignInWithGPG();
+		GPGS.OnRequestServerSideAccessResult += HandleRequestServerSideAccessResult;
 		await Task.Delay(TimeSpan.FromSeconds(2f));
 		GooglePlayGames.OurUtils.Logger.DebugLogEnabled = true;
 		GPGS.Login();
+	}
+
+	private void HandleRequestServerSideAccessResult(bool success, string token)
+	{
+		if(!success)
+		{
+			throw new Exception("Cannot get server token from GPGS, please check if your configuration is correct.");
+		}
+		else
+		{
+			Debug.Log($"SUCCESS: {token}");
+		}
 	}
 }
