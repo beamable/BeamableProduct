e6539525d92aa71d303aadf671224c48968d3bce
Chris Hanna <cdhannaphone@gmail.com>
2022-05-11 16:24:17 -0400

    beam 2550 - doc links (#1066)

--- client/Packages/com.beamable/Common/Runtime/Api/CloudData/CloudDataApi.cs
+++ client/Packages/com.beamable/Common/Runtime/Api/CloudData/CloudDataApi.cs
@@ -77,7 +77,7 @@ namespace Beamable.Common.Api.CloudData
 	/// [img beamable-logo]: https://landen.imgix.net/7udgo2lvquge/assets/xgh89bz1.png?w=400 "Beamable Logo"
 	///
 	/// #### Related Links
-	/// - See the <a target="_blank" href="https://docs.beamable.com/docs/abtesting-feature">A/B Testing</a> feature documentation
+	/// - See the <a target="_blank" href="https://docs.beamable.com/docs/ab-testing-feature-overview">A/B Testing</a> feature documentation
 	/// - See Beamable.API script reference
 	///
 	/// ![img beamable-logo]
