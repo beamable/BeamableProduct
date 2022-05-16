0733804ac13576b1c4eda905b650573e628cb095
Artur Mankowski <arturmankowski22@gmail.com>
2022-05-16 12:24:43 +0200

    Added ScrollViewMode as parameter

--- client/Packages/com.beamable/Editor/UI/Common/UIElementsPolyfill.cs
+++ client/Packages/com.beamable/Editor/UI/Common/UIElementsPolyfill.cs
@@ -396,10 +396,10 @@ namespace UnityEngine.UIElements
       self.AppendAction(title, evt => callback(evt.eventInfo.mousePosition), enabled ? DropdownMenuAction.Status.Normal : DropdownMenuAction.Status.Disabled);
     }
     
-    public static void TryAddScrollViewAsMainElement(this VisualElement self)
+    public static void TryAddScrollViewAsMainElement(this VisualElement self, ScrollViewMode mode = ScrollViewMode.VerticalAndHorizontal)
     {
 #if UNITY_2021_1_OR_NEWER
-	    var scrollView = new ScrollView(ScrollViewMode.Vertical) {name = "main-scrollView"};
+	    var scrollView = new ScrollView(mode) {name = "main-scrollView"};
 	    scrollView.contentContainer.Add(self.Children().First());
 	    self.Add(scrollView);
 #endif
