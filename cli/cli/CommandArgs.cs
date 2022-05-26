6d187f850c519e2dfcf2b2e08ac5256f05f62a5e
Chris Hanna <cdhannaphone@gmail.com>
2022-05-26 17:01:17 -0400

    pr feedback

--- cli/cli/CommandArgs.cs
+++ cli/cli/CommandArgs.cs
@@ -8,3 +8,4 @@ public abstract class CommandArgs
 {
 	public bool Dryrun { get; set; }
 }
+
--- cli/cli/Program.cs
+++ cli/cli/Program.cs
@@ -2,13 +2,11 @@
 
 public static class Program
 {
-	public static int Main(string[] args)
+	public static async Task<int> Main(string[] args)
 	{
 		var app = new App();
 		app.Configure();
 		app.Build();
-		var task = app.RunAsync(args);
-		task.Wait();
-		return task.Result;
+		return await app.RunAsync(args);
 	}
 }
