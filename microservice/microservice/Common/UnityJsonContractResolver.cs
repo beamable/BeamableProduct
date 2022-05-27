20ad91c940eb730a40bcbe322a9c0c1b4626766b
Lukasz Wysk <lukaszw@beamable.com>
2022-05-27 19:47:41 +0200

    [BEAM-2561] namespace cleanup

--- microservice/microservice/Common/UnityJsonContractResolver.cs
+++ microservice/microservice/Common/UnityJsonContractResolver.cs
@@ -2,9 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Beamable.Serialization.SmallerJSON;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using UnityEngine;
 
