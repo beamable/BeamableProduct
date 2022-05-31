18c3abe39414529e4f72e84a819b80b49195f72e
Artur Mankowski <arturmankowski22@gmail.com>
2022-05-31 15:41:38 +0200

    [BEAM-2234] Create content manager using [right mouse click > create] should show sorted list (#1200)

--- client/Packages/com.beamable/CHANGELOG.md
+++ client/Packages/com.beamable/CHANGELOG.md
@@ -13,6 +13,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - `StopListeningForUpdates` and `ResumeListeningForUpdates` methods in `ContentService` to manual control content refresh on ClientManifest deployment.
 
+### Changed
+- List of available to create `ContentTypes` in `Content Manager` contextual menu is now ordered alphabetically
+
 ## [1.2.0]
 ### Added
 - Unity 2021 LTS support.
--- client/Packages/com.beamable/Editor/UI/Content/Components/ContentListVisualElement/ContentListVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Content/Components/ContentListVisualElement/ContentListVisualElement.cs
@@ -322,7 +322,9 @@ namespace Beamable.Editor.Content.Components
 		private void AddCreateItemMenu(ContextualMenuPopulateEvent evt)
 		{
 			var selectedTypes = Model.SelectedContentTypes;
-			var types = Model.GetContentTypes().ToList();
+			var types = Model.GetContentTypes()
+				.OrderBy(x => x.TypeName)
+				.ToList();
 			string currentCategoryName = "";
 
 			if (selectedTypes.FirstOrDefault() is ContentTypeTreeViewItem selectedType)
