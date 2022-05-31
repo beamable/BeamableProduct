4bf6d68e2fd939f20aab47ce2fddf882fde1df0e
Chris Hanna <cdhannaphone@gmail.com>
2022-05-31 17:59:35 -0400

    use preprelease on validation

--- build/docker/scripts/build-cli.sh
+++ build/docker/scripts/build-cli.sh
@@ -6,13 +6,11 @@ cd ./cli/cli
 echo "printing version"
 echo $VERSION_PREFIX
 echo $VERSION_SUFFIX
-export VERSION_SUFFIX=$(echo $VERSION_SUFFIX | tr . -)
-echo $VERSION_SUFFIX
-dotnet pack --configuration Release --version-suffix=$VERSION_SUFFIX /p:VersionPrefix=$VERSION_PREFIX
+dotnet pack --configuration Release --version-suffix=$(echo $VERSION_SUFFIX | tr . -) /p:VersionPrefix=$VERSION_PREFIX
 
 echo "Installing built package..."
 export PATH="$PATH:/root/.dotnet/tools"
-dotnet tool install --global --add-source ./nupkg/ beamable.tools
+dotnet tool install --global -prerelease --add-source ./nupkg/ beamable.tools
 
 echo "Checking built version..."
 beam --version #todo: is it possible to assert that the output must match the $VERSION string?
@@ -24,5 +22,5 @@ then
     echo "Not running due to dry run."
     exit $?
 else
-    dotnet nuget push ./nupkg/Beamable.Tools.${VERSION}.nupkg --source https://api.nuget.org/v3/index.json --api-key ${NUGET_TOOLS_KEY}
+    dotnet nuget push ./nupkg/Beamable.Tools.${VERSION_PREFIX}-${VERSION_SUFFIX}.nupkg --source https://api.nuget.org/v3/index.json --api-key ${NUGET_TOOLS_KEY}
 fi
