c3ff22f19613fb4694aabfa5c5df6ab7a357e42f
Chris Hanna <cdhannaphone@gmail.com>
2022-05-31 13:59:23 -0400

    forward dry run information to build script

--- build/docker/scripts/build-cli.sh
+++ build/docker/scripts/build-cli.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 echo "Packing..."
-pwd
 echo "printing version"
 echo $VERSION
 cd ./cli/cli
-echo "in cli"
-pwd
 dotnet pack --configuration Release /p:Version=$VERSION
 
-echo "okay, doing an ls"
-ls -a
-
 echo "Verifying..."
+export PATH="$PATH:/root/.dotnet/tools"
 dotnet tool install --global --add-source ./nupkg/ beamcli
 beam --version #todo: is it possible to assert that the output must match the $VERSION string?
 
-echo "Publishing..."
-dotnet nuget push ./bin/release/BeamCli.${VERSION}.nupkg --source https://api.nuget.org/v3/index.json --api-key test
+
+if [ "$DRY_RUN" == "true" ]
+then
+	echo "Not running due to dry run."
+else 
+    echo "Publishing..."
+    dotnet nuget push ./bin/release/BeamCli.${VERSION}.nupkg --source https://api.nuget.org/v3/index.json --api-key test
+fi
