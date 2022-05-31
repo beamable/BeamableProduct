2b0e87d8acdd54a1eb28be791d81126d05e2bb32
Chris Hanna <cdhannaphone@gmail.com>
2022-05-31 14:25:27 -0400

    cant use bash 4 I guess

--- build/docker/scripts/build-cli.sh
+++ build/docker/scripts/build-cli.sh
@@ -13,9 +13,9 @@ dotnet tool install --global --add-source ./nupkg/ beamcli
 echo "Checking built version..."
 beam --version #todo: is it possible to assert that the output must match the $VERSION string?
 
-if [ "${DRY_RUN,,}" == true ] ; then
+if [ "$DRY_RUN" == true ] ; then
 	echo "Not running due to dry run."
+    exit $?
 else 
-    echo "Publishing..."
     dotnet nuget push ./nupkg/BeamCli.${VERSION}.nupkg --source https://api.nuget.org/v3/index.json --api-key test
 fi
