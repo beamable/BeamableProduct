3e1d62bf48cc2bf2553d89ded42661b36f23de79
Chris Hanna <cdhannaphone@gmail.com>
2022-05-31 14:45:59 -0400

    updates to beamable.tools and marks as mit

--- build/docker/scripts/build-cli.sh
+++ build/docker/scripts/build-cli.sh
@@ -8,7 +8,7 @@ dotnet pack --configuration Release /p:Version=$VERSION
 
 echo "Installing built package..."
 export PATH="$PATH:/root/.dotnet/tools"
-dotnet tool install --global --add-source ./nupkg/ beamcli
+dotnet tool install --global --add-source ./nupkg/ beamable.tools
 
 echo "Checking built version..."
 beam --version #todo: is it possible to assert that the output must match the $VERSION string?
@@ -20,5 +20,5 @@ then
     echo "Not running due to dry run."
     exit $?
 else
-    dotnet nuget push ./nupkg/BeamCli.${VERSION}.nupkg --source https://api.nuget.org/v3/index.json --api-key test
+    dotnet nuget push ./nupkg/Beamable.Tools.${VERSION}.nupkg --source https://api.nuget.org/v3/index.json --api-key test
 fi
