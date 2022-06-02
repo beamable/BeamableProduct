dc719d702a4dad5be96ba4d1582f1a02036d0eee
Piotr Siuszko <piotr@beamable.com>
2022-06-02 22:56:08 +0200

    Basic request- start

--- cli/cli/CliToken.cs
+++ cli/cli/CliToken.cs
@@ -7,11 +7,11 @@ public class CliToken : IAccessToken
 {
 	public CliToken(TokenResponse response, string cid, string pid)
 	{
-		Token = response.access_token;
-		RefreshToken = response.refresh_token;
+		Token = response?.access_token ?? string.Empty;
+		RefreshToken = response?.refresh_token ?? string.Empty;
 		Cid = cid;
 		Pid = pid;
-		ExpiresAt = DateTime.FromFileTimeUtc(response.expires_in);
+		ExpiresAt = DateTime.FromFileTimeUtc(response?.expires_in ?? 0);
 	}
 	public string Token { get; }
 	public string RefreshToken { get; }
--- cli/cli/Services/CliRequester.cs
+++ cli/cli/Services/CliRequester.cs
@@ -7,21 +7,38 @@ namespace cli;
 
 public class CliRequester : IBeamableRequester
 {
-	public IAccessToken AccessToken { get; private set;  }
+	private const string BASE_PATH = "https://dev.api.beamable.com";
+	public IAccessToken AccessToken => Token;
+	private CliToken Token { get; set; }
 
 	public CliRequester()
 	{
+		Token = null;
 	}
 
 	public void UpdateToken(TokenResponse response, string cid, string pid) =>
-		AccessToken = new CliToken(response, cid, pid);
+		Token = new CliToken(response, cid, pid);
 	
 	public Promise<T> Request<T>(Method method, string uri, object body = null, bool includeAuthHeader = true, Func<string, T> parser = null,
 		bool useCache = false)
 	{
 		Console.WriteLine($"Would have sent a {method} to {uri} but not implemented yet");
 		// throw new NotImplementedException();
+		var client = new HttpClient();
+		client.DefaultRequestHeaders.Add("contentType", "application/json");
+		var request = new HttpRequestMessage(HttpMethod.Get, BASE_PATH + uri);
+		request.Headers.Add("contentType", "application/json");
+		if (body != null)
+		{
+			// request.Content = new 
+		}
 
+		if (includeAuthHeader && !string.IsNullOrWhiteSpace(Token?.Token))
+		{
+			request.Headers.Add("Authorization", $"Bearer {AccessToken.Token}");
+		}
+		var result = client.Send(request);
+		Console.WriteLine("RESULT: " + result.ToString());
 		return Promise<T>.Successful(default(T));
 	}
 
