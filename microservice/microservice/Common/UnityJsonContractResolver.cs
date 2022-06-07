ea6b1a8bc4eb2771c4159ff3a7c7a6a0bd03e41f
Lukasz Wysk <lukaszw@beamable.com>
2022-06-07 14:31:40 +0200

    BEAM-2561 cleanup

--- microservice/microservice/Common/UnityJsonContractResolver.cs
+++ microservice/microservice/Common/UnityJsonContractResolver.cs
@@ -165,8 +165,7 @@ namespace Beamable.Server.Common
                 var value = _baseProvider.GetValue(target);
                 
                 ((ISerializationCallbackReceiver) value)?.OnBeforeSerialize();
-
-                // temporary solution because i can't access to generic properties (even with Reflection) to serialize from SerializableDictionary<TKey, TValue> for request from inside of Microservice (like SendMail)
+                
                 var serializeDictMethod = value?.GetType().GetMethod("Serialize");
                 
                 if (serializeDictMethod != null)
