d6df6fa89c96e070e11ded3c6a4069543f4f892f
Tomasz Kaczmarz <tomasz@beamable.com>
2022-06-10 17:50:19 +0200

    Working on Party SDK

---
+++ client/Packages/com.beamable/Common/Runtime/Api/Parties/IPartyApi.cs
@@ -0,0 +1,18 @@
+﻿using Beamable.Common;
+using System.Collections.Generic;
+
+namespace Beamable.Experimental.Api.Parties
+{
+	public interface IPartyApi
+	{
+		Promise<PartyQueryResponse> FindParties();
+
+		Promise<Party> JoinParty(string partyId);
+
+		Promise<Party> GetParty(string partyId);
+
+		Promise LeaveParty(string partyId);
+
+		Promise KickPlayer(string lobbyId, string partyId);
+	}
+}
---
+++ client/Packages/com.beamable/Common/Runtime/Api/Parties/Party.cs
@@ -0,0 +1,17 @@
+﻿using Beamable.Common.Player;
+using System;
+
+namespace Beamable.Experimental.Api.Parties
+{
+	[Serializable]
+	public class Party : DefaultObservable
+	{
+		public string partyId;
+
+		public string restriction;
+
+		public string host;
+
+		public int maxPlayers;
+	}
+}
