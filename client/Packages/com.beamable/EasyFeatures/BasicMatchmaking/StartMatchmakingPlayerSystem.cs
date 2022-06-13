00d0fb05218d40ffe553a51f5cae21caf783184a
Lukasz Kundys <lukaszkundys@gmail.com>
2022-06-13 10:34:11 +0200

    Feature control reacting on some matchmaking callbacks

--- client/Packages/com.beamable/EasyFeatures/BasicMatchmaking/StartMatchmakingPlayerSystem.cs
+++ client/Packages/com.beamable/EasyFeatures/BasicMatchmaking/StartMatchmakingPlayerSystem.cs
@@ -1,17 +1,83 @@
-﻿using Beamable.Common.Content;
+﻿using Beamable.Common;
+using Beamable.Common.Content;
 using Beamable.EasyFeatures.Basicmatchmaking;
+using Beamable.Experimental.Api.Matchmaking;
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Beamable.EasyFeatures.BasicMatchmaking
 {
 	public class StartMatchmakingPlayerSystem : StartMatchmakingView.IDependencies
 	{
+		public event Action<MatchmakingState> OnStateChanged;
+		
+		public BeamContext BeamContext { get; }
+		
 		public bool IsVisible { get; set; }
 		public List<SimGameType> GameTypes { get; set; }
+		public int SelectedGameTypeIndex { get; set; }
+		public int TimeoutSeconds { get; set; }
+		public bool InProgress { get; set; }
 
-		public void Setup(List<SimGameType> gameTypes)
+		private MatchmakingState _matchmakingState;
+		public MatchmakingState MatchmakingState
+		{
+			get => _matchmakingState;
+			set
+			{
+				_matchmakingState = value;
+				OnStateChanged?.Invoke(_matchmakingState);
+			}
+		}
+
+		public StartMatchmakingPlayerSystem(BeamContext beamContext)
+		{
+			BeamContext = beamContext;
+		}
+		
+		public void Setup(List<SimGameType> gameTypes, int timeoutSeconds)
 		{
 			GameTypes = gameTypes;
+			SelectedGameTypeIndex = 0;
+			TimeoutSeconds = timeoutSeconds;
+			InProgress = false;
+		}
+
+		public async Promise StartMatchmaking()
+		{
+			InProgress = true;
+
+			MatchmakingHandle matchmakingHandle = await BeamContext.Api.Experimental.MatchmakingService.StartMatchmaking(
+				GameTypes[SelectedGameTypeIndex].Id, OnUpdate, OnReady, OnTimeout,
+				TimeSpan.FromSeconds(TimeoutSeconds));
+
+			RegisterMatchmakingHandle(matchmakingHandle);
+		}
+
+		private void RegisterMatchmakingHandle(MatchmakingHandle matchmakingHandle)
+		{
+			 MatchmakingState = matchmakingHandle.State;
+		}
+
+		private void OnUpdate(MatchmakingHandle matchmakingHandle)
+		{
+			RegisterMatchmakingHandle(matchmakingHandle);
+			Debug.Log("OnUpdate");
+		}
+
+		private void OnReady(MatchmakingHandle matchmakingHandle)
+		{
+			RegisterMatchmakingHandle(matchmakingHandle);
+			InProgress = false;
+			Debug.Log("OnReady");
+		}
+
+		private void OnTimeout(MatchmakingHandle matchmakingHandle)
+		{
+			RegisterMatchmakingHandle(matchmakingHandle);
+			InProgress = false;
+			Debug.Log("OnTimeout");
 		}
 	}
 }
--- client/Packages/com.beamable/EasyFeatures/BasicMatchmaking/StartMatchmakingView.cs
+++ client/Packages/com.beamable/EasyFeatures/BasicMatchmaking/StartMatchmakingView.cs
@@ -1,7 +1,13 @@
-﻿using Beamable.Common.Content;
+﻿using Beamable.Api;
+using Beamable.Common;
+using Beamable.Common.Content;
+using Beamable.EasyFeatures.Components;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace Beamable.EasyFeatures.Basicmatchmaking
 {
@@ -11,21 +17,82 @@ namespace Beamable.EasyFeatures.Basicmatchmaking
 		{
 			bool IsVisible { get; set; }
 			List<SimGameType> GameTypes { get; }
+			int SelectedGameTypeIndex { get; set; }
+			int TimeoutSeconds { get; set; }
+			bool InProgress { get; set; }
+			Promise StartMatchmaking();
+			
 		}
-		
+
 		[Header("View Configuration")]
 		public int EnrichOrder;
-		
+
+		[Header("Components")]
+		public MultiToggleComponent TypesToggle;
+
+		public Button StartMatchmakingButton;
+		public Button BackButton;
+
+		[Header("Callbacks")]
+		public UnityEvent OnStartMatchmakingRequestSent;
+		public UnityEvent OnStartMatchmakingResponseReceived;
+		public UnityEvent OnBackButtonClicked;
+
 		public Action<string> OnError;
 
 		public int GetEnrichOrder() => EnrichOrder;
 
+		protected IDependencies System;
+
 		public void EnrichWithContext(BeamContextGroup managedPlayers)
 		{
 			var ctx = managedPlayers.GetSinglePlayerContext();
-			var dependencies = ctx.ServiceProvider.GetService<IDependencies>();
+			System = ctx.ServiceProvider.GetService<IDependencies>();
+
+			gameObject.SetActive(System.IsVisible);
+
+			// Setting up all components
+			TypesToggle.Setup(System.GameTypes.Select(gameType => gameType.name).ToList(), OnGameTypeSelected,
+			                  System.SelectedGameTypeIndex);
+
+			StartMatchmakingButton.onClick.ReplaceOrAddListener(StartMatchmakingButtonClicked);
+			BackButton.onClick.ReplaceOrAddListener(() =>
+			{
+				OnBackButtonClicked?.Invoke();
+			});
+
+		}
+
+		private void OnGameTypeSelected(int optionId)
+		{
+			if (optionId == System.SelectedGameTypeIndex)
+			{
+				return;
+			}
+
+			System.SelectedGameTypeIndex = optionId;
+		}
+
+		private async void StartMatchmakingButtonClicked()
+		{
+			if (System.InProgress)
+			{
+				return;
+			}
 			
-			gameObject.SetActive(dependencies.IsVisible);
+			try
+			{
+				OnStartMatchmakingRequestSent?.Invoke();
+				await System.StartMatchmaking();
+			}
+			catch (Exception e)
+			{
+				if (e is PlatformRequesterException pre)
+				{
+					OnError?.Invoke(pre.Error.error);
+					System.InProgress = false;
+				}
+			}
 		}
 	}
 }
