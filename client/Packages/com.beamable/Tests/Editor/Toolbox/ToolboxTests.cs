12a6527c24798eb8e9a9a44980417a52da6b3e6a
roald-beamable <roald@beamable.com>
2022-06-17 21:12:27 +0800

    Remove some tests in ToolboxTests

--- client/Packages/com.beamable/Tests/Editor/Toolbox/ToolboxTests.cs
+++ client/Packages/com.beamable/Tests/Editor/Toolbox/ToolboxTests.cs
@@ -57,52 +57,5 @@ namespace Beamable.Editor.Tests.Toolbox
 			Debug.Log(text.value);
 			Assert.AreEqual("tag:flow", text.value);
 		}
-
-		//Test how many widgets in toolbox appears when NOT filtered
-		[Test]
-		public void TestEmptyFilterToolbox()
-		{
-			IDependencyBuilder builder = new DependencyBuilder();
-			builder.AddSingleton<IToolboxViewService, MockToolboxViewService>();
-
-			provider = builder.Build();
-
-			IToolboxViewService model = provider.GetService<IToolboxViewService>();
-
-			ToolboxContentListVisualElement toolboxContent = new ToolboxContentListVisualElement();
-			toolboxContent.Refresh(provider);
-
-			var x = model.GetFilteredWidgets().Count();
-			var filter = toolboxContent.Q("gridContainer");
-			var cnt = filter.childCount;
-
-			Debug.Log(cnt);
-			Assert.AreEqual(10, cnt);
-		}
-
-		//Test if dropdown visual elements pops up
-		[Test]
-		public void TestCategoryDropdownVisualElement()
-		{
-			IDependencyBuilder builder = new DependencyBuilder();
-			builder.AddSingleton<IToolboxViewService, MockToolboxViewService>();
-
-			provider = builder.Build();
-
-			IToolboxViewService model = provider.GetService<IToolboxViewService>();
-
-			ToolboxActionBarVisualElement tb = new ToolboxActionBarVisualElement();
-			tb.Refresh(provider);
-
-			Button button = tb.Q<Button>("CategoryButton");
-
-			CategoryDropdownVisualElement content = new CategoryDropdownVisualElement();
-			Rect popupWindowRect = BeamablePopupWindow.GetLowerLeftOfBounds(button.worldBound);
-			content.Model = model;
-			var wnd = BeamablePopupWindow.ShowDropdown("Tags", popupWindowRect, new Vector2(200, 250), content);
-
-
-			Assert.NotNull(wnd);
-		}
 	}
 }
