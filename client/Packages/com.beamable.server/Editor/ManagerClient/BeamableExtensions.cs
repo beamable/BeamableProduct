d3b6d4bcfefabd466ad363b5fa0127f67701ebea
Chris Hanna <cdhannaphone@gmail.com>
2022-06-29 10:55:08 -0400

    remove variable

--- client/Packages/com.beamable.server/Editor/ManagerClient/BeamableExtensions.cs
+++ client/Packages/com.beamable.server/Editor/ManagerClient/BeamableExtensions.cs
@@ -5,8 +5,6 @@ namespace Beamable.Server.Editor.ManagerClient
 {
 	public static class BeamableExtensions
 	{
-		private static MicroserviceManager _manager;
-
 		public static MicroserviceManager GetMicroserviceManager(this BeamEditorContext de)
 		{
 			return de.ServiceScope.GetService<MicroserviceManager>();
