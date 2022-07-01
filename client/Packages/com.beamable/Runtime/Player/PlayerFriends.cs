3696ac687ff470d2f771e1f0419a058567cfc0cd
Tomasz Kaczmarz <tomasz@beamable.com>
2022-07-01 17:55:37 +0200

    Added Friends layer to PSDK; Further implementation needed

---
+++ client/Packages/com.beamable/Runtime/Player/PlayerFriends.cs
@@ -0,0 +1,25 @@
+﻿using Beamable.Common;
+using Beamable.Common.Api.Social;
+using Beamable.Common.Player;
+using System;
+
+namespace Beamable.Player
+{
+	[Serializable]
+	public class PlayerFriends : AbsObservableReadonlyList<Friend>
+	{
+		private ISocialApi _socialApi;
+		
+		public PlayerFriends(ISocialApi socialApi)
+		{
+			_socialApi = socialApi;
+		}
+		
+		protected override async Promise PerformRefresh()
+		{
+			var friendsList = await _socialApi.Get();
+			
+			SetData(friendsList.friends);
+		}
+	}
+}
