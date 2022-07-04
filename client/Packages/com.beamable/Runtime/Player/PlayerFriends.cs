cf6f74bf9dcdb7efa7fb4464eeacf3a70534b5ce
Tomasz Kaczmarz <tomasz@beamable.com>
2022-07-04 20:50:12 +0200

    Added documentation and PlayerFriends init

--- client/Packages/com.beamable/Runtime/Player/PlayerFriends.cs
+++ client/Packages/com.beamable/Runtime/Player/PlayerFriends.cs
@@ -2,24 +2,62 @@
 using Beamable.Common.Api.Social;
 using Beamable.Common.Player;
 using System;
+using System.Collections.Generic;
 
 namespace Beamable.Player
 {
 	[Serializable]
-	public class PlayerFriends : AbsObservableReadonlyList<Friend>
+	public class PlayerFriends : Observable<SocialList>
 	{
+		/// <summary>
+		/// Use this to make sure the object is initialized.
+		/// </summary>
+		public Promise OnReady
+		{
+			get
+			{
+				if (IsAssigned)
+				{
+					return Promise.Success;
+				}
+
+				return Refresh();
+			}
+		}
+
+		/// <summary>
+		/// This is a list of players which user added as friends.
+		/// </summary>
+		public List<Friend> FriendsList => Value?.friends;
+		/// <summary>
+		/// This is a list of players which user blocked.
+		/// </summary>
+		public List<Common.Api.Social.Player> Blocked => Value?.blocked;
+
 		private ISocialApi _socialApi;
-		
+
 		public PlayerFriends(ISocialApi socialApi)
 		{
 			_socialApi = socialApi;
 		}
-		
+
 		protected override async Promise PerformRefresh()
 		{
-			var friendsList = await _socialApi.Get();
-			
-			SetData(friendsList.friends);
+			Value = await _socialApi.Get();
 		}
+
+		/// <summary>
+		/// Check if player with given id was blocked by the user.
+		/// </summary>
+		/// <param name="playerId">Id of the player to check.</param>
+		/// <returns>True if given player is blocked.</returns>
+		public bool IsBlocked(long playerId) => Value.IsBlocked(playerId);
+
+		/// <summary>
+		/// Check if player with given id was added to the user's friends list.
+		/// </summary>
+		/// <param name="playerId">Id of the player to check.</param>
+		/// <returns>True if given player is a friend.</returns>
+		public bool IsFriend(long playerId) => Value.IsFriend(playerId);
 	}
 }
