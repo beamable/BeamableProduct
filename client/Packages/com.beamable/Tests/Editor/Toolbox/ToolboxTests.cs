69d1dd9731712c484477f25a159212e0557ec063
roald-beamable <roald@beamable.com>
2022-07-08 01:44:02 +0800

    Fix toolbox tests by clearing query after testing

--- client/Packages/com.beamable/Tests/Editor/Toolbox/ToolboxTests.cs
+++ client/Packages/com.beamable/Tests/Editor/Toolbox/ToolboxTests.cs
@@ -27,6 +27,33 @@ namespace Beamable.Editor.Tests.Toolbox
 			builder.ReplaceSingleton<IToolboxViewService, MockToolboxViewService>();
 		}
 
+		//Test if ticking filter in tags will change the search bar value to tag:{tag}
+		[Test]
+		public void TestQueryTag()
+		{
+			IToolboxViewService model = Provider.GetService<IToolboxViewService>();
+
+			ToolboxActionBarVisualElement tbActionBar = new ToolboxActionBarVisualElement();
+			tbActionBar.Refresh(Provider);
+
+			model.SetQueryTag(WidgetTags.FLOW, true);
+
+			var search = tbActionBar.Q<SearchBarVisualElement>();
+			TextField text = search.Q<TextField>();
+
+			var w = model.GetFilteredWidgets();
+
+			foreach (var i in w)
+			{
+				Debug.Log(i.Name);
+			}
+
+			Debug.Log(text.value);
+			Assert.AreEqual("tag:flow", text.value);
+
+			model.SetQuery(string.Empty);
+		}
+
 		//Test if setting search bar to "layout:landscape" will filter toolbox widgets and tick landscape in layout dropdown
 		[Test]
 		public void LayoutSearchbarLandscapeTest()
@@ -60,9 +87,16 @@ namespace Beamable.Editor.Tests.Toolbox
 			Debug.Log("Number of Widgets: " + x.Count());
 			Debug.Log(search.Value);
 
+			string searchQuery = search.Value;
+
+			//Set Search query to blank
+			search.SetValueWithoutNotify(string.Empty);
+
 			Assert.AreEqual(true, drp[1].Q<Toggle>().value);
-			Assert.AreEqual("layout:landscape", search.Value);
+			Assert.AreEqual("layout:landscape", searchQuery);
 			Assert.AreEqual(4, x.Count());
+
+			model.SetQuery(string.Empty);
 		}
 
 		//Test if tag filter toolbox widgets
@@ -87,6 +121,8 @@ namespace Beamable.Editor.Tests.Toolbox
 			var cnt = filter.childCount;
 
 			Assert.AreEqual(8, cnt);
+
+			model.SetQuery(string.Empty);
 		}
 	}
 }
