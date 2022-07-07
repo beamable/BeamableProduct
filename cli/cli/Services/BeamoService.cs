e42f032618825f276600a8a38e196a647ffb750e
Piotr Siuszko <piotr@beamable.com>
2022-07-07 10:40:10 +0200

    Start work on Beamo service

---
+++ cli/cli/Services/BeamoService.cs
@@ -0,0 +1,113 @@
+﻿using Beamable.Common;
+using Beamable.Common.Api;
+
+namespace cli;
+
+public class BeamoService { 
+	public const string SERVICE = "/basic/beamo";
+	public CliRequester Requester { get; }
+	
+	public BeamoService(CliRequester requester)
+	{
+		Requester = requester;
+	}
+	
+	public Promise<ServiceManifest> GetCurrentManifest()
+	{
+		return Requester.Request<GetManifestResponse>(Method.GET, $"{SERVICE}/manifest/current", "{}")
+		                .Map(res => res.manifest)
+		                .RecoverFrom40x(ex => new ServiceManifest());
+	}
+}
+
+[System.Serializable]
+public class GetManifestResponse
+{
+	public ServiceManifest manifest;
+}
+
+[System.Serializable]
+public class GetManifestsResponse
+{
+	public List<ServiceManifest> manifests;
+}
+
+[System.Serializable]
+public class PostManifestRequest
+{
+	public string comments;
+	public List<ServiceReference> manifest;
+	public List<ServiceStorageReference> storageReferences;
+}
+
+[System.Serializable]
+public class ServiceManifest
+{
+	public long id;
+	public long created;
+	public List<ServiceReference> manifest = new List<ServiceReference>();
+	public List<ServiceStorageReference> storageReference = new List<ServiceStorageReference>();
+	public long createdByAccountId;
+	public string comments;
+}
+
+[System.Serializable]
+public class ServiceReference
+{
+	public string serviceName;
+	public string checksum;
+	public bool enabled;
+	public string imageId;
+	public string templateId;
+	public string comments;
+	public List<ServiceDependency> dependencies;
+	public long containerHealthCheckPort = 6565;
+}
+
+[System.Serializable]
+public class ServiceStorageReference
+{
+	public string id;
+	public string storageType;
+	public bool enabled;
+	public string templateId;
+	public string checksum;
+}
+
+[System.Serializable]
+public class ServiceDependency
+{
+	public string storageType;
+	public string id;
+}
+
+[System.Serializable]
+public class GetStatusResponse
+{
+	public bool isCurrent;
+	public List<ServiceStatus> services;
+}
+
+[System.Serializable]
+public class ServiceStatus
+{
+	public string serviceName;
+	public string imageId;
+	public bool running;
+	public bool isCurrent;
+}
+
+[System.Serializable]
+public class GetLogsResponse
+{
+	public string serviceName;
+	public List<LogMessage> logs;
+}
+
+[System.Serializable]
+public class LogMessage
+{
+	public string level;
+	public long timestamp;
+	public string message;
+}
