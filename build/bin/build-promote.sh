21e227f90c7c2402855e58ae20a79b34fca613d8
Chris Hanna <cdhannaphone@gmail.com>
2022-07-12 17:20:00 -0400

    revert ci to not use buildx

--- build/bin/build-promote.sh
+++ build/bin/build-promote.sh
@@ -24,8 +24,7 @@ export REMOTE_REPO_TAG=beamableinc/${LOCAL_REPO_TAG}
 
 echo "Building Microservice base image..."
 docker --version
-docker buildx ls
-docker build -t ${LOCAL_REPO_TAG} ../microservice/microservice --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --build-arg BEAMABLE_SDK_VERSION=${VERSION:-0.0.0}
+docker build -t ${LOCAL_REPO_TAG} ../microservice/microservice --build-arg BEAMABLE_SDK_VERSION=${VERSION:-0.0.0}
 
 echo "Pushing Microservice base image..."
 docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}
