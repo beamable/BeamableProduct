57f7b33949ea74e0805e026ed43489e7ec020da4
Chris Hanna <cdhannaphone@gmail.com>
2022-07-12 15:02:56 -0400

    stupid namespace

--- client/Packages/com.beamable.server/SharedRuntime/Api/Chat/IMicroserviceChatApi.cs
+++ client/Packages/com.beamable.server/SharedRuntime/Api/Chat/IMicroserviceChatApi.cs
@@ -3,11 +3,12 @@ using Beamable.Common.Api;
 using Beamable.Experimental.Api.Chat;
 using System.Collections.Generic;
 
-namespace Beamable.Server.Api.Chat;
-
-/// <summary>
-/// The chat API for Microservice usage
-/// </summary>
-public interface IMicroserviceChatApi : IChatApi
+namespace Beamable.Server.Api.Chat
 {
+
+	/// <summary>
+	/// The chat API for Microservice usage
+	/// </summary>
+	public interface IMicroserviceChatApi : IChatApi { }
+
 }
