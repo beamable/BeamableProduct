dc902072fe1e15b024d3708e699af51b32a8f5a1
Chris Hanna <cdhannaphone@gmail.com>
2022-07-13 13:18:08 -0400

    multi-arch build

--- build/bin/build-promote.sh
+++ build/bin/build-promote.sh
@@ -23,8 +23,7 @@ export LOCAL_REPO_TAG=beamservice:${BEAMSERVICE_TAG}
 export REMOTE_REPO_TAG=beamableinc/${LOCAL_REPO_TAG}
 
 echo "Building Microservice base image..."
-docker --version
-docker build -t ${LOCAL_REPO_TAG} ../microservice/microservice --build-arg BEAMABLE_SDK_VERSION=${VERSION:-0.0.0}
+docker-buildx build --platform linux/arm64,linux/amd64,linux/arm/v8 -t ${LOCAL_REPO_TAG} ../microservice/microservice --build-arg BEAMABLE_SDK_VERSION=${VERSION:-0.0.0}
 
 echo "Pushing Microservice base image..."
 docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}
