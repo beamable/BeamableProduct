1287d4b73c0f6e0ba6da1008ac2d4f99cff51fb4
Chris Hanna <cdhannaphone@gmail.com>
2022-07-13 13:40:01 -0400

    use beamable builder

--- build/bin/build-promote.sh
+++ build/bin/build-promote.sh
@@ -23,7 +23,8 @@ export LOCAL_REPO_TAG=beamservice:${BEAMSERVICE_TAG}
 export REMOTE_REPO_TAG=beamableinc/${LOCAL_REPO_TAG}
 
 echo "Building Microservice base image..."
-/usr/libexec/docker/cli-plugins/docker-buildx build --platform linux/arm64,linux/amd64,linux/arm/v8 -t ${LOCAL_REPO_TAG} ../microservice/microservice --build-arg BEAMABLE_SDK_VERSION=${VERSION:-0.0.0}
+whoami
+/usr/libexec/docker/cli-plugins/docker-buildx build --builder beamable-builder --platform linux/arm64,linux/amd64,linux/arm/v8 -t ${LOCAL_REPO_TAG} ../microservice/microservice --build-arg BEAMABLE_SDK_VERSION=${VERSION:-0.0.0}
 
 echo "Pushing Microservice base image..."
 docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}
