9b96e2a91d4b3a7aa886d854f0e23b617463d5a4
Lukasz Wysk <lukaszw@beamable.com>
2022-07-21 18:46:16 +0200

    [BEAM-2782]

--- client/Packages/com.beamable/Editor/BeamEditor.cs
+++ client/Packages/com.beamable/Editor/BeamEditor.cs
@@ -563,7 +563,32 @@ namespace Beamable
 
 		public async Promise<Unit> LoginCustomer(string aliasOrCid, string email, string password)
 		{
-			var res = await ServiceScope.GetService<AliasService>().Resolve(aliasOrCid);
+			var requester = ServiceScope.GetService<PlatformRequester>();
+			AliasResolve res = null;
+			
+			if (!string.IsNullOrEmpty(requester.Pid)) // check is cached realm archived
+			{
+				try
+				{
+					res = await ServiceScope.GetService<AliasService>().Resolve(aliasOrCid);
+				}
+				catch (Exception ex)
+				{
+					if (ex is RequesterException err && err.Status == 400)
+					{
+						requester.Cid = string.Empty;
+						requester.Pid = string.Empty;
+						CurrentRealm = null;
+					
+						res = await ServiceScope.GetService<AliasService>().Resolve(aliasOrCid);
+					}
+					else
+					{
+						throw ex;
+					}
+				};
+			}
+			
 			var alias = res.Alias.GetOrElse("");
 			var cid = res.Cid.GetOrThrow();
 
@@ -616,7 +641,7 @@ namespace Beamable
 				if (pid == null)
 				{
 					var realms = await realmService.GetRealms(games.First());
-					realm = realms.First();
+					realm = realms.First(rv => !rv.Archived);
 				}
 				else
 					realm = (await realmService.GetRealms(pid)).First(rv => rv.Pid == pid);
