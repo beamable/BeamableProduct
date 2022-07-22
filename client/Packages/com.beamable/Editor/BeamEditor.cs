d8edea948fc81223d0afcecb6e9c0e2c75203638
Lukasz Wysk <lukaszw@beamable.com>
2022-07-22 13:28:46 +0200

    [BEAM-2782] tweaks

--- client/Packages/com.beamable/Editor/BeamEditor.cs
+++ client/Packages/com.beamable/Editor/BeamEditor.cs
@@ -563,31 +563,7 @@ namespace Beamable
 
 		public async Promise<Unit> LoginCustomer(string aliasOrCid, string email, string password)
 		{
-			var requester = ServiceScope.GetService<PlatformRequester>();
-			AliasResolve res = null;
-			
-			if (!string.IsNullOrEmpty(requester.Pid)) // check is cached realm archived
-			{
-				try
-				{
-					res = await ServiceScope.GetService<AliasService>().Resolve(aliasOrCid);
-				}
-				catch (Exception ex)
-				{
-					if (ex is RequesterException err && err.Status == 400)
-					{
-						requester.Cid = string.Empty;
-						requester.Pid = string.Empty;
-						CurrentRealm = null;
-					
-						res = await ServiceScope.GetService<AliasService>().Resolve(aliasOrCid);
-					}
-					else
-					{
-						throw ex;
-					}
-				};
-			}
+			AliasResolve res = await GetAliasResolve(aliasOrCid);
 			
 			var alias = res.Alias.GetOrElse("");
 			var cid = res.Cid.GetOrThrow();
@@ -687,7 +663,7 @@ namespace Beamable
 				return (TData)formatter.Deserialize(stream);
 			}
 		}
-
+		
 		private static string SerializeToString<TData>(TData settings)
 		{
 			using (var stream = new MemoryStream())
@@ -773,6 +749,41 @@ namespace Beamable
 				}
 				else throw;
 			}
+		}
+		
+		private async Promise<AliasResolve> GetAliasResolve(string aliasOrCid)
+		{
+			var requester = ServiceScope.GetService<PlatformRequester>();
+			AliasResolve res = null;
+			
+			if (!string.IsNullOrEmpty(requester.Pid)) // check is cached realm archived
+			{
+				try
+				{
+					res = await ServiceScope.GetService<AliasService>().Resolve(aliasOrCid);
+				}
+				catch (Exception ex)
+				{
+					if (ex is RequesterException err && err.Status == 400)
+					{
+						requester.Cid = string.Empty;
+						requester.Pid = string.Empty;
+						CurrentRealm = null;
+					
+						res = await ServiceScope.GetService<AliasService>().Resolve(aliasOrCid);
+					}
+					else
+					{
+						throw ex;
+					}
+				};
+			}
+			else
+			{
+				res = await ServiceScope.GetService<AliasService>().Resolve(aliasOrCid);
+			}
+
+			return res;
 		}
 
 		public void Logout()
