17b8d9270488bf83c79696cb52c7df5628d674a1
roald-beamable <104795527+roald-beamable@users.noreply.github.com>
2022-07-27 21:02:22 +0800

    Beam 2793 - Toolbox Portal Test (#1344)
    * Add websitehook files
    
    * Add portal button test
    
    * Add test util and fix portal test
    
    * Remove extra code
    
    * Add BeamEditorContext in VisualElement and instantiate vars in context in base test class
    
    * Fix cid bug and remove comments
    
    * remove contextleftclickevent script
    
    * Remove duplicate Context
    
    * Add fix for 2021 version
    
    * Fix 2020 version not being included

---
+++ client/Packages/com.beamable/Tests/Editor/MockWebsiteHook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Beamable.Common;
+using Beamable.Editor.Toolbox.UI.Components;
+using Beamable.Editor.UI.Components;
+using Beamable.Editor.Toolbox.Models;
+
+namespace Beamable.Editor.Tests
+{
+	public class MockWebsiteHook : IWebsiteHook
+	{
+		public string Url { get; set; }
+		public void OpenUrl(string url)
+		{
+			Url = url;
+		}
+	}
+}
+
---
+++ client/Packages/com.beamable/Tests/Editor/TestUtil.cs
@@ -0,0 +1,68 @@
+﻿using Beamable.Common.Dependencies;
+using Beamable.Editor.Toolbox.Components;
+using Beamable.Editor.Toolbox.Models;
+using Beamable.Editor.UI;
+using Beamable.Editor.UI.Components;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEditor.Events;
+using UnityEditor.EventSystems;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+static class TestUtil
+{
+	public class TestEditorWindow : EditorWindow
+	{
+		public VisualElement Root => this.GetRootVisualContainer();
+		public void Mount(VisualElement element)
+		{
+			;
+			Root.Clear();
+			Root.Add(element);
+		}
+	}
+
+	/// <summary>
+	/// The root visual element for your test needs to be mounted onto a real IMGUI window.
+	/// Call this method with the root element for your test.
+	/// Before sending any events, you'll need to yield a Unity render frame, so that Unity has time to force the layout
+	/// </summary>
+	/// <param name="elem"></param>
+	/// <returns>An EditorWindow that you must explicitly call `Close()` on at the end of the test.</returns>
+	public static TestEditorWindow MountForTest(this VisualElement elem)
+	{
+		var window = EditorWindow.GetWindow<TestEditorWindow>();
+		window.Mount(elem);
+		return window;
+	}
+
+	/// <summary>
+	/// In order to trick a Button into receiving a click, we need to send both a MouseDown and MouseUp event. This utility method makes that a
+	/// little easier.
+	/// </summary>
+	/// <param name="button"></param>
+	public static void SendTestClick(this Button button)
+	{
+#if UNITY_2019 || UNITY_2020
+		using (var evt = MouseDownEvent.GetPooled(button.worldBound.position + Vector2.one, 0, 1, Vector2.zero, EventModifiers.None))
+		{
+			button.SendEvent(evt);
+		}
+		using (var evt = MouseUpEvent.GetPooled(button.worldBound.position + Vector2.one, 0, 1, Vector2.zero, EventModifiers.None))
+		{
+			button.SendEvent(evt);
+		}
+#elif UNITY_2021_1_OR_NEWER
+		using (var evt = new NavigationSubmitEvent() { target = button })
+			button.SendEvent(evt);
+#endif
+	}
+}
