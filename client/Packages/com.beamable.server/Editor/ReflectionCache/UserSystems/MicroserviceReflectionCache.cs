ba0932c843c3b1284b1f341dffa1e2fe377a58e0
cdhanna <cdhanna@users.noreply.github.com>
2022-08-08 12:58:18 +0000

    Formatting: beam 2896 - bind mount with spaces (#1458)

--- client/Packages/com.beamable.server/Editor/ReflectionCache/UserSystems/MicroserviceReflectionCache.cs
+++ client/Packages/com.beamable.server/Editor/ReflectionCache/UserSystems/MicroserviceReflectionCache.cs
@@ -640,7 +640,7 @@ namespace Beamable.Server.Editor
 						Timestamp = LogMessage.GetTimeDisplay(DateTime.Now),
 						Message = $"Comitting storage=[{storage.StorageName}]"
 					});
-					var storageDesc = new StorageObjectDescriptor {Name = storage.StorageName};
+					var storageDesc = new StorageObjectDescriptor { Name = storage.StorageName };
 					onServiceDeployed?.Invoke(storageDesc);
 					OnServiceDeployStatusChanged?.Invoke(storageDesc, ServicePublishState.Published);
 				}
@@ -649,7 +649,7 @@ namespace Beamable.Server.Editor
 				var remoteOnlyServices = model.Services.Where(s => !nameToImageDetails.ContainsKey(s.Key)).ToList();
 				foreach (var remoteOnly in remoteOnlyServices)
 				{
-					var desc = new MicroserviceDescriptor {Name = remoteOnly.Key};
+					var desc = new MicroserviceDescriptor { Name = remoteOnly.Key };
 					onServiceDeployed?.Invoke(desc);
 					OnServiceDeployStatusChanged?.Invoke(desc, ServicePublishState.Published);
 				}
