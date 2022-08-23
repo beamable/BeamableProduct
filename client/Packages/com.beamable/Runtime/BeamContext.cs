d5af9e5398c772f7d2bf1da89dd5f8794891cef6
Pedro Rauiz Estigarribia Gestal <pedro@beamable.com>
2022-08-23 15:46:27 -0300

    fixed issue with ChangeAuthorizedUser that cause the BeamContext to enter a bad state with respect to its MonoBehaviour systems (coroutine/pubnub)

--- client/Packages/com.beamable/Runtime/BeamContext.cs
+++ client/Packages/com.beamable/Runtime/BeamContext.cs
@@ -264,6 +264,7 @@ namespace Beamable
 				OnUserLoggingOut?.Invoke(AuthorizedUser);
 			}
 
+			await Stop();
 			await SaveToken(token); // set the token so that it gets picked up on the next initialization
 			var ctx = Instantiate(_behaviour, PlayerCode);
 
@@ -771,6 +772,8 @@ namespace Beamable
 			if (GameObject)
 			{
 				UnityEngine.Object.Destroy(GameObject);
+				_behaviour = null;
+				_gob = null;
 			}
 
 
