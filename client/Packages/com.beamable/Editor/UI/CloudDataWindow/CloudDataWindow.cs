2c752544421c48022443d0bd4fe850ccd752c9e3
Piotr Siuszko <siuszko@zoho.com>
2022-09-01 19:56:43 +0200

    Cloud Data Window

---
+++ client/Packages/com.beamable/Editor/UI/CloudDataWindow/CloudDataWindow.cs
@@ -0,0 +1,93 @@
+﻿using Beamable.Common;
+using Beamable.Common.Api.CloudData;
+using Beamable.Editor.Toolbox.UI;
+using Beamable.Editor.UI;
+using Beamable.Editor.UI.CloudDataWindow.Models;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+#if UNITY_2018
+using UnityEngine.Experimental.UIElements;
+using UnityEditor.Experimental.UIElements;
+#elif UNITY_2019_1_OR_NEWER
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+#endif
+using static Beamable.Common.Constants;
+
+namespace Beamable.Editor.UI.CloudDataWindow
+{
+	public class CloudDataWindow : BeamEditorWindow<CloudDataWindow>
+	{
+		private VisualElement _windowRoot;
+		private CloudDataViewService _model;
+		static CloudDataWindow()
+		{
+			WindowDefaultConfig = new BeamEditorWindowInitConfig()
+			{
+				Title = "Cloud Data Window",
+				DockPreferenceTypeName = typeof(SceneView).AssemblyQualifiedName,
+				FocusOnShow = false,
+				RequireLoggedUser = true,
+			};
+		}
+
+		[MenuItem(
+			Constants.MenuItems.Windows.Paths.MENU_ITEM_PATH_WINDOW_BEAMABLE + "/" +
+			Commons.OPEN + " " +
+			"Cloud Data Window",
+			priority = MenuItems.Windows.Orders.MENU_ITEM_PATH_WINDOW_PRIORITY_1
+		)]
+		public static async void Init() => await GetFullyInitializedWindow();
+		public static async void Init(BeamEditorWindowInitConfig initParameters) => await GetFullyInitializedWindow(initParameters);
+	
+		protected override void Build()
+		{
+
+			// Refresh if/when the user logs-in or logs-out while this window is open
+			ActiveContext.OnUserChange += _ => BuildWithContext();
+
+			_model = new CloudDataViewService();
+			_model.OnAvailableMetaDataDownloaded += HandleMetaDataDownloaded;
+			_model.Init();
+			// _model = ActiveContext.ServiceScope.GetService<IToolboxViewService>();
+			//
+			// // Force refresh to build the initial window
+			// _model?.Destroy();
+			//
+			// _model.UseDefaultWidgetSource();
+			// _model.Initialize();
+
+			SetForContent();
+		}
+
+		private void HandleMetaDataDownloaded(Dictionary<CloudMetaData, string> cloudMetaDatas)
+		{
+			var s = _windowRoot.Q("window-main");
+			s.Clear();
+			foreach (KeyValuePair<CloudMetaData, string> metaData in cloudMetaDatas)
+			{
+				var label = new Label(metaData.Key.@ref);
+				label.AddToClassList("refName");
+				var content = new Label(metaData.Value);
+				content.AddToClassList("content");
+				
+				s.Add(label);
+				s.Add(content);
+			}
+		}
+
+		private void SetForContent()
+		{
+			var root = this.GetRootVisualContainer();
+			root.Clear();
+			var uiAsset =
+				AssetDatabase.LoadAssetAtPath<VisualTreeAsset>($"{Directories.BEAMABLE_PACKAGE_EDITOR_UI}/{nameof(CloudDataWindow)}/{nameof(CloudDataWindow)}.uxml");
+			_windowRoot = uiAsset.CloneTree();
+			_windowRoot.AddStyleSheet($"{Directories.BEAMABLE_PACKAGE_EDITOR_UI}/{nameof(CloudDataWindow)}/{nameof(CloudDataWindow)}.uss");
+			_windowRoot.name = nameof(_windowRoot);
+			root.Add(_windowRoot);
+			_windowRoot.TryAddScrollViewAsMainElement();
+		}
+	}
+}
---
+++ client/Packages/com.beamable/Editor/UI/CloudDataWindow/Models/CloudDataViewService.cs
@@ -0,0 +1,43 @@
+﻿using Beamable.Api.CloudData;
+using Beamable.Common.Api;
+using Beamable.Common.Api.CloudData;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Beamable.Editor.UI.CloudDataWindow.Models
+{
+	public class CloudDataViewService
+	{
+		public event Action<Dictionary<CloudMetaData,string>> OnAvailableMetaDataDownloaded;
+		private List<CloudMetaData> _cloudMeta;
+		private Dictionary<CloudMetaData,string> _cloudMetaContent;
+		public void Init()
+		{
+			var api = BeamEditorContext.Default;
+			Debug.Log($"<b>[CloudDataViewService]</b> Init");
+			api.Requester.Request<GetCloudDataManifestResponse>(
+				Method.GET,
+				"/basic/cloud/meta"
+			).Then(response =>
+			{
+				_cloudMeta = response.meta;
+				_cloudMetaContent = new Dictionary<CloudMetaData, string>();
+				Debug.Log($"<b>[CloudDataViewService]</b> {response.result}: {response.meta?.Count}");
+				foreach (CloudMetaData metaData in _cloudMeta)
+				{
+					api.Requester.Request(Method.GET,
+					                      $"https://{metaData.uri}", parser: s => s).Then(s =>
+					{
+						_cloudMetaContent.Add(metaData, s);
+						if(_cloudMetaContent.Count == _cloudMeta.Count)
+						{
+							OnAvailableMetaDataDownloaded?.Invoke(_cloudMetaContent);
+						}
+							
+					});
+				}
+			});
+		}
+	}
+}
