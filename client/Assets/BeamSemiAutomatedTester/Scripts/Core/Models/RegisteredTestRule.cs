e963f0775e32f9df754de1c13bc627a79e9db11c
Artur Mankowski <arturmankowski22@gmail.com>
2022-09-05 19:14:44 +0200

    [BEAM-2608] Beam Semi Automated Tester (#1332)

---
+++ client/Assets/BeamSemiAutomatedTester/Scripts/Core/Models/RegisteredTestRule.cs
@@ -0,0 +1,52 @@
+﻿using Beamable.BSAT.Core.Models.Descriptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Beamable.BSAT.Core.Models
+{
+	[Serializable]
+	public class RegisteredTestRule
+	{
+		public event Action OnTestResultChanged;
+		public TestRuleDescriptor TestRuleDescriptor => _testRuleDescriptor;
+		public string TestMethodName => _testMethodName;
+		public int Order => _order;
+
+		public TestResult TestResult => _testResult = RegisteredTestRuleMethods.Any(x => x.TestResult == TestResult.Failed) ? TestResult.Failed :
+				RegisteredTestRuleMethods.All(x => x.TestResult == TestResult.Passed) ? TestResult.Passed :
+				TestResult.NotSet;
+		
+		public List<RegisteredTestRuleMethod> RegisteredTestRuleMethods => _registeredTestRuleMethods;
+
+		[SerializeField] private string _testMethodName;
+		[SerializeField] private int _order;
+		[SerializeField] private TestResult _testResult;
+		[SerializeField, HideInInspector] TestRuleDescriptor _testRuleDescriptor;
+		[SerializeField] private List<RegisteredTestRuleMethod> _registeredTestRuleMethods = new List<RegisteredTestRuleMethod>();
+
+		public RegisteredTestRule(string testMethodName, int order, TestRuleDescriptor testRuleDescriptor)
+		{
+			_testMethodName = testMethodName;
+			_order = order;
+			_testRuleDescriptor = testRuleDescriptor;
+		}
+		public void Reset()
+		{
+			foreach (var registeredMethodTest in RegisteredTestRuleMethods)
+				registeredMethodTest.Reset();
+		}
+
+		public void SetupEvents()
+		{
+			foreach (var registeredTestRuleMethod in _registeredTestRuleMethods)
+			{
+				registeredTestRuleMethod.OnTestResultChanged -= HandleTestResultChange;
+				registeredTestRuleMethod.OnTestResultChanged += HandleTestResultChange;
+			}
+		}
+		private void HandleTestResultChange()
+			=> OnTestResultChanged?.Invoke();
+	}
+}
