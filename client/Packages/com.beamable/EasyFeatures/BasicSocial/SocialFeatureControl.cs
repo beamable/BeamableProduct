75c168961db6a2a71a24b7b14134fa18096d12b6
Tomasz Kaczmarz <tomasz@beamable.com>
2022-09-19 20:00:07 +0200

    Created basic feature control

---
+++ client/Packages/com.beamable/EasyFeatures/BasicSocial/SocialFeatureControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Beamable.EasyFeatures.BasicSocial
+{
+	public class SocialFeatureControl : MonoBehaviour, IBeamableFeatureControl
+	{
+		public IEnumerable<BeamableViewGroup> ManagedViewGroups { get; }
+
+		[SerializeField]
+		private bool _runOnEnable = true;
+		public bool RunOnEnable
+		{
+			get => _runOnEnable;
+			set => _runOnEnable = value;
+		}
+
+		public BeamableViewGroup ViewGroup;
+
+		protected BeamContext Context;
+		
+		public void OnEnable()
+		{
+			ViewGroup.RebuildManagedViews();
+
+			if (!_runOnEnable)
+			{
+				return;
+			}
+			
+			Run();
+		}
+
+		public async void Run()
+		{
+			await ViewGroup.RebuildPlayerContexts(ViewGroup.AllPlayerCodes);
+
+			Context = ViewGroup.AllPlayerContexts[0];
+			await Context.OnReady;
+		}
+	}
+}
