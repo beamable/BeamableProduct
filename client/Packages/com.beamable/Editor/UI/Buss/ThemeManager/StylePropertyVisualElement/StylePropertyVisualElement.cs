d70968c41461a52cef2ec9fdbe8defc83c9270f8
LukaszKundys <LukaszKundys@users.noreply.github.com>
2022-09-27 10:11:36 +0000

    Formatting: Add variable button style changed (#1674)

--- client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/StylePropertyVisualElement/StylePropertyVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/StylePropertyVisualElement/StylePropertyVisualElement.cs
@@ -26,17 +26,17 @@ namespace Beamable.Editor.UI.Components
 		{
 			base.Init();
 
-			_labelComponent = new TextElement {name = "propertyLabel", tooltip = _model.Tooltip};
+			_labelComponent = new TextElement { name = "propertyLabel", tooltip = _model.Tooltip };
 			_labelComponent.RegisterCallback<MouseDownEvent>(_model.LabelClicked);
 			Root.Add(_labelComponent);
 
-			_valueParent = new VisualElement {name = "value"};
+			_valueParent = new VisualElement { name = "value" };
 			Root.Add(_valueParent);
 
-			_variableParent = new VisualElement {name = "globalVariable"};
+			_variableParent = new VisualElement { name = "globalVariable" };
 			Root.Add(_variableParent);
 
-			var overrideIndicatorParent = new VisualElement {name = "overrideIndicatorParent"};
+			var overrideIndicatorParent = new VisualElement { name = "overrideIndicatorParent" };
 			overrideIndicatorParent.AddToClassList("overrideIndicatorParent");
 			Root.Add(overrideIndicatorParent);
 
@@ -149,13 +149,13 @@ namespace Beamable.Editor.UI.Components
 				if (variableSource.StyleSheet == null)
 				{
 					_model.Tooltip = $"Variable: {variableSource.PropertyProvider.Key}\n" +
-					                 "Declared in inline style.";
+									 "Declared in inline style.";
 				}
 				else
 				{
 					_model.Tooltip = $"Variable: {variableSource.PropertyProvider.Key}\n" +
-					                 $"Selector: {variableSource.StyleRule.SelectorString}\n" +
-					                 $"Style sheet: {variableSource.StyleSheet.name}";
+									 $"Selector: {variableSource.StyleRule.SelectorString}\n" +
+									 $"Style sheet: {variableSource.StyleSheet.name}";
 				}
 			}
 			else
