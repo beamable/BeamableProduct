d854e14c8e9fe7a94a8990717d022a91a1c6d5e7
Łukasz Kundys <lukaszk@beamable.com>
2022-09-28 13:02:23 +0200

    [BEAM-3063] Possibility to fold style card (#1682)

--- client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/StyleCardVisualElement/StyleCardModel.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/StyleCardVisualElement/StyleCardModel.cs
@@ -50,6 +50,7 @@ namespace Beamable.Editor.UI.Components
 		private PropertySourceDatabase PropertiesDatabase { get; }
 		private IEnumerable<BussStyleSheet> WritableStyleSheets { get; }
 		public bool IsWritable => StyleSheet.IsWritable;
+		public bool IsFolded => StyleRule.Folded;
 		public bool ShowAll { get; private set; }
 		private bool Sorted { get; set; }
 		private BussElement SelectedElement { get; }
@@ -317,6 +318,13 @@ namespace Beamable.Editor.UI.Components
 			}
 
 			Change?.Invoke();
+		}
+
+		public void FoldButtonClicked(MouseDownEvent evt)
+		{
+			StyleRule.SetFolded(!StyleRule.Folded);
+			AssetDatabase.SaveAssets();
+			_globalRefresh?.Invoke();
 		}
 	}
 }
