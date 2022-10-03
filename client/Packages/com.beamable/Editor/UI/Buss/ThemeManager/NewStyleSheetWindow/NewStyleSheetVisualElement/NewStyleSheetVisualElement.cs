6bdd43d3822f52e4daaa9e54b3354a2a4024416c
Łukasz Wysk <lukaszw@beamable.com>
2022-10-03 17:03:49 +0200

    [BEAM-3078] tweaks

--- client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/NewStyleSheetWindow/NewStyleSheetVisualElement/NewStyleSheetVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/NewStyleSheetWindow/NewStyleSheetVisualElement/NewStyleSheetVisualElement.cs
@@ -75,13 +75,13 @@ namespace Beamable.Editor.UI.Buss
 
 			if (string.IsNullOrWhiteSpace(variableName))
 			{
-				message = "Variable name can't be empty";
+				message = "Steel sheet name can't be empty";
 				return false;
 			}
 
 			if (!Regex.IsMatch(variableName, VARIABLE_NAME_REGEX))
 			{
-				message = "Variable name can contain only letters";
+				message = "Steel sheet name can contain only letters";
 				return false;
 			}
 
