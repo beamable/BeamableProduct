40ba6246538a48fdc65b66614f49a63f8a82c22c
Artur Mankowski <arturmankowski22@gmail.com>
2022-10-27 14:57:37 +0200

    Added an option to customize service name in Create New context

--- client/Packages/com.beamable.server/Editor/UI/Components/CreateNewServiceDropdownVisualElement/CreateNewServiceDropdownVisualElement.cs
+++ client/Packages/com.beamable.server/Editor/UI/Components/CreateNewServiceDropdownVisualElement/CreateNewServiceDropdownVisualElement.cs
@@ -1,31 +1,38 @@
 ﻿using Beamable.Server.Editor;
 using System;
+using System.Collections.Generic;
 using UnityEngine.UIElements;
 
 namespace Beamable.Editor.Microservice.UI.Components
 {
 	public class CreateNewServiceDropdownVisualElement : MicroserviceComponent
 	{
+		
+		
 		public CreateNewServiceDropdownVisualElement() : base(nameof(CreateNewServiceDropdownVisualElement)) { }
-
 		public event Action<ServiceType> OnCreateNewClicked;
 
 		private VisualElement _servicesList;
-		
+
+		private readonly Dictionary<ServiceType, string> _serviceTypeCustomNamesDict = new Dictionary<ServiceType, string>
+		{
+			{ ServiceType.MicroService, "Microservice" }, 
+			{ ServiceType.StorageObject, "Storage Object" }
+		};
+
 		public override void Refresh()
 		{
 			base.Refresh();
 			_servicesList = Root.Q<VisualElement>("servicesList");
 			SetContent();
 		}
-
 		private void SetContent()
 		{
 			foreach (var serviceType in (ServiceType[]) Enum.GetValues(typeof(ServiceType)))
 			{
 				var serviceEntryButton = new VisualElement {name = "serviceEntryButton"};
 				serviceEntryButton.Add(new Image {name = $"image{serviceType}"});
-				serviceEntryButton.Add(new Label(serviceType.ToString()){name = "label"});
+				serviceEntryButton.Add(new Label(TryGetCustomServiceName(serviceType)){name = "label"});
 				serviceEntryButton.RegisterCallback<MouseDownEvent>(_ =>
 				{
 					OnCreateNewClicked?.Invoke(serviceType);
@@ -33,5 +40,7 @@ namespace Beamable.Editor.Microservice.UI.Components
 				_servicesList.Add(serviceEntryButton);
 			}
 		}
+		private string TryGetCustomServiceName(ServiceType serviceType) 
+			=> _serviceTypeCustomNamesDict.ContainsKey(serviceType) ? _serviceTypeCustomNamesDict[serviceType] : serviceType.ToString();
 	}
 }
