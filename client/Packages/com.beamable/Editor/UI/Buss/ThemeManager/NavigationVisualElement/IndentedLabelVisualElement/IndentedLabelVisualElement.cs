702f24e7959bcaa231d6aed0196eeb4a8eb40a42
Chris Hanna <chris@beamable.com>
2022-11-01 16:21:43 -0400

    beam 3196, 3197 - nav window folding

--- client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/NavigationVisualElement/IndentedLabelVisualElement/IndentedLabelVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/NavigationVisualElement/IndentedLabelVisualElement/IndentedLabelVisualElement.cs
@@ -1,6 +1,9 @@
-﻿using Beamable.Editor.UI.Common;
+﻿using Beamable.Editor.UI.Buss;
+using Beamable.Editor.UI.Common;
 using Beamable.UI.Buss;
 using System;
+using System.Diagnostics;
+using System.Linq;
 #if UNITY_2018
 using UnityEngine.Experimental.UIElements.StyleSheets;
 using UnityEngine.Experimental.UIElements;
@@ -10,23 +13,26 @@ using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 #endif
 using static Beamable.Common.Constants.Features.Buss.ThemeManager;
+using Debug = UnityEngine.Debug;
 
 namespace Beamable.Editor.UI.Components
 {
+	[DebuggerDisplay("Label = {_label} Depth = {Level}")]
 	public class IndentedLabelVisualElement : BeamableBasicVisualElement
 	{
 		public const float DEFAULT_SINGLE_INDENT_WIDTH = 20.0f;
 
 		private float _singleIndentWidth;
-		private float _level;
+		public int Level { get; private set; }
 		private string _label;
 
 		private Action<BussElement> _onMouseClicked;
+		public Action<bool> OnFoldChanged;
 
 		private VisualElement _container;
-		private TextElement _labelComponent;
 		private BussElement _bussElement;
 		private bool _selected;
+		public bool IsFolded { get; private set; }
 
 		public IndentedLabelVisualElement() : base(
 			$"{BUSS_THEME_MANAGER_PATH}/{nameof(NavigationVisualElement)}/{nameof(IndentedLabelVisualElement)}/{nameof(IndentedLabelVisualElement)}.uss")
@@ -37,13 +43,15 @@ namespace Beamable.Editor.UI.Components
 						  Action<BussElement> onMouseClicked,
 						  int level,
 						  float width,
-						  bool selected)
+						  bool selected,
+						  bool isFolded)
 		{
+			IsFolded = isFolded;
 			_bussElement = bussElement;
 			_onMouseClicked = onMouseClicked;
 
 			_label = label;
-			_level = level;
+			Level = level;
 			_singleIndentWidth = width;
 			_selected = selected;
 		}
@@ -54,19 +62,53 @@ namespace Beamable.Editor.UI.Components
 
 			_container = new VisualElement { name = "indentedLabelContainer" };
 
-			_labelComponent = new TextElement { name = "indentedLabel", text = _label };
+			var elem = new VisualElement { name="element"};
+			
+			var typeLabel = new TextElement { name = "typeLabel", text = _bussElement.TypeName };
 			_container.SetSelected(_selected);
 
-			float width = (_singleIndentWidth * _level) + _singleIndentWidth;
+			var idLabel = new TextElement {name = "idLabel", text = BussNameUtility.AsIdSelector(_bussElement.Id) };
+			var classLabel = new TextElement {name = "classLabel", text = BussNameUtility.ClassListString(_bussElement.Classes)};
+
+			var iconContainer = new VisualElement {name = "iconContainer"};
+			Image foldIcon = new Image { name = "foldIcon" };
+			foldIcon.EnableInClassList("folded", IsFolded);
+			foldIcon.EnableInClassList("unfolded", !IsFolded);
+			
+			iconContainer.RegisterCallback<MouseDownEvent>(e =>
+			{
+				IsFolded = !IsFolded;
+				foldIcon.EnableInClassList("folded", IsFolded);
+				foldIcon.EnableInClassList("unfolded", !IsFolded);
+
+				OnFoldChanged?.Invoke(IsFolded);
+				e.StopPropagation();
+				e.PreventDefault();
+			});
+			
+			iconContainer.Add(foldIcon);
+
+			if (_bussElement.Children.Any())
+			{
+				elem.Add(iconContainer);
+			}
+			
+			elem.Add(typeLabel);
+			elem.Add(idLabel);
+			
+			
+			elem.Add(classLabel);
+
+			float width = (_singleIndentWidth * Level) + _singleIndentWidth;
 
 #if UNITY_2018
-			_labelComponent.SetLeft(width);
+			elem.SetLeft(width);
 #elif UNITY_2019_1_OR_NEWER
-			_labelComponent.style.paddingLeft = new StyleLength(width);
+			elem.style.paddingLeft = new StyleLength(width);
 #endif
 
-			_container.Add(_labelComponent);
-
+			_container.Add(elem);
+			
 			Root.Add(_container);
 
 			_container.RegisterCallback<MouseDownEvent>(OnMouseClicked);
--- client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/NavigationVisualElement/NavigationVisualElement.cs
+++ client/Packages/com.beamable/Editor/UI/Buss/ThemeManager/NavigationVisualElement/NavigationVisualElement.cs
@@ -2,6 +2,8 @@
 using Beamable.Editor.UI.Common;
 using Beamable.UI.Buss;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.UIElements;
 using static Beamable.Common.Constants.Features.Buss.ThemeManager;
 
@@ -61,14 +63,87 @@ namespace Beamable.Editor.UI.Components
 			_spawnedLabels.Clear();
 			_hierarchyContainer.Clear();
 
+			var totalCount = 0;
+			var foldedAtDepth = 0;
+			var isParentFolded = false;
 			foreach (KeyValuePair<BussElement, int> pair in _model.FoundElements)
 			{
 				IndentedLabelVisualElement label = new IndentedLabelVisualElement();
+				var selfDepth = pair.Value;
+				var initialFold = _model.GetElementFolded(pair.Key);
 				label.Setup(pair.Key, BussNameUtility.GetFormattedLabel(pair.Key), _model.NavigationElementClicked,
-							pair.Value, IndentedLabelVisualElement.DEFAULT_SINGLE_INDENT_WIDTH, pair.Key == _model.SelectedElement);
+							pair.Value, IndentedLabelVisualElement.DEFAULT_SINGLE_INDENT_WIDTH, pair.Key == _model.SelectedElement, initialFold);
 				label.Init();
+				var index = totalCount;
+				totalCount++;
+
+				void HandleFold(bool isFolded)
+				{
+					_model.SetElementFold(pair.Key, isFolded);
+
+					var stack = new Stack<IndentedLabelVisualElement>();
+					stack.Push(label);
+					
+					
+					for (var i = index + 1; i < _spawnedLabels.Count; i++)
+					{
+						// if the stack is empty, we exit.
+						if (!stack.Any())
+						{
+							break;
+						}
+						
+						var master = stack.Peek();
+						var curr = _spawnedLabels[i];
+
+						// if the current element matches the parent depth, we step out.
+						if (curr.Level <= master.Level)
+						{
+							stack.Pop();
+							i--; // go back one element so we can re-process with the parent context
+							continue;
+						}
+						
+						
+						if (master.IsFolded)
+						{
+							// if the the parent is folded, then we hide everything...
+							curr.EnableInClassList("hidden", true);
+						}
+						else
+						{
+							// otherwise, we can enable this element.
+							curr.EnableInClassList("hidden", false);
+							
+							// and, if this element itself IS folded, then we need to hide its sub elements.
+							if (curr.IsFolded)
+							{
+								stack.Push(curr);
+								
+							}
+						}
+					}
+					
+				}
+
+				label.OnFoldChanged += HandleFold;
 				_spawnedLabels.Add(label);
 				_hierarchyContainer.Add(label);
+				
+
+				label.EnableInClassList("hidden", isParentFolded);
+				if (selfDepth <= foldedAtDepth)
+				{
+					isParentFolded = initialFold;
+					foldedAtDepth = selfDepth;
+					label.EnableInClassList("hidden", false);
+				}
+				else if (!isParentFolded && initialFold)
+				{
+					foldedAtDepth = selfDepth;
+					isParentFolded = true;
+				}
+				
 			}
 		}
 
