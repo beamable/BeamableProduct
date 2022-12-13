c8785d0c555ab070061b9a77b98d427bc41c941a
MevLyshkin <piotr@beamable.com>
2022-12-13 14:05:23 +0100

    [BEAM-3068] Content CLI  (#1960)
    * Content pull command- part one
    
    * Fix different domains requests
    
    * Content Local Cache
    
    * Cleanup
    
    * Rework ContentService, create ContentDocument
    
    * Make calculate checksum algorithm match server side
    
    * Content status command
    
    * Manifest updates
    
    * Hide content type since it always would be content
    
    * Allow skiping upToDate content, manifestId setting
    
    * Tags base support
    
    * Add default constructor- fix deseralization
    
    * Change tags file name
    
    * Limit output and print options, names matches git status
    
    * Update Getting local content status
    
    * Move getting content path to sepeare method
    
    * Restore skipping up to date entries
    
    * Content open command
    
    * Move content classes to separate folder
    
    * Content publish part I
    
    * Working publishing
    
    * Working publishing
    
    * Cleanup publish methods
    
    * Cleanup
    
    * Fix tags
    
    * Replace skipUpToDate with showUpToDate option
    
    * Beam content opens file explorer inside content folder
    
    * Skip and limit options update in status

---
+++ cli/cli/Commands/Content/ContentPullCommand.cs
@@ -0,0 +1,39 @@
+﻿using Beamable.Common;
+using cli.Services;
+using cli.Services.Content;
+using System.Text.Json;
+
+namespace cli.Content;
+
+public class ContentPullCommand : AppCommand<ContentPullCommandArgs>
+{
+	private readonly ContentService _contentService;
+	public ContentPullCommand(ContentService contentService) : base("pull", "Pulls currently deployed content")
+	{
+		_contentService = contentService;
+	}
+
+	public override async Task Handle(ContentPullCommandArgs args)
+	{
+		var manifest = await _contentService.GetManifest(args.ManifestId);
+		_contentService.UpdateTags(manifest);
+		var result = await _contentService.PullContent(manifest);
+		if(args.printOutput){
+			var json = JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true });
+			BeamableLogger.Log(json);
+		}
+	}
+
+	public override void Configure()
+	{
+		AddOption(new ConfigurableOption("manifestId", "set the manifest to use, 'global' by default"),
+			(args, s) => args.ManifestId = s);
+		AddOption(new ConfigurableOptionFlag("printOutput", "Print content to console"),
+			(args, b) => args.printOutput = b);
+	}
+}
+
+public class ContentPullCommandArgs : ContentCommandArgs
+{
+	public bool printOutput;
+}
