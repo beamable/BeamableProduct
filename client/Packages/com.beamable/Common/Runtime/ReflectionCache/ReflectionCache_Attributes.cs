ea8cff889459c3cfaa3eca96c488fcd90bb9e2a9
cdhanna <cdhanna@users.noreply.github.com>
2022-12-15 01:27:28 +0000

    Formatting: syncing sdk from openapi (#1994)

--- client/Packages/com.beamable/Common/Runtime/ReflectionCache/ReflectionCache_Attributes.cs
+++ client/Packages/com.beamable/Common/Runtime/ReflectionCache/ReflectionCache_Attributes.cs
@@ -100,7 +100,7 @@ namespace Beamable.Common.Reflection
 		public bool TryGetFromMemberInfo(MemberInfo info, out Attribute attribute)
 		{
 			attribute = null;
-			
+
 			foreach (var customAttributeData in info.CustomAttributes)
 			{
 				if (customAttributeData.AttributeType == AttributeType)
@@ -131,7 +131,7 @@ namespace Beamable.Common.Reflection
 				if (FoundInBaseTypes[i].IsAssignableFrom(type))
 					return true;
 			}
-			
+
 			for (int i = 0; i < FoundInTypesWithAttributes.Count; i++)
 			{
 				foreach (var element in type.CustomAttributes)
@@ -142,7 +142,7 @@ namespace Beamable.Common.Reflection
 					}
 				}
 			}
-			
+
 			return false;
 		}
 
@@ -235,9 +235,9 @@ namespace Beamable.Common.Reflection
 
 				return false;
 			}
-			
+
 			// Check for attributes over the type itself.
-			
+
 			for (int i = 0; i < attributesToSearchFor.Count; i++)
 			{
 				if (HasType(member, attributesToSearchFor[i].AttributeType))
@@ -264,13 +264,13 @@ namespace Beamable.Common.Reflection
 
 					// For each declared member, check if they have the current attribute of interest -- if they do, add them to the found attribute list.
 					// In this step we catch every member with the attribute --- individual systems are welcome to parse and yield errors at a later step.
-					
+
 					var filteredMembers = type.FindMembers(AttributeOfInterest.INTERNAL_TYPE_SEARCH_WHEN_IS_MEMBER_TYPES, BindingFlags.Public |
-					                                       BindingFlags.NonPublic |
-					                                       BindingFlags.Instance |
-					                                       BindingFlags.Static, null, null);
-					
-					for (int k = 0; k < filteredMembers.Length; k++) 
+														   BindingFlags.NonPublic |
+														   BindingFlags.Instance |
+														   BindingFlags.Static, null, null);
+
+					for (int k = 0; k < filteredMembers.Length; k++)
 					{
 						if (declaredMemberAttributesToSearchFor[i].TryGetFromMemberInfo(filteredMembers[k], out var attribute))
 							foundAttributes[declaredMemberAttributesToSearchFor[i]].Add(new MemberAttribute(filteredMembers[k], attribute));
