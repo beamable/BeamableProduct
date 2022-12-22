65116ed8b5f4581b0dcb8d310a4b2a421181edaf
Tomasz Kaczmarz <tomasz@beamable.com>
2022-12-22 13:14:10 +0100

    [WIP] Sign in flow rework

--- client/Packages/com.beamable/EasyFeatures/BasicAccountManagement/AccountsView.cs
+++ client/Packages/com.beamable/EasyFeatures/BasicAccountManagement/AccountsView.cs
@@ -1,5 +1,6 @@
 ﻿using Beamable.Common;
 using Beamable.EasyFeatures.Components;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,18 +14,33 @@ namespace Beamable.EasyFeatures.BasicAccountManagement
 
 			Promise<AccountSlotPresenter.ViewData> GetAccountViewData();
 		}
+
+		
+		private const string ENTER_EMAIL_TEXT = "Please enter your email here...";
+		private const string SIGN_IN_INFO_TEXT = "Sign in or create an account to save your progress online.";
 		
 		public AccountManagementFeatureControl FeatureControl;
 		public int EnrichOrder;
 
 		public AccountSlotPresenter AccountPresenter;
+		public TextMeshProUGUI InfoText;
+		
+		[Header("Button groups")]
 		public GameObject SignInButtonsGroup;
 		public GameObject SwitchButtonsGroup;
+		public GameObject NextCancelButtonsGroup;
+		
+		[Space]
 		public Button SignInButton;
 		public Button CreateAccountButton;
+		public Button NextButton;
+		public Button CancelButton;
+		public TMP_InputField EmailInputField;
+		
+		[Header("Switch Account Popup")]
+		public SwitchAccountPopup SwitchAccountPopupPrefab;
 		public Button LoadGameButton;
 		public Button SwitchAccountButton;
-		public SwitchAccountPopup SwitchAccountPopupPrefab;
 
 		protected IDependencies System;
 		
@@ -50,7 +66,12 @@ namespace Beamable.EasyFeatures.BasicAccountManagement
 			// TODO enable proper set of buttons
 			SignInButtonsGroup.SetActive(false);
 			SwitchButtonsGroup.SetActive(true);
+			NextCancelButtonsGroup.SetActive(false);
+			
 			SwitchAccountPopupPrefab.gameObject.SetActive(false);
+			EmailInputField.gameObject.SetActive(false);
+
+			InfoText.text = SIGN_IN_INFO_TEXT;
 			
 			// setup callbacks
 			FeatureControl.SetBackAction(GoBack);
@@ -59,6 +80,8 @@ namespace Beamable.EasyFeatures.BasicAccountManagement
 			CreateAccountButton.onClick.ReplaceOrAddListener(OnCreateAccount);
 			LoadGameButton.onClick.ReplaceOrAddListener(OnLoadGame);
 			SwitchAccountButton.onClick.ReplaceOrAddListener(OnSwitchAccount);
+			NextButton.onClick.ReplaceOrAddListener(OnSignIn);
+			CancelButton.onClick.ReplaceOrAddListener(OnCancel);
 
 			AccountSlotPresenter.PoolData data = new AccountSlotPresenter.PoolData
 			{
@@ -68,15 +91,26 @@ namespace Beamable.EasyFeatures.BasicAccountManagement
 			AccountPresenter.Setup(data, OpenAccountInfoView, null);
 		}
 
+		private void OnCancel()
+		{
+			// restore normal accounts view
+			NextCancelButtonsGroup.SetActive(false);
+			SwitchButtonsGroup.SetActive(true);
+			EmailInputField.gameObject.SetActive(false);
+		}
+
 		private void OnSwitchAccount()
 		{
 			var popup = FeatureControl.OverlaysController.CustomOverlay.Show(SwitchAccountPopupPrefab);
-			popup.Setup(OpenSignInView, OpenCreateAccountView);
+			popup.Setup(StartSignIn, OpenCreateAccountView);
 
-			void OpenSignInView()
+			void StartSignIn()
 			{
 				FeatureControl.OverlaysController.CustomOverlay.Hide();
-				FeatureControl.OpenSignInView();
+				SwitchButtonsGroup.SetActive(false);
+				NextCancelButtonsGroup.SetActive(true);
+				EmailInputField.gameObject.SetActive(true);
+				InfoText.text = ENTER_EMAIL_TEXT;
 			}
 
 			void OpenCreateAccountView()
