bfd1ce836bd83ba4c3a3aeecf78920e98f9b444b
Chris Hanna <chris@beamable.com>
2022-12-23 14:11:05 -0500

    fix content modification bug

--- client/Packages/com.beamable/Common/Runtime/Content/ContentObject.cs
+++ client/Packages/com.beamable/Common/Runtime/Content/ContentObject.cs
@@ -354,18 +354,36 @@ namespace Beamable.Common.Content
       [SerializeField]
       private string _serializedValidationGUID { get; set; }
       
+      private static readonly int[] _guidByteOrder =
+	      new[] { 15, 14, 13, 12, 11, 10, 9, 8, 6, 7, 4, 5, 0, 1, 2, 3 };
+      private static Guid Increment(Guid guid)
+      {
+	      var bytes = guid.ToByteArray();
+	      bool carry = true;
+	      for (int i = 0; i < _guidByteOrder.Length && carry; i++)
+	      {
+		      int index = _guidByteOrder[i];
+		      byte oldValue = bytes[index]++;
+		      carry = oldValue > bytes[index];
+	      }
+	      return new Guid(bytes);
+      }
+
       private void OnValidate()
       {
-	      if (!string.IsNullOrEmpty(_serializedValidationGUID))
+	      if (!string.IsNullOrEmpty(_serializedValidationGUID) && Guid.TryParse(_serializedValidationGUID, out var guid))
 	      {
-		      ValidationGuid = Guid.Parse(_serializedValidationGUID);
+		      ValidationGuid = guid;
 	      }
 	      else
 	      {
-		      ValidationGuid = Guid.NewGuid();
+		      ValidationGuid = Guid.Empty;
 		      _serializedValidationGUID = ValidationGuid.ToString();
 	      }
 
+	      ValidationGuid = Increment(ValidationGuid);
+	      _serializedValidationGUID = ValidationGuid.ToString();
+	      
          if (ValidationContext == null)
          {
 	         // if we have no validation context assigned yet, then we cannot possibly validate.
