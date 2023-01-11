79b73a4c964d6b03c7a6c9e595926f3d09bd368d
Łukasz Wysk <lukaszw@beamable.com>
2023-01-11 15:58:18 +0100

    BEAM-3049 fix

--- client/Packages/com.beamable/Editor/Utility/BeamableAssetDatabase.cs
+++ client/Packages/com.beamable/Editor/Utility/BeamableAssetDatabase.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.QuickSearch;
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace Beamable.Editor
@@ -20,11 +24,56 @@ namespace Beamable.Editor
 			Assert.IsFalse(t.FullName != null && t.FullName.Contains(nameof(UnityEditorInternal)),
 						   $"Type {t.FullName} is part of `UnityEditorInternal`- these assets should be found using just nameof, not full type name");
 			var fullName = t.FullName;
+			
+			var searchContext = SearchService.CreateContext(new string[] {"asset", "object", "res"}, $"t:{fullName}");
 
-			return AssetDatabase.FindAssets($"t:{fullName}", searchInFolders);
+			SearchProvider beamableProvider = new SearchProvider("beam", "beamableProvider");
+
+			var result1 = SearchService.GetItems(searchContext, SearchFlags.WantsMore)
+			                           .Select(x => AssetDatabase.AssetPathToGUID(x.id)).ToArray();
+			
+			//var results = new List<SearchItem>();
+			//results.AddRange(SearchService.GetItems(searchContext, SearchFlags.WantsMore));
+			//SearchService.Request(searchContext, SearchFlags.WantsMore | SearchFlags.Debug);
+		
+			
+			//while (searchContext.searchInProgress);
+			
+			//var rr = results .Select(x => AssetDatabase.AssetPathToGUID(x.id)).ToArray();
+			
+				var result2 = AssetDatabase.FindAssets($"t:{fullName}", searchInFolders);
+
+				if (result1.Length != result2.Length)
+				{
+
+					for (int i = 0; i < result2.Length; i++)
+					{
+						if (!result1.Contains(result2[i]))
+						{
+							string path = AssetDatabase.GUIDToAssetPath(result2[i]);
+							Debug.LogError("DIFFERENT FOR: " + path);
+						}
+					}
+				}
+				else
+				{
+					for (int i = 0; i < result2.Length; i++)
+					{
+						string path = AssetDatabase.GUIDToAssetPath(result2[i]);
+						Debug.LogError("WORKS FOR: " + path);
+					}
+				}
+				
+			return result2;
+
+			//	Debug.LogError("DIFFERENT");
+
+			//return resitem.ToArray();
 		}
 
 		/// <inheritdoc cref="FindAssets"/>
 		public static string[] FindAssets<T>(string[] searchInFolders = null) => FindAssets(typeof(T), searchInFolders);
+		
+		
 	}
 }
