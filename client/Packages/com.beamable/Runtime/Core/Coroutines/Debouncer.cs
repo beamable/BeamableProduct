0f664f04c826964e271f85c69f2aa555e0f7f621
LukaszKundys <LukaszKundys@users.noreply.github.com>
2023-01-19 10:25:45 +0000

    Formatting: [BEAM-3355] IThirdPartyCloudIdentity interface implemented and dropdown in CurrencyContent (#2085)

--- client/Packages/com.beamable/Runtime/Core/Coroutines/Debouncer.cs
+++ client/Packages/com.beamable/Runtime/Core/Coroutines/Debouncer.cs
@@ -15,7 +15,7 @@ namespace Beamable.Coroutines
 			return Promise.Success;
 		}
 	}
-	
+
 	public class FunctionPromiseDebouncer : DebounceService<Func<Promise>>
 	{
 		public FunctionPromiseDebouncer(CoroutineService coroutineService) : base(coroutineService) { }
@@ -26,20 +26,20 @@ namespace Beamable.Coroutines
 	}
 
 
-	public abstract class DebounceService<T> where T: Delegate
+	public abstract class DebounceService<T> where T : Delegate
 	{
 		private readonly CoroutineService _coroutineService;
 
 		private Dictionary<T, TimeoutYieldInstruction> _actionToYielder =
 			new Dictionary<T, TimeoutYieldInstruction>();
-			
+
 		public DebounceService(CoroutineService coroutineService)
 		{
 			_coroutineService = coroutineService;
 		}
 
 		protected abstract Promise Invoke(T action);
-		
+
 		/// <summary>
 		/// Enqueue some action to run "later", but only once.
 		/// If this method were to be called many times in a short order, the given <see cref="action"/>
@@ -50,7 +50,7 @@ namespace Beamable.Coroutines
 		/// <param name="action">Some action to execute one time</param>
 		/// <param name="bouncer">A <see cref="CustomYieldInstruction"/> that controls how long to wait
 		/// before executing the action. By default, it produces a wait time of .1 seconds.</param>
-		public Promise SetTimeout(T action, CustomYieldInstruction bouncer=null)
+		public Promise SetTimeout(T action, CustomYieldInstruction bouncer = null)
 		{
 			if (bouncer == null)
 			{
@@ -77,7 +77,7 @@ namespace Beamable.Coroutines
 			{
 				instruction.Instruction = bouncer;
 			}
-			
+
 			return instruction.Promise;
 		}
 
@@ -99,8 +99,8 @@ namespace Beamable.Coroutines
 			return false;
 		}
 	}
-	
-	
+
+
 	public class Debouncer
 	{
 		private ActionDebouncer _actions;
@@ -111,14 +111,14 @@ namespace Beamable.Coroutines
 			_actions = new ActionDebouncer(coroutineService);
 			_funcs = new FunctionPromiseDebouncer(coroutineService);
 		}
-		
+
 		public Promise SetTimeout(Func<Promise> generator, CustomYieldInstruction bouncer = null) =>
 			_funcs.SetTimeout(generator, bouncer);
 
 		public Promise SetTimeout(Action generator, CustomYieldInstruction bouncer = null) =>
 			_actions.SetTimeout(generator, bouncer);
 	}
-	
+
 	class TimeoutYieldInstruction : CustomYieldInstruction
 	{
 		public CustomYieldInstruction Instruction { get; set; }
@@ -126,13 +126,13 @@ namespace Beamable.Coroutines
 
 		public bool IsCancelled { get; private set; }
 		public Promise Promise { get; }
-			
+
 		public TimeoutYieldInstruction(CustomYieldInstruction instruction)
 		{
 			Instruction = instruction;
 			Promise = new Promise();
 		}
-			
+
 		public void Cancel()
 		{
 			IsCancelled = true;
