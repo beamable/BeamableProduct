5b954026b6dad7ced1f4ee1af451a4185e3d267f
arturmankowski <arturmankowski22@gmail.com>
2023-01-27 12:41:26 +0100

    Fix

--- client/Packages/com.beamable/Runtime/UI/Scripts/Buss/Styles/Properties/ColorProperties.cs
+++ client/Packages/com.beamable/Runtime/UI/Scripts/Buss/Styles/Properties/ColorProperties.cs
@@ -8,7 +8,7 @@ namespace Beamable.UI.Buss
 	public class SingleColorBussProperty : DefaultBussProperty, IColorBussProperty, IVertexColorBussProperty
 	{
 		[SerializeField]
-		private Color _color = Color.black;
+		private Color _color = Color.white;
 
 		public Color Color
 		{
