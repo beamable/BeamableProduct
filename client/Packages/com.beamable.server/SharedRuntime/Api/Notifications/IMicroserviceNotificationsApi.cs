016c6ca73c214d4c19cb83f69ef54e848bb53475
arturmankowski <arturmankowski22@gmail.com>
2023-02-01 13:59:36 +0100

    New supported notifications

--- client/Packages/com.beamable.server/SharedRuntime/Api/Notifications/IMicroserviceNotificationsApi.cs
+++ client/Packages/com.beamable.server/SharedRuntime/Api/Notifications/IMicroserviceNotificationsApi.cs
@@ -52,6 +52,172 @@ namespace Beamable.Server.Api.Notifications
 		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
 		/// <param name="messagePayload">The data to send along with the notification. Must be a JSON-serializable type.</param>
 		Promise<EmptyResponse> NotifyPlayer<T>(List<long> gamertags, string name, T messagePayload);
-	}
+		
+		
+		/// <summary>
+		/// Notifies the player with the given <paramref name="gamertag"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertag">The gamertag for the player you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">
+		/// The non-JSON string data to send along with the notification.
+		/// Due to Beamable constraints, note that the string message will be sent with an outer "stringValue" field wrapping it.
+		/// </param>
+		Promise<EmptyResponse> NotifyGlobal(long gamertag, string name, string messagePayload);
+		/// <summary>
+		/// Notifies the players identified by the given <paramref name="gamertags"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertags">The list of gamertags for the players you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The non-JSON string data to send along with the notification.
+		/// Due to Beamable constraints, note that the string message will be sent with an outer "stringValue" field wrapping it.
+		/// </param>
+		Promise<EmptyResponse> NotifyGlobal(List<long> gamertags, string name, string messagePayload);
+		/// <summary>
+		/// Notifies the player with the given <paramref name="gamertag"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertag">The gamertag for the player you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The data to send along with the notification. Must be a JSON-serializable type.</param>
+		Promise<EmptyResponse> NotifyGlobal<T>(long gamertag, string name, T messagePayload);
+		/// <summary>
+		/// Notifies the players identified by the given <paramref name="gamertags"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertags">The list of gamertags for the players you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The data to send along with the notification. Must be a JSON-serializable type.</param>
+		Promise<EmptyResponse> NotifyGlobal<T>(List<long> gamertags, string name, T messagePayload);
 
+		
+		/// <summary>
+		/// Notifies the player with the given <paramref name="gamertag"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertag">The gamertag for the player you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">
+		/// The non-JSON string data to send along with the notification.
+		/// Due to Beamable constraints, note that the string message will be sent with an outer "stringValue" field wrapping it.
+		/// </param>
+		Promise<EmptyResponse> NotifyCustom(long gamertag, string name, string messagePayload);
+		/// <summary>
+		/// Notifies the players identified by the given <paramref name="gamertags"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertags">The list of gamertags for the players you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The non-JSON string data to send along with the notification.
+		/// Due to Beamable constraints, note that the string message will be sent with an outer "stringValue" field wrapping it.
+		/// </param>
+		Promise<EmptyResponse> NotifyCustom(List<long> gamertags, string name, string messagePayload);
+		/// <summary>
+		/// Notifies the player with the given <paramref name="gamertag"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertag">The gamertag for the player you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The data to send along with the notification. Must be a JSON-serializable type.</param>
+		Promise<EmptyResponse> NotifyCustom<T>(long gamertag, string name, T messagePayload);
+		/// <summary>
+		/// Notifies the players identified by the given <paramref name="gamertags"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertags">The list of gamertags for the players you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The data to send along with the notification. Must be a JSON-serializable type.</param>
+		Promise<EmptyResponse> NotifyCustom<T>(List<long> gamertags, string name, T messagePayload);
+		
+		
+		/// <summary>
+		/// Notifies the player with the given <paramref name="gamertag"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertag">The gamertag for the player you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">
+		/// The non-JSON string data to send along with the notification.
+		/// Due to Beamable constraints, note that the string message will be sent with an outer "stringValue" field wrapping it.
+		/// </param>
+		Promise<EmptyResponse> NotifyGeneric(long gamertag, string name, string messagePayload);
+		/// <summary>
+		/// Notifies the players identified by the given <paramref name="gamertags"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertags">The list of gamertags for the players you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The non-JSON string data to send along with the notification.
+		/// Due to Beamable constraints, note that the string message will be sent with an outer "stringValue" field wrapping it.
+		/// </param>
+		Promise<EmptyResponse> NotifyGeneric(List<long> gamertags, string name, string messagePayload);
+		/// <summary>
+		/// Notifies the player with the given <paramref name="gamertag"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertag">The gamertag for the player you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The data to send along with the notification. Must be a JSON-serializable type.</param>
+		Promise<EmptyResponse> NotifyGeneric<T>(long gamertag, string name, T messagePayload);
+		/// <summary>
+		/// Notifies the players identified by the given <paramref name="gamertags"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertags">The list of gamertags for the players you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The data to send along with the notification. Must be a JSON-serializable type.</param>
+		Promise<EmptyResponse> NotifyGeneric<T>(List<long> gamertags, string name, T messagePayload);
+		
+		
+		/// <summary>
+		/// Notifies the player with the given <paramref name="gamertag"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertag">The gamertag for the player you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">
+		/// The non-JSON string data to send along with the notification.
+		/// Due to Beamable constraints, note that the string message will be sent with an outer "stringValue" field wrapping it.
+		/// </param>
+		Promise<EmptyResponse> NotifyGame(long gamertag, string name, string messagePayload);
+		/// <summary>
+		/// Notifies the players identified by the given <paramref name="gamertags"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertags">The list of gamertags for the players you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The non-JSON string data to send along with the notification.
+		/// Due to Beamable constraints, note that the string message will be sent with an outer "stringValue" field wrapping it.
+		/// </param>
+		Promise<EmptyResponse> NotifyGame(List<long> gamertags, string name, string messagePayload);
+		/// <summary>
+		/// Notifies the player with the given <paramref name="gamertag"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertag">The gamertag for the player you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The data to send along with the notification. Must be a JSON-serializable type.</param>
+		Promise<EmptyResponse> NotifyGame<T>(long gamertag, string name, T messagePayload);
+		/// <summary>
+		/// Notifies the players identified by the given <paramref name="gamertags"/> at the given <paramref name="name"/>. The <paramref name="name"/> is the one you should subscribe to in
+		/// your <see cref="INotificationService.Subscribe{T}"/> calls in the client-code.
+		/// </summary>
+		/// <param name="gamertags">The list of gamertags for the players you wish to notify.</param>
+		/// <param name="name">The context that player's client must be subscribed too to see the notification.</param>
+		/// <param name="messagePayload">The data to send along with the notification. Must be a JSON-serializable type.</param>
+		Promise<EmptyResponse> NotifyGame<T>(List<long> gamertags, string name, T messagePayload);
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="toAll"></param>
+		/// <param name="eventName"></param>
+		/// <param name="messagePayload"></param>
+		/// <returns></returns>
+		Promise<EmptyResponse> NotifyServer(bool toAll, string eventName, string messagePayload);
+		
+		// TODO-ARTUR - Add correct summary to each option 
+	}
 }
--- microservice/microservice/Api/Notifications/MicroserviceNotificationsApi.cs
+++ microservice/microservice/Api/Notifications/MicroserviceNotificationsApi.cs
@@ -13,38 +13,115 @@ namespace Beamable.Server.Api.Notifications
         public IBeamableRequester Requester { get; }
         public RequestContext Context { get; }
 
+        private const string URI_NOTIFICATION_BASE = "/basic/notification";
+        
+        private readonly string URI_NOTIFICATION_PLAYER = $"{URI_NOTIFICATION_BASE}/player";
+        private readonly string URI_NOTIFICATION_GLOBAL = $"{URI_NOTIFICATION_BASE}/global";
+        private readonly string URI_NOTIFICATION_CUSTOM = $"{URI_NOTIFICATION_BASE}/custom";
+        private readonly string URI_NOTIFICATION_GENERIC = $"{URI_NOTIFICATION_BASE}/generic";
+        private readonly string URI_NOTIFICATION_GAME = $"{URI_NOTIFICATION_BASE}/game";
+        private readonly string URI_NOTIFICATION_SERVER = $"{URI_NOTIFICATION_BASE}/server";
+
         public MicroserviceNotificationApi(IBeamableRequester requester, RequestContext context)
         {
             Requester = requester;
             Context = context;
         }
 
-        public Promise<EmptyResponse> NotifyPlayer(long dbid, string subscriptionId, string messagePayload)
-        {
-            var payload = CreateStringPayload(messagePayload);
-            var jsonSerializedPayload = JsonConvert.SerializeObject(payload, Formatting.None, UnitySerializationSettings.Instance);
-            return SendPlayerNotificationJson(dbid, subscriptionId, jsonSerializedPayload);
-        }
+        #region Notify Player
+        public Promise<EmptyResponse> NotifyPlayer(long dbid, string subscriptionId, string messagePayload) =>
+	        Notify(dbid, subscriptionId, messagePayload, URI_NOTIFICATION_PLAYER);
+        public Promise<EmptyResponse> NotifyPlayer(List<long> dbids, string subscriptionId, string messagePayload) =>
+	        Notify(dbids, subscriptionId, messagePayload, URI_NOTIFICATION_PLAYER);
+        public Promise<EmptyResponse> NotifyPlayer<T>(long dbid, string subscriptionId, T messagePayload)=>
+	        Notify(dbid, subscriptionId, messagePayload, URI_NOTIFICATION_PLAYER);
+        public Promise<EmptyResponse> NotifyPlayer<T>(List<long> dbids, string subscriptionId, T messagePayload)=>
+	        Notify(dbids, subscriptionId, messagePayload, URI_NOTIFICATION_PLAYER);
+        #endregion
 
-        public Promise<EmptyResponse> NotifyPlayer(List<long> dbids, string subscriptionId, string messagePayload)
-        {
-            var payload = CreateStringPayload(messagePayload);
-            var jsonSerializedPayload = JsonConvert.SerializeObject(payload, Formatting.None, UnitySerializationSettings.Instance);
-            return SendPlayerNotificationJson(dbids, subscriptionId, jsonSerializedPayload);
-        }
+        #region Notify Global
+        public Promise<EmptyResponse> NotifyGlobal(long dbid, string subscriptionId, string messagePayload) =>
+	        Notify(dbid, subscriptionId, messagePayload, URI_NOTIFICATION_GLOBAL);
+        public Promise<EmptyResponse> NotifyGlobal(List<long> dbids, string subscriptionId, string messagePayload) =>
+	        Notify(dbids, subscriptionId, messagePayload, URI_NOTIFICATION_GLOBAL);
+        public Promise<EmptyResponse> NotifyGlobal<T>(long dbid, string subscriptionId, T messagePayload) =>
+	        Notify(dbid, subscriptionId, messagePayload, URI_NOTIFICATION_GLOBAL);
+        public Promise<EmptyResponse> NotifyGlobal<T>(List<long> dbids, string subscriptionId, T messagePayload) =>
+	        Notify(dbids, subscriptionId, messagePayload, URI_NOTIFICATION_GLOBAL);
+        #endregion
 
-        public Promise<EmptyResponse> NotifyPlayer<T>(long dbid, string subscriptionId, T messagePayload)
-        {
-            var jsonSerializedPayload = JsonConvert.SerializeObject(messagePayload, Formatting.None, UnitySerializationSettings.Instance);
-            return SendPlayerNotificationJson(dbid, subscriptionId, jsonSerializedPayload);
-        }
+        #region Notify Custom
+        public Promise<EmptyResponse> NotifyCustom(long dbid, string subscriptionId, string messagePayload) =>
+	        Notify(dbid, subscriptionId, messagePayload, URI_NOTIFICATION_CUSTOM);
+        public Promise<EmptyResponse> NotifyCustom(List<long> dbids, string subscriptionId, string messagePayload) =>
+	        Notify(dbids, subscriptionId, messagePayload, URI_NOTIFICATION_CUSTOM);
+        public Promise<EmptyResponse> NotifyCustom<T>(long dbid, string subscriptionId, T messagePayload) =>
+	        Notify(dbid, subscriptionId, messagePayload, URI_NOTIFICATION_CUSTOM);
+        public Promise<EmptyResponse> NotifyCustom<T>(List<long> dbids, string subscriptionId, T messagePayload) =>
+	        Notify(dbids, subscriptionId, messagePayload, URI_NOTIFICATION_CUSTOM);
+        #endregion
 
-        public Promise<EmptyResponse> NotifyPlayer<T>(List<long> dbids, string subscriptionId, T messagePayload)
-        {
-            var jsonSerializedPayload = JsonConvert.SerializeObject(messagePayload, Formatting.None, UnitySerializationSettings.Instance);
-            return SendPlayerNotificationJson(dbids, subscriptionId, jsonSerializedPayload);
-        }
+        #region Notify Generic
+        public Promise<EmptyResponse> NotifyGeneric(long dbid, string subscriptionId, string messagePayload) =>
+	        Notify(dbid, subscriptionId, messagePayload, URI_NOTIFICATION_GENERIC);
+        public Promise<EmptyResponse> NotifyGeneric(List<long> dbids, string subscriptionId, string messagePayload) =>
+	        Notify(dbids, subscriptionId, messagePayload, URI_NOTIFICATION_GENERIC);
+        public Promise<EmptyResponse> NotifyGeneric<T>(long dbid, string subscriptionId, T messagePayload) =>
+	        Notify(dbid, subscriptionId, messagePayload, URI_NOTIFICATION_GENERIC);
+        public Promise<EmptyResponse> NotifyGeneric<T>(List<long> dbids, string subscriptionId, T messagePayload) =>
+	        Notify(dbids, subscriptionId, messagePayload, URI_NOTIFICATION_GENERIC);
+        #endregion
 
+        #region Notify Game
+        public Promise<EmptyResponse> NotifyGame(long dbid, string subscriptionId, string messagePayload) =>
+	        Notify(dbid, subscriptionId, messagePayload, URI_NOTIFICATION_GAME);
+        public Promise<EmptyResponse> NotifyGame(List<long> dbids, string subscriptionId, string messagePayload) =>
+	        Notify(dbids, subscriptionId, messagePayload, URI_NOTIFICATION_GAME);
+        public Promise<EmptyResponse> NotifyGame<T>(long dbid, string subscriptionId, T messagePayload) =>
+	        Notify(dbid, subscriptionId, messagePayload, URI_NOTIFICATION_GAME);
+        public Promise<EmptyResponse> NotifyGame<T>(List<long> dbids, string subscriptionId, T messagePayload) =>
+	        Notify(dbids, subscriptionId, messagePayload, URI_NOTIFICATION_GAME);
+        #endregion
+
+        #region Notify Server
+        public Promise<EmptyResponse> NotifyServer(bool toAll, string eventName, string messagePayload)
+        {
+	        var payload = CreateStringPayload(messagePayload);
+	        var jsonSerializedPayload = JsonConvert.SerializeObject(payload, Formatting.None, UnitySerializationSettings.Instance);
+	        
+	        var body = new
+	        {
+		        @event = eventName, 
+		        payload = messagePayload,
+		        toAll = toAll
+	        };
+	        return Requester.Request<EmptyResponse>(Method.POST, URI_NOTIFICATION_SERVER, body);
+        }
+        #endregion
+
+        private Promise<EmptyResponse> Notify(long dbid, string subscriptionId, string messagePayload, string uri)
+        {
+	        var payload = CreateStringPayload(messagePayload);
+	        var jsonSerializedPayload = JsonConvert.SerializeObject(payload, Formatting.None, UnitySerializationSettings.Instance);
+	        return SendNotificationJson(dbid, subscriptionId, jsonSerializedPayload, uri);
+        }
+        private Promise<EmptyResponse> Notify(List<long> dbids, string subscriptionId, string messagePayload, string uri)
+        {
+	        var payload = CreateStringPayload(messagePayload);
+	        var jsonSerializedPayload = JsonConvert.SerializeObject(payload, Formatting.None, UnitySerializationSettings.Instance);
+	        return SendNotificationJson(dbids, subscriptionId, jsonSerializedPayload, uri);
+        }
+        private Promise<EmptyResponse> Notify<T>(long dbid, string subscriptionId, T messagePayload, string uri)
+        {
+	        var jsonSerializedPayload = JsonConvert.SerializeObject(messagePayload, Formatting.None, UnitySerializationSettings.Instance);
+	        return SendNotificationJson(dbid, subscriptionId, jsonSerializedPayload, uri);
+        }
+        private Promise<EmptyResponse> Notify<T>(List<long> dbids, string subscriptionId, T messagePayload, string uri)
+        {
+	        var jsonSerializedPayload = JsonConvert.SerializeObject(messagePayload, Formatting.None, UnitySerializationSettings.Instance);
+	        return SendNotificationJson(dbids, subscriptionId, jsonSerializedPayload, uri);
+        }
+        
         private PrimitiveStringPayload CreateStringPayload(string message)
         {
             return new PrimitiveStringPayload
@@ -52,27 +129,32 @@ namespace Beamable.Server.Api.Notifications
                 stringValue = message
             };
         }
-
-        private Promise<EmptyResponse> SendPlayerNotificationJson(long dbid, string subscriptionId, string json)
+        private Promise<EmptyResponse> SendNotificationJson(long dbid, string subscriptionId, string json, string uri)
         {
-            return Requester.Request<EmptyResponse>(Method.POST, "/basic/notification/player",
-                new PlayerNotificationRequest
-                {
-                    dbid = dbid,
-                    payload = new PlayerNotificationPayload { context = subscriptionId, messageFull = json }
-                });
+	        var body = new NotificationBody
+	        {
+		        dbid = dbid, 
+		        payload = new NotificationPayload
+		        {
+			        context = subscriptionId, 
+			        messageFull = json
+		        }
+	        };
+            return Requester.Request<EmptyResponse>(Method.POST, uri, body);
         }
-        private Promise<EmptyResponse> SendPlayerNotificationJson(List<long> dbids, string subscriptionId, string json)
+        private Promise<EmptyResponse> SendNotificationJson(List<long> dbids, string subscriptionId, string json, string uri)
         {
-            return Requester.Request<EmptyResponse>(Method.POST, "/basic/notification/player",
-                new PlayerBatchNotificationRequest
-                {
-                    dbids = dbids,
-                    payload = new PlayerNotificationPayload { context = subscriptionId, messageFull = json }
-                });
+	        var body = new NotificationBatchedBody
+	        {
+		        dbids = dbids, 
+		        payload = new NotificationPayload
+		        {
+			        context = subscriptionId, 
+			        messageFull = json
+		        }
+	        };
+	        return Requester.Request<EmptyResponse>(Method.POST, uri, body);
         }
-
-
     }
 
 
@@ -80,27 +162,27 @@ namespace Beamable.Server.Api.Notifications
     /// Notification request format.
     /// </summary>
     [Serializable]
-    public class PlayerNotificationRequest
+    public class NotificationBody
     {
         public long dbid;
-        public PlayerNotificationPayload payload;
+        public NotificationPayload payload;
     }
 
     /// <summary>
     /// Format of the Notification request when notifying multiple players.
     /// </summary>
     [Serializable]
-    public class PlayerBatchNotificationRequest
+    public class NotificationBatchedBody
     {
         public List<long> dbids;
-        public PlayerNotificationPayload payload;
+        public NotificationPayload payload;
     }
 
     /// <summary>
     /// Structure representing the expected format for the notification we are sending.
     /// </summary>
     [Serializable]
-    public class PlayerNotificationPayload
+    public class NotificationPayload
     {
         public string messageFull;
         public string context;
