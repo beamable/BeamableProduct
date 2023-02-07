7fe1ae0d412fa63010c4bb26892cb9a305102b7f
Chris Hanna <chris@beamable.com>
2023-02-07 14:40:33 -0500

    beam-3423 - bad di on exit

--- client/Packages/com.beamable/Runtime/BeamContext.cs
+++ client/Packages/com.beamable/Runtime/BeamContext.cs
@@ -387,6 +387,7 @@ namespace Beamable
 				// the GameObject will get destroyed soon, and services will start throwing
 				// exceptions that the GameObject doesn't exist. Better to curb the init process all
 				// together, since the application is exiting playmode anyway.
+				_initPromise = new Promise(); // do not complete the promise...
 				return;
 			}
 #endif
