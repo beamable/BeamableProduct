d950421744787c24fef3793091b1d60d9fb69239
Chris Hanna <chris@beamable.com>
2023-02-28 15:51:39 -0500

    test

--- stress-tests/standalone-microservice/services/StressTest/Program.cs
+++ stress-tests/standalone-microservice/services/StressTest/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace StressTest;
+﻿using System.Diagnostics;
+
+namespace StressTest;
 
 public class Program
 {
 	public static void Main()
 	{
-		// (new StandaloneMicroserviceTesting()).Run();
+		// Console.WriteLine("Hello");
+		(new StandaloneMicroserviceTesting()).Run();
 		// sudo dotnet-counters collect --refresh-interval 3 --format csv -- standalone-microservice.dll
 
 	}
