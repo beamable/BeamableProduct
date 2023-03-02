5cfb05274d26e0c4c901fc28891a196958e2d0f0
Chris Hanna <chris@beamable.com>
2023-03-02 11:48:32 -0500

    shim

--- client/Packages/com.beamable/Editor/Environment/BeamablePackages.cs
+++ client/Packages/com.beamable/Editor/Environment/BeamablePackages.cs
@@ -84,9 +84,55 @@ namespace Beamable.Editor.Environment
 			if (!File.Exists(file)) return false;
 			
 			// if the path is relative to the /Library/PackageCache folder, then this file doesn't _really_ exist in the same way we think it does.
-			var relativePath = Path.GetRelativePath(System.Environment.CurrentDirectory, file);
+			var relativePath = GetRelativePath(System.Environment.CurrentDirectory, file);
 			var isInPackageCache = relativePath.StartsWith("Library/PackageCache");
 			return !isInPackageCache;
+		}
+		
+		/// <summary>
+		/// In NetCore 2.0, there is a system function for this, but Unity 2019 & 2020 don't support it. So here is
+		/// a shim from https://stackoverflow.com/questions/275689/how-to-get-relative-path-from-absolute-path
+		private static string GetRelativePath(string fromPath, string toPath)
+		{
+			if (string.IsNullOrEmpty(fromPath))
+			{
+				throw new ArgumentNullException("fromPath");
+			}
+
+			if (string.IsNullOrEmpty(toPath))
+			{
+				throw new ArgumentNullException("toPath");
+			}
+
+			Uri fromUri = new Uri(AppendDirectorySeparatorChar(fromPath));
+			Uri toUri = new Uri(AppendDirectorySeparatorChar(toPath));
+
+			if (fromUri.Scheme != toUri.Scheme)
+			{
+				return toPath;
+			}
+
+			Uri relativeUri = fromUri.MakeRelativeUri(toUri);
+			string relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+			if (string.Equals(toUri.Scheme, Uri.UriSchemeFile, StringComparison.OrdinalIgnoreCase))
+			{
+				relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+			}
+
+			return relativePath;
+		}
+
+		private static string AppendDirectorySeparatorChar(string path)
+		{
+			// Append a slash only if the path is a directory and does not have a slash.
+			if (!Path.HasExtension(path) &&
+			    !path.EndsWith(Path.DirectorySeparatorChar.ToString()))
+			{
+				return path + Path.DirectorySeparatorChar;
+			}
+
+			return path;
 		}
 
 		private static async Promise CheckForPendingUpdates()
