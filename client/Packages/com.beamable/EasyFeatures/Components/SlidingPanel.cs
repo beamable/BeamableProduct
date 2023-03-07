4a0975fb5e6b8c4624cf0d10e74bb66dfb0203ee
Tomasz Kaczmarz <tomasz@beamable.com>
2023-03-07 14:19:15 +0100

    Fix delete button aspect ratio; code cleanup

--- client/Packages/com.beamable/EasyFeatures/Components/SlidingPanel.cs
+++ client/Packages/com.beamable/EasyFeatures/Components/SlidingPanel.cs
@@ -13,17 +13,22 @@ namespace Beamable.EasyFeatures.Components
 		}
 
 		[SerializeField]
-		private SlidingMode Mode = SlidingMode.PositionBased;
-		
-		public Vector2 SlideAxis;
+		private SlidingMode SlideMode = SlidingMode.PositionBased;
+
 		public RectTransform Transform;
+		public Vector2 SlideAxis;
 		public float SlideSpeed = 20;
-		
+
 		[Header("Position Based Properties")]
 		public Vector2 VisibleAnchoredPosition;
 		public Vector2 HiddenAnchoredPosition;
 
 		[Header("Size Based Properties")]
+		[Tooltip(
+			"Optional reference to the transform hidden beneath this panel. It's used to automatically set folded and unfolded sizes. Unfolded is the size on `Start`.")]
+		public RectTransform TransformToShow;
+		[Tooltip("The margin added to the automatically set size based on the `Transform To Show` above.")]
+		public float Margin = 5;
 		[Tooltip("This size has to be bigger than Folded Size Delta")]
 		public Vector2 UnfoldedSizeDelta;
 		[Tooltip("This size has to be smaller than Unfolded Size Delta")]
@@ -32,18 +37,32 @@ namespace Beamable.EasyFeatures.Components
 		public event Action OnDragStarted;
 		public event Action OnHidden;
 
+		private void Start()
+		{
+			if (Transform == null)
+			{
+				Transform = GetComponent<RectTransform>();
+			}
+
+			if (TransformToShow)
+			{
+				UnfoldedSizeDelta = Transform.sizeDelta;
+				FoldedSizeDelta = UnfoldedSizeDelta - TransformToShow.sizeDelta * SlideAxis - Margin * SlideAxis;
+			}
+		}
+
 		public void OnBeginDrag(PointerEventData eventData)
 		{
 			OnDragStarted?.Invoke();
 		}
-		
+
 		public void OnDrag(PointerEventData eventData)
 		{
-			switch (Mode)
+			switch (SlideMode)
 			{
 				case SlidingMode.PositionBased:
 					Vector2 position = Transform.anchoredPosition + eventData.delta * SlideAxis;
-			
+
 					// make sure the current position is between hidden and visible positions
 					// otherwise clamp the position to the closer one (hidden or visible)
 					var lhs = VisibleAnchoredPosition - position;
@@ -53,23 +72,23 @@ namespace Beamable.EasyFeatures.Components
 					{
 						position = IsCloserToVisiblePosition() ? VisibleAnchoredPosition : HiddenAnchoredPosition;
 					}
-			
+
 					Transform.anchoredPosition = position;
 
 					break;
-				
+
 				case SlidingMode.SizeBased:
 					Vector2 newSize = Transform.sizeDelta + eventData.delta * SlideAxis;
 
 					newSize.x = Mathf.Clamp(newSize.x, FoldedSizeDelta.x, UnfoldedSizeDelta.x);
 					newSize.y = Mathf.Clamp(newSize.y, FoldedSizeDelta.y, UnfoldedSizeDelta.y);
-					
+
 					Transform.sizeDelta = newSize;
-					
+
 					break;
-				
+
 				default:
-					throw new ArgumentException($"Unhandled Sliding Mode: '{Mode}'");
+					throw new ArgumentException($"Unhandled Sliding Mode: '{SlideMode}'");
 			}
 		}
 
@@ -87,44 +106,43 @@ namespace Beamable.EasyFeatures.Components
 
 		private bool IsCloserToVisiblePosition()
 		{
-			if (Mode == SlidingMode.PositionBased)
+			if (SlideMode == SlidingMode.PositionBased)
 			{
 				return Vector2.Distance(Transform.anchoredPosition, VisibleAnchoredPosition) <
 				       Vector2.Distance(Transform.anchoredPosition, HiddenAnchoredPosition);
 			}
-			
+
 			return Vector2.Distance(Transform.sizeDelta, UnfoldedSizeDelta) >
 			       Vector2.Distance(Transform.sizeDelta, FoldedSizeDelta);
 		}
 
-
 		public bool IsHidden()
 		{
-			if (Mode == SlidingMode.PositionBased)
+			if (SlideMode == SlidingMode.PositionBased)
 			{
 				return Vector2.Distance(Transform.anchoredPosition, HiddenAnchoredPosition) <= 1;
 			}
 
 			return Vector2.Distance(Transform.sizeDelta, UnfoldedSizeDelta) <= 1;
 		}
-		
+
 		protected void SlideIn()
 		{
 			StopAllCoroutines();
-			Vector2 targetVector = Mode == SlidingMode.PositionBased ? VisibleAnchoredPosition : FoldedSizeDelta;
+			Vector2 targetVector = SlideMode == SlidingMode.PositionBased ? VisibleAnchoredPosition : FoldedSizeDelta;
 			StartCoroutine(SlidePopup(targetVector));
 		}
 
 		protected void SlideOut()
 		{
 			StopAllCoroutines();
-			Vector2 targetVector = Mode == SlidingMode.PositionBased ? HiddenAnchoredPosition : UnfoldedSizeDelta;
+			Vector2 targetVector = SlideMode == SlidingMode.PositionBased ? HiddenAnchoredPosition : UnfoldedSizeDelta;
 			StartCoroutine(SlidePopup(targetVector, OnHidden));
 		}
 
 		private IEnumerator SlidePopup(Vector2 targetVector, Action onSlideFinished = null)
 		{
-			if (Mode == SlidingMode.PositionBased)
+			if (SlideMode == SlidingMode.PositionBased)
 			{
 				while (Vector2.Distance(Transform.anchoredPosition, targetVector) > 1)
 				{
@@ -146,7 +164,7 @@ namespace Beamable.EasyFeatures.Components
 
 				Transform.sizeDelta = targetVector;
 			}
-			
+
 			onSlideFinished?.Invoke();
 		}
 
