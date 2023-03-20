31f14fc6bd5d331810f7b6f3389297cae39ec41c
Piotr Siuszko <siuszko@zoho.com>
2023-03-20 15:17:21 +0100

    Add UNITY_PURCHASING_4_6_OR_NEWER version define

--- client/Packages/com.beamable/Runtime/Modules/Purchasing/UnityBeamablePurchaser.cs
+++ client/Packages/com.beamable/Runtime/Modules/Purchasing/UnityBeamablePurchaser.cs
@@ -162,15 +162,21 @@ namespace Beamable.Purchasing
 
 				// Begin the asynchronous process of restoring purchases. Expect a confirmation response in
 				// the Action<bool> below, and ProcessPurchase if there are previously purchased products to restore.
+#if UNITY_PURCHASING_4_6_OR_NEWER
 				_appleExtensions.RestoreTransactions((result, error) =>
+#else
+				_appleExtensions.RestoreTransactions(result =>
+#endif
 				{
 					// The first phase of restoration. If no more responses are received on ProcessPurchase then
 					// no purchases are available to be restored.
 					InAppPurchaseLogger.Log("RestorePurchases continuing: " + result + ". If no further messages, no purchases available to restore.");
+#if UNITY_PURCHASING_4_6_OR_NEWER
 					if (!string.IsNullOrWhiteSpace(error))
 					{
 						InAppPurchaseLogger.Log($"Error: {error}");
 					}
+#endif
 				});
 			}
 			else
