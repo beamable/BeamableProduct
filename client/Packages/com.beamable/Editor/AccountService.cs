81bb81bd1e0fb1433cd7ba9d07fa5bc64425a8dc
Chris Hanna <cdhannaphone@gmail.com>
2023-03-29 13:33:56 -0400

    beam 3513 - depth limit issue (#2312)
    Co-authored-by: Chris Hanna <chris@beamable.com>

--- client/Packages/com.beamable/Editor/AccountService.cs
+++ client/Packages/com.beamable/Editor/AccountService.cs
@@ -281,7 +281,7 @@ namespace Beamable.Editor
 	}
 
 	[Serializable]
-	public class EditorAccountInfo
+	public class EditorAccountInfo : ISerializationCallbackReceiver
 	{
 		public string cid;
 
@@ -298,7 +298,10 @@ namespace Beamable.Editor
 		[SerializeField]
 		private CustomerViewResponse customerViewResponse;
 
+		[field: NonSerialized]
 		public List<RealmView> CustomerRealms { get; private set; }
+		
+		[field: NonSerialized]
 		public CustomerView CustomerView { get; private set; }
 
 		public List<RealmView> CustomerGames => CustomerRealms.Where(r => r.IsProduction).ToList(); // TODO: don't use linq.
@@ -394,6 +397,15 @@ namespace Beamable.Editor
 			var api = new RealmsApi(requester);
 			var res = await api.GetCustomer();
 			SetCustomerViewResponse(res);
+		}
+
+		public void OnBeforeSerialize()
+		{
+		}
+
+		public void OnAfterDeserialize()
+		{
+			Refresh();
 		}
 	}
 
