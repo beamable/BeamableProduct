ee493497e242aaba0a422cb44cf76709c422f701
Piotr Siuszko <siuszko@zoho.com>
2023-04-27 13:52:59 +0200

    [BEAM-3553] Support for iOS Notifications library

--- client/Packages/com.beamable/Runtime/Core/Platform/SDK/Notification/Internal/AppleLocalNotificationRelay.cs
+++ client/Packages/com.beamable/Runtime/Core/Platform/SDK/Notification/Internal/AppleLocalNotificationRelay.cs
@@ -1,45 +1,79 @@
-#if UNITY_IOS
+#if UNITY_IOS && (NOTIFICATIONS_PACKAGE || !UNITY_2022_1_OR_NEWER)
 #pragma warning disable CS0618
 using System;
 using System.Collections.Generic;
-using UnityEngine.iOS;
 using Beamable.Common.Spew;
+using Beamable.Serialization.SmallerJSON;
+using System.Text;
+#if NOTIFICATIONS_PACKAGE
+using Unity.Notifications.iOS;
+#else
+using UnityEngine.iOS;
+#endif
 
 namespace Beamable.Api.Notification.Internal
 {
-   /// <summary>
-   /// Apple local notification relay, for scheduling or cancelling
-   /// background notifications that are local to the device.
-   /// </summary>
-   public class AppleLocalNotificationRelay : ILocalNotificationRelay
-   {
-      public const string CancellationKey = "NOTIFICATION_KEY";
+	/// <summary>
+	/// Apple local notification relay, for scheduling or cancelling
+	/// background notifications that are local to the device.
+	/// </summary>
+	public class AppleLocalNotificationRelay : ILocalNotificationRelay
+	{
+#if NOTIFICATIONS_PACKAGE
+		int ScheduledLocalNotificationsAmount => iOSNotificationCenter.GetScheduledNotifications().Length;
+#else
+	   int ScheduledLocalNotificationsAmount => NotificationServices.scheduledLocalNotifications.Length
+#endif
+		public const string CancellationKey = "NOTIFICATION_KEY";
 
-      public void CreateNotificationChannel(string id, string name, string description)
-      {
-          NotificationLogger.LogFormat("Create Notification Channel not implemented on this platform.");
-      }
+		public void CreateNotificationChannel(string id, string name, string description)
+		{
+			NotificationLogger.LogFormat("Create Notification Channel not implemented on this platform.");
+		}
 
-      public void ScheduleNotification(string channel, string key, string title, string message, DateTime when, Dictionary<string, string> data)
-      {
-         // Make certain we haven't reached our maximum.
-         if (NotificationServices.scheduledLocalNotifications.Length >= NotificationService.MaxLocalNotifications)
-         {
-            NotificationLogger.LogFormat("Local Notification Limit of {0} has been reached. Ignoring {1}", NotificationService.MaxLocalNotifications, key);
-            return;
-         }
-         // Unless we cancel previous ones, the device will show multiple notifications.
-         CancelNotification(key);
-         Dictionary<string, string> userInfo = null;
-         if (data != null)
-         {
-            userInfo = new Dictionary<string, string>(data) {{CancellationKey, key}};
-         }
-         else
-         {
-            userInfo = new Dictionary<string, string>() {{CancellationKey, key}};
-         }
+		public void ScheduleNotification(string channel,
+		                                 string key,
+		                                 string title,
+		                                 string message,
+		                                 DateTime when,
+		                                 Dictionary<string, string> data)
+		{
+			// Make certain we haven't reached our maximum.
 
+			if (ScheduledLocalNotificationsAmount >= NotificationService.MaxLocalNotifications)
+			{
+				NotificationLogger.LogFormat("Local Notification Limit of {0} has been reached. Ignoring {1}",
+				                             NotificationService.MaxLocalNotifications, key);
+				return;
+			}
+
+			// Unless we cancel previous ones, the device will show multiple notifications.
+			CancelNotification(key);
+			Dictionary<string, string> userInfo = null;
+			if (data != null)
+			{
+				userInfo = new Dictionary<string, string>(data) {{CancellationKey, key}};
+			}
+			else
+			{
+				userInfo = new Dictionary<string, string>() {{CancellationKey, key}};
+			}
+#if NOTIFICATIONS_PACKAGE
+			var notification = new iOSNotification()
+			{
+				Title = title,
+				Body = message,
+				Trigger = new iOSNotificationTimeIntervalTrigger()
+				{
+					TimeInterval = when - DateTime.Now, Repeats = false
+				},
+				Data = Json.Serialize(userInfo, new StringBuilder()),
+				Identifier = key,
+				ShowInForeground = true,
+				ForegroundPresentationOption = PresentationOption.Alert | PresentationOption.Sound
+			};
+			iOSNotificationCenter.ScheduleNotification(notification);
+#else
          var note = new LocalNotification
          {
             alertBody = message,
@@ -48,29 +82,34 @@ namespace Beamable.Api.Notification.Internal
             userInfo = userInfo
          };
          NotificationServices.ScheduleLocalNotification(note);
-      }
+#endif
+		}
 
-      public void CancelNotification(string key)
-      {
-         var notifications = NotificationServices.scheduledLocalNotifications;
-         for (var i = 0; i < notifications.Length; i++)
-         {
-            var note = notifications[i];
-            if (key == (string)note.userInfo[CancellationKey])
-            {
-               NotificationServices.CancelLocalNotification(note);
-            }
-         }
-      }
+		public void CancelNotification(string key)
+		{
+#if NOTIFICATIONS_PACKAGE
+			iOSNotificationCenter.RemoveScheduledNotification(key);
+#else
+			var notifications = NotificationServices.scheduledLocalNotifications;
+			for (var i = 0; i < notifications.Length; i++)
+			{
+				var note = notifications[i];
+				if (key == (string)note.userInfo[CancellationKey])
+				{
+					NotificationServices.CancelLocalNotification(note);
+				}
+			}
+#endif
+		}
 
-      public void ClearDeliveredNotifications()
-      {
-         //clearAllNotification();
-      }
+		public void ClearDeliveredNotifications()
+		{
+			//clearAllNotification();
+		}
 
-      //[DllImport("__Internal")]
-      //private static extern void clearAllNotification();
-   }
+		//[DllImport("__Internal")]
+		//private static extern void clearAllNotification();
+	}
 }
 #pragma warning restore CS0618
 #endif
