235bc90f74aa4ccfba0f07943a536ad56f74a3fb
Pedro Rauiz Estigarribia Gestal <pedro@beamable.com>
2023-05-09 14:02:37 -0300

    fix failing test

--- cli/tests/BasicTests.cs
+++ cli/tests/BasicTests.cs
@@ -137,7 +137,7 @@ public class Tests
 		Console.WriteLine("----- OUTPUT ----");
 		Console.WriteLine(string.Join("\n", descriptors.Select(d => $"{d.FileName}\n\n{d.Content}\n")));
 
-		Assert.AreEqual(14, descriptors.Count);
+		Assert.AreEqual(15, descriptors.Count);
 	}
 
 	[Microservice("troublesome")]
