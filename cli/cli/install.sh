4f35eab5510840b4811bba622a2e3015db199516
Piotr Siuszko <siuszko@zoho.com>
2023-06-13 13:11:30 +0200

    [BEAM-3660] passing version as argument during install

--- cli/cli/install.sh
+++ cli/cli/install.sh
@@ -1,5 +1,5 @@
 #bin/sh
 
-dotnet pack /p:Version=0.0.0
+dotnet pack /p:Version=${1:0.0.0}
 dotnet tool uninstall beamable.tools -g || true
-dotnet tool install --global --version 0.0.0 --add-source ./nupkg/ beamable.tools
+dotnet tool install --global --version ${1:0.0.0} --add-source ./nupkg/ beamable.tools
