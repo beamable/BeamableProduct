80f3e92fbf2fb820f0732754997cb0f76672b9dd
MevLyshkin <piotr@beamable.com>
2023-06-14 11:37:52 +0200

    [BEAM-3660] passing version as argument during install (#2485)
    * [BEAM-3660] passing version as argument during install
    
    * Add empty lines at end of scripts
    
    * script typo

--- cli/cli/install.bat
+++ cli/cli/install.bat
@@ -1,4 +1,6 @@
-dotnet pack /p:Version=0.0.0
+IF "%1"=="" ( SET "VERSION=0.0.0" ) ELSE ( SET "VERSION=%1" )
+
+dotnet pack /p:Version=%VERSION%
 taskkill /IM "beam.exe" /F || true
 dotnet tool uninstall beamable.tools -g || true
-dotnet tool install --global --version 0.0.0 --add-source ./nupkg/ beamable.tools
+dotnet tool install --global --version %VERSION% --add-source ./nupkg/ beamable.tools
