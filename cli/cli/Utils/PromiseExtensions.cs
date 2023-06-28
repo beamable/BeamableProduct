7eed9d041acb7e9626a6151de483d99416832054
Piotr Siuszko <siuszko@zoho.com>
2023-06-28 14:58:45 +0200

    Simpler solution

--- cli/cli/Utils/PromiseExtensions.cs
+++ cli/cli/Utils/PromiseExtensions.cs
@@ -5,23 +5,12 @@ namespace cli.Utils;
 
 public static class PromiseExtensions
 {
-	public static async Promise<T> ShowLoading<T>(this Promise<T> self, string message = "loading...", Spinner? spinner = null)
+	public static Promise<T> ShowLoading<T>(this Promise<T> self, string message = "loading...",
+		Spinner? spinner = null)
 	{
 		spinner ??= Spinner.Known.Default;
-		return await AnsiConsole.Status()
+		return AnsiConsole.Status()
 			.Spinner(spinner)
-			.StartAsync(message, async ctx =>
-			{
-				try
-				{
-					return await self;
-				}
-				catch (Exception ex)
-				{
-					ctx.Status = "failed: " + ex.Message;
-					throw;
-				}
-			}
-			);
+			.StartAsync(message, async ctx => await self).ToPromise();
 	}
 }
