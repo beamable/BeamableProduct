7e2da3969b2a6f9431425db18de5ad220f853aca
Chris Hanna <cdhannaphone@gmail.com>
2023-07-05 09:52:31 -0400

    Revert "staging << main 1.16.2 rc (#2556)" (#2557)
    This reverts commit 25e6e426fdfaa2b101afb0386dc16abf932fd8ce.

--- cli/cli/Commands/Project/OpenSwaggerCommand.cs
+++ cli/cli/Commands/Project/OpenSwaggerCommand.cs
@@ -1,6 +1,4 @@
-using Beamable.Common;
 using Beamable.Common.Semantics;
-using cli.Services;
 using cli.Utils;
 using System.CommandLine;
 
@@ -20,27 +18,12 @@ public class OpenSwaggerCommand : AppCommand<OpenSwaggerCommandArgs>, IEmptyResu
 
 	public override void Configure()
 	{
-		AddArgument(new Argument<ServiceName>("service-name",()=> new ServiceName(), "Name of the service to open swagger to"), (arg, i) => arg.serviceName = i);
+		AddArgument(new Argument<ServiceName>("service-name", "Name of the service to open swagger to"), (arg, i) => arg.serviceName = i);
 		AddOption(new Option<bool>("--remote", "If passed, swagger will open to the remote version of this service. Otherwise, it will try and use the local version"), (arg, i) => arg.isRemote = i);
 	}
 
 	public override Task Handle(OpenSwaggerCommandArgs args)
 	{
-		if (string.IsNullOrWhiteSpace(args.serviceName))
-		{
-			var serviceDefinitions = args.BeamoLocalSystem.BeamoManifest.ServiceDefinitions
-				.Where(definition => definition.Protocol == BeamoProtocolType.HttpMicroservice).ToList();
-			if (serviceDefinitions.Count == 1)
-			{
-				args.serviceName = new ServiceName(serviceDefinitions[0].BeamoId);
-				BeamableLogger.Log(
-					$"No service-name passed as argument. Running command for {args.serviceName} since it is the only one microservice in BeamoManifest.");
-			}
-			else
-			{
-				throw new CliException("No service-name passed as argument.");
-			}
-		}
 		var cid = args.AppContext.Cid;
 		var pid = args.AppContext.Pid;
 		var url = $"{args.AppContext.Host.Replace("dev.", "dev-").Replace("api", "portal")}/{cid}/games/{pid}/realms/{pid}/microservices/{args.serviceName}/docs";
--- format-code.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dotnet tool restore
-dotnet tool run dotnet-format -f ${1:"client/Packages"}
