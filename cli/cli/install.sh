ad816b7b6f4080e3bf93670453c80791f41fc533
Piotr Siuszko <siuszko@zoho.com>
2023-07-06 12:20:40 +0200

    Script update

--- cli/cli/install.sh
+++ cli/cli/install.sh
@@ -3,6 +3,6 @@
 default_version='0.0.0'
 version=${1:-$default_version}
 
-dotnet pack /p:Version=$version
+dotnet pack -p:PackageVersion=$version
 dotnet tool uninstall beamable.tools -g || true
 dotnet tool install --global --version $version --add-source ./nupkg/ beamable.tools
