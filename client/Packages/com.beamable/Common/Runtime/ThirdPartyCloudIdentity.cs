be521fd3bee76ae1d8d1c3b410b03c12fd416efb
dpatekar <dpatekar@gmail.com>
2023-07-18 15:48:45 +0200

    We need to use different models for inventory proxy operations

--- client/Packages/com.beamable/Common/Runtime/ThirdPartyCloudIdentity.cs
+++ client/Packages/com.beamable/Common/Runtime/ThirdPartyCloudIdentity.cs
@@ -35,9 +35,9 @@ namespace Beamable.Common
 			string id,
 			string transaction,
 			Dictionary<string, long> currencies,
-			List<ItemCreateRequest> newItems,
-			List<ItemDeleteRequest> deleteItems,
-			List<ItemUpdateRequest> updateItems);
+			List<FederatedItemCreateRequest> newItems,
+			List<FederatedItemDeleteRequest> deleteItems,
+			List<FederatedItemUpdateRequest> updateItems);
 	}
 
 	[Serializable]
