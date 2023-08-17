e880a7b22a987703e4366a05db17344514451a92
Chris Hanna <cdhannaphone@gmail.com>
2023-08-17 07:44:38 -0400

    1.17.3 RC1 (#2679)
    * [BEAM-3758][BEAM-3757] CLI warning fixes  (#2664)
    
    * Beamable.server warning fixes
    
    * Extra fixes
    
    * Formatting: [BEAM-3758][BEAM-3757] CLI warning fixes (#2664) (#2666)
    
    Co-authored-by: Leinnan <Leinnan@users.noreply.github.com>
    
    * fix: archived realms showing in options when selecting realm (#2668)
    
    * use latest image for C#MS (#2671)
    
    Co-authored-by: Chris Hanna <chris@beamable.com>
    
    * [BEAM-3494] Fix: wrong error code when providing wrong password while logging in (#2672)
    
    * fix: wrong error code when providing wrong password while logging in
    
    * docs: update changelog file
    
    * chore: handle case for a status code 403 as well
    
    * beam 3303 - simplify log outputs (#2669)
    
    * beam 3303 - simplify log outputs
    
    * tweak
    
    * min not max
    
    ---------
    
    Co-authored-by: Chris Hanna <chris@beamable.com>
    
    * [BEAM-3671] Update Unity packages from CLI (#2667)
    
    * Update Unity Beam Package Command
    
    * Add command
    
    * CHANGELOG.md updates
    
    * Update options names
    
    * update only specific fields
    
    * [BEAM-3759] Beamable.tooling.common code documentation (#2665)
    
    * Beamable.server warning fixes
    
    * Extra fixes
    
    * docs for code in beamable.tooling.common
    
    * last warnings in CLI project
    
    * scoped nullable solution
    
    * TreatWarningsAsErrors in CLI project
    
    * CHANGELOG.md update
    
    * Formatting: [BEAM-3759] Beamable.tooling.common code documentation (#2665) (#2676)
    
    Co-authored-by: Leinnan <Leinnan@users.noreply.github.com>
    
    * [BEAM-2891] SDK - Email validation (#2674)
    
    * feat: add email validation and password validation
    
    * docs: merge main changelog file changes
    
    * chore: revert password validation changes
    
    * [BEAM-3785] [BEAM-3786] SAMS- Update Beamable deps on project creation (#2663)
    
    * [BEAM-3786] SAMS- Update Beamable deps on project creation
    
    * Apply feedback
    
    * Update CHANGELOG
    
    * adds template updating
    
    ---------
    
    Co-authored-by: Chris Hanna <chris@beamable.com>
    
    * Formatting: [BEAM-3785] [BEAM-3786] SAMS- Update Beamable deps on project creation (#2663) (#2678)
    
    Co-authored-by: Leinnan <Leinnan@users.noreply.github.com>
    
    * add missing nuget package (#2677)
    
    Co-authored-by: Chris Hanna <chris@beamable.com>
    
    * Update changelog (#2680)
    
    ---------
    
    Co-authored-by: MevLyshkin <piotr@beamable.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Leinnan <Leinnan@users.noreply.github.com>
    Co-authored-by: Mohammed Agboola <moh.agboola@gmail.com>
    Co-authored-by: Chris Hanna <chris@beamable.com>

--- microservice/beamable.tooling.common/Microservice/MicroserviceRequestContext.cs
+++ microservice/beamable.tooling.common/Microservice/MicroserviceRequestContext.cs
@@ -5,30 +5,47 @@ using System.Threading;
 
 namespace Beamable.Server;
 
+/// <summary>
+/// Represents a microservice-specific request context.
+/// </summary>
 public class MicroserviceRequestContext : RequestContext
 {
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MicroserviceRequestContext"/> class.
+	/// </summary>
 	public MicroserviceRequestContext(string cid, string pid, long id, int status, long userId, string path, string method, string body, HashSet<string> scopes = null, IDictionary<string, string> headers = null) : base(cid, pid, id, status, userId, path, method, body, scopes, headers)
 	{
 	}
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MicroserviceRequestContext"/> class with minimal information.
+	/// </summary>
 	public MicroserviceRequestContext(string cid, string pid) : base(cid, pid)
 	{
 	}
 
+	/// <summary>
+	/// The cancellation token associated with the request context.
+	/// </summary>
 	public CancellationToken CancellationToken { get; set; }
 	
+	/// <summary>
+	/// The JSON element representing the request body.
+	/// </summary>
 	public JsonElement BodyElement { get; set; }
+	/// <summary>
+	/// The JSON element representing the request headers.
+	/// </summary>
 	public JsonElement HeaderElement { get; set; }
-	private string _body;
-	private RequestHeaders _headers;
 
-	public override void ThrowIfCancelled()
-	{
-		CancellationToken.ThrowIfCancellationRequested();
-	}
-
+	/// <summary>
+	/// Gets a value indicating whether the cancellation token is canceled.
+	/// </summary>
 	public override bool IsCancelled => CancellationToken.IsCancellationRequested;
 
+	/// <summary>
+	/// Gets the request body as a string.
+	/// </summary>
 	public override string Body
 	{
 		get
@@ -43,6 +60,9 @@ public class MicroserviceRequestContext : RequestContext
 		}
 	}
 
+	/// <summary>
+	/// Gets the request headers.
+	/// </summary>
 	public override RequestHeaders Headers
 	{
 		get
@@ -64,5 +84,16 @@ public class MicroserviceRequestContext : RequestContext
 
 			return _headers;
 		}
+	}
+
+	private string _body;
+	private RequestHeaders _headers;
+	
+	/// <summary>
+	/// Throws a cancellation exception if the cancellation token is canceled.
+	/// </summary>
+	public override void ThrowIfCancelled()
+	{
+		CancellationToken.ThrowIfCancellationRequested();
 	}
 }
