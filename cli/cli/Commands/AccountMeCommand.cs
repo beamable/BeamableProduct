3db07dd6975f80ed250c786b65296cbed47ff76b
Piotr Siuszko <siuszko@zoho.com>
2023-08-17 19:11:44 +0200

    [BEAM-3799] CLI requests commands always output to console

--- cli/cli/Commands/AccountMeCommand.cs
+++ cli/cli/Commands/AccountMeCommand.cs
@@ -2,6 +2,7 @@ using Beamable.Common;
 using Beamable.Common.Api.Auth;
 using cli.Utils;
 using Newtonsoft.Json;
+using Spectre.Console;
 
 namespace cli;
 
@@ -11,17 +12,13 @@ public class AccountMeCommand : AppCommand<AccountMeCommandArgs>
 {
 
 	public AccountMeCommand() : base("me", "Temp command to get current account")
-	{
-	}
+	{}
 
-	public override void Configure()
-	{
-
-	}
+	public override void Configure(){}
 
 	public override async Task Handle(AccountMeCommandArgs args)
 	{
 		var response = await args.AuthApi.GetUser().ShowLoading("Sending Request...");
-		BeamableLogger.Log(JsonConvert.SerializeObject(response));
+		AnsiConsole.WriteLine(JsonConvert.SerializeObject(response));
 	}
 }
