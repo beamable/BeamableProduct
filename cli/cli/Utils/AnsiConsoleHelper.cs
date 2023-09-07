06dad1f53662ffaa673b78b42c01fc92db49d079
Mohammed Agboola <moh.agboola@gmail.com>
2023-09-07 17:18:53 +0000

    [BEAM-3670] CLI - Fixed Powershell users having blue-on-blue text (#2707)
    * fix: powershell users having blue-on-blue text when selecting options
    
    * Beam highlight
    
    * fix: code error in merge changes
    
    ---------
    
    Co-authored-by: Piotr Siuszko <siuszko@zoho.com>

---
+++ cli/cli/Utils/AnsiConsoleHelper.cs
@@ -0,0 +1,17 @@
+﻿using Spectre.Console;
+
+namespace cli.Utils;
+
+public static class AnsiConsoleHelper
+{
+	private static readonly Style BeamSelectionHightlightStyle =
+		new Style(Color.NavajoWhite1, new Color(130, 108, 207), Decoration.Bold);
+
+	public static SelectionPrompt<T> AddBeamHightlight<T>(this SelectionPrompt<T> obj)
+		where T : notnull =>
+		obj.HighlightStyle(BeamSelectionHightlightStyle);
+
+	public static MultiSelectionPrompt<T> AddBeamHightlight<T>(this MultiSelectionPrompt<T> obj)
+		where T : notnull =>
+		obj.HighlightStyle(BeamSelectionHightlightStyle);
+}
