19b8b1e80f438230eea195d4f94cc78a2f640479
Chris Hanna <cdhannaphone@gmail.com>
2023-09-19 09:58:56 -0400

    fix for cli (#2782)
    Co-authored-by: Chris Hanna <chris@beamable.com>

--- cli/cli/CommandContext.cs
+++ cli/cli/CommandContext.cs
@@ -158,6 +158,8 @@ public abstract partial class AppCommand<TArgs> : Command, IResultProvider
 	protected virtual void BindBaseContext(IServiceProvider provider, TArgs args, BindingContext bindingContext)
 	{
 		args.Dryrun = bindingContext.ParseResult.GetValueForOption(provider.GetRequiredService<DryRunOption>());
+		args.IgnoreStandaloneValidation =
+			bindingContext.ParseResult.GetValueForOption(provider.GetRequiredService<SkipStandaloneValidationOption>());
 	}
 
 	public class Binder : BinderBase<TArgs>
