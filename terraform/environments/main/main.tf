4abe3a6ab873288c119f4367a0632dccfe719435
Chris Hanna <chris@beamable.com>
2023-10-11 16:38:26 -0400

    move env

--- terraform/environments/main/main.tf
+++ terraform/environments/main/main.tf
@@ -20,7 +20,7 @@ variable "CERTIFICATE" {
 }
 
 module "s3" {
-  source              = "../modules/s3"
+  source              = "../../modules/s3"
   hosted_zone         = var.HOSTED_ZONE_ID
   lightbeam_domain_name = "lightbeams.${var.ROOT_DOMAIN_NAME}"
   certificate         = var.CERTIFICATE
