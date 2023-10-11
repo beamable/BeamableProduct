02e789e505d54d6adae4a47dc1b3bfaf5023056f
Chris Hanna <chris@beamable.com>
2023-10-11 17:16:05 -0400

    syntax s3

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -88,9 +88,10 @@ resource "aws_s3_bucket_policy" "lightbeam" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Principal = "*"
-        Action = "*"
+        Sid= "BucketReadObjects",
+        Effect = "Allow",
+        Principal = "*",
+        Action = "*",
         Resource = [
           "${aws_s3_bucket.lightbeam.arn}/*",
           "${aws_s3_bucket.lightbeam.arn}"
