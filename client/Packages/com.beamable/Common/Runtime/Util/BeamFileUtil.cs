05a8613caa1db0d069eeefb3f1e02fc16edb1642
Chris Hanna <cdhannaphone@gmail.com>
2023-10-11 07:58:08 -0400

    beam 3872 - sanitize file path for player code (#2833)
    * beam 3872 - sanitize file path for player code
    
    * Update client/Packages/com.beamable/Common/Runtime/Util/BeamFileUtil.cs
    
    Co-authored-by: MevLyshkin <piotr@beamable.com>
    
    * pr comment
    
    ---------
    
    Co-authored-by: Chris Hanna <chris@beamable.com>
    Co-authored-by: MevLyshkin <piotr@beamable.com>

---
+++ client/Packages/com.beamable/Common/Runtime/Util/BeamFileUtil.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+
+namespace Beamable.Common.Util
+{
+	public partial class BeamUtil
+	{
+		private static readonly char[] InvalidFileChars = Path.GetInvalidFileNameChars();
+		
+		/// <summary>
+		/// replaces all invalid file name characters with a dash
+		/// </summary>
+		public static string SanitizeStringForPath(string str)
+		{
+			if (string.IsNullOrEmpty(str)) return str;
+			
+			foreach (var invalidChar in InvalidFileChars)
+			{
+				str = str.Replace(invalidChar, '-');
+			}
+
+			return str;
+		}
+	}
+}
