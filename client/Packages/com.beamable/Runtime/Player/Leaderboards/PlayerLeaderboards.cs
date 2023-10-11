2bc42beec1043415cdfd49c4a6cf054c7def7243
cdhanna <cdhanna@users.noreply.github.com>
2023-10-11 00:38:10 +0000

    Formatting: Merge pull request #2831 from beamable/update-openapi

--- client/Packages/com.beamable/Runtime/Player/Leaderboards/PlayerLeaderboards.cs
+++ client/Packages/com.beamable/Runtime/Player/Leaderboards/PlayerLeaderboards.cs
@@ -8,7 +8,7 @@ namespace Beamable.Player
 	{
 		void Save();
 	}
-	
+
 	/// <summary>
 	/// A <see cref="PlayerLeaderboards"/> contains a set of <see cref="PlayerLeaderboard"/>,
 	/// accessible via the <see cref="PlayerLeaderboards.GetBoard(string)"/> method.
@@ -17,7 +17,7 @@ namespace Beamable.Player
 	/// </para>
 	/// </summary>
 	[Serializable]
-	public class PlayerLeaderboards : 
+	public class PlayerLeaderboards :
 		IPlayerLeaderboardsFriend,
 		IStorageHandler<PlayerLeaderboards>, IServiceStorable
 	{
@@ -25,12 +25,12 @@ namespace Beamable.Player
 		private StorageHandle<PlayerLeaderboards> _handle;
 
 		public LeaderboardDictionary boards = new LeaderboardDictionary();
-		
+
 		public PlayerLeaderboards(IDependencyProvider provider)
 		{
 			_provider = provider;
 		}
-		
+
 		public void ReceiveStorageHandle(StorageHandle<PlayerLeaderboards> handle)
 		{
 			_handle = handle;
@@ -58,7 +58,7 @@ namespace Beamable.Player
 		/// </param>
 		/// <returns>A <see cref="PlayerLeaderboard"/></returns>
 		public PlayerLeaderboard GetBoard(LeaderboardRef leaderboardRef) => GetBoard(leaderboardRef?.Id);
-		
+
 		/// <summary>
 		/// Get a <see cref="PlayerLeaderboard"/> for the given board id.
 		/// 
@@ -76,7 +76,7 @@ namespace Beamable.Player
 			{
 				throw new ArgumentException("board id cannot be null", nameof(boardId));
 			}
-			
+
 			if (!boards.TryGetValue(boardId, out var board))
 			{
 				boards[boardId] = board = DependencyBuilder.Instantiate<PlayerLeaderboard>(_provider);
