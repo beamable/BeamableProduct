70cb51e576427c6f436d62454a86453012b7faed
Chris Hanna <chris@beamable.com>
2023-10-11 16:30:48 -0400

    terraform

---
+++ terraform/modules/s3/variables.tf
@@ -0,0 +1,24 @@
+variable "hosted_zone" {
+  type        = string
+  description = "Hosted zone that DNS validation entries will be created in."
+}
+
+variable "certificate" {
+  type        = string
+  description = "ARN of the SSL/TLS certificate for the domain name."
+}
+
+variable "lightbeam_domain_name" {
+  type        = string
+  description = "Fully qualified domain name for hosting Lightbeams, like 'lightbeams.beamable.com'."
+}
+
+variable "lightbeam_price_class" {
+  type        = string
+  description = "CloudFront price class: PriceClass_Any (global), PriceClass_200 (most regions), or PriceClass_100 (US, Canada, Europe)"
+  validation {
+    condition     = contains(["PriceClass_All", "PriceClass_200", "PriceClass_100"], var.content_price_class)
+    error_message = "Price class must be one of [PriceClass_All, PriceClass_200, PriceClass_100]."
+  }
+  default = "PriceClass_100"
+}
