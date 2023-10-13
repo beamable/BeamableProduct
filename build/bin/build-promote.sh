b9273389297b3a81ffd953a0b37e5329dcb99c2e
Chris Hanna <chris@beamable.com>
2023-10-13 14:43:25 -0400

    revert

--- build/bin/build-promote.sh
+++ build/bin/build-promote.sh
@@ -6,12 +6,10 @@ export LOCAL_REPO_TAG=beamservice:${BEAMSERVICE_TAG}
 
 echo "Starting Microservice dependencies..."
 docker-compose --no-ansi -f docker/image.microservice/docker-compose.yml build --pull --no-cache # Fresh pull, no cache, builds
-docker-compose --no-ansi -f docker/image.microservice/docker-compose.yml down --exit-code-from microservice # Runs containers and checks the exit code
 docker-compose --no-ansi -f docker/image.microservice/docker-compose.yml up --exit-code-from microservice # Runs containers and checks the exit code
 docker-compose --no-ansi -f docker/image.microservice/docker-compose.yml down # TODO: Ensure that this down command executes
 
 echo "Starting nuget builds"
-docker-compose --no-ansi -f docker/cli/docker-compose.yml down # TODO: Ensure that this down command executes
 docker-compose --no-ansi -f docker/cli/docker-compose.yml up --build --exit-code-from cli
 docker-compose --no-ansi -f docker/cli/docker-compose.yml down # TODO: Ensure that this down command executes
 
@@ -22,12 +20,10 @@ echo "Building Microservice base image..."
 /usr/libexec/docker/cli-plugins/docker-buildx build --builder beamable-builder --platform linux/arm64,linux/amd64 --push -t beamableinc/${LOCAL_REPO_TAG} ../microservice/microservice --build-arg BEAMABLE_SDK_VERSION=${VERSION:-0.0.0}
 
 echo "Starting com.beamable package build"
-docker-compose --no-ansi -f docker/package.com.beamable/docker-compose.yml down # TODO: Ensure that this down command executes
 docker-compose --no-ansi -f docker/package.com.beamable/docker-compose.yml up --build --exit-code-from beamable
 docker-compose --no-ansi -f docker/package.com.beamable/docker-compose.yml down # TODO: Ensure that this down command executes
 
 echo "Starting com.beamable.server package build"
-docker-compose --no-ansi -f docker/package.com.beamable.server/docker-compose.yml down # TODO: Ensure that this down command executes
 docker-compose --no-ansi -f docker/package.com.beamable.server/docker-compose.yml up --build --exit-code-from beamable_server
 docker-compose --no-ansi -f docker/package.com.beamable.server/docker-compose.yml down # TODO: Ensure that this down command executes
 
