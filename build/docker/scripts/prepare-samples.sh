7af1111ac35e1850f27d122d30670edf0190efcc
Chris Hanna <chris@beamable.com>
2023-10-13 13:54:06 -0400

    dont base64 decode samples

--- build/docker/scripts/prepare-samples.sh
+++ build/docker/scripts/prepare-samples.sh
@@ -4,9 +4,9 @@ mkdir -p Samples~
 echo "Converting samples into UPM samples"
 ls
 # for the package, we need to identify the files listed in the ./package.json's Samples section
-for row in $(jq -r '.samples[] | @base64' ./package.json); do
+for row in $(jq -r '.samples[]' ./package.json); do
     _jq() {
-     echo ${row} | base64 --decode | jq -r ${1}
+     echo ${row} | jq -r ${1}
     }
 
     path=$(_jq '.path')
