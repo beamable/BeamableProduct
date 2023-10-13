4171baeb6f9637922da0c82972881ed058eb7352
Chris Hanna <chris@beamable.com>
2023-10-13 14:19:41 -0400

    try agian

--- build/docker/scripts/prepare-samples.sh
+++ build/docker/scripts/prepare-samples.sh
@@ -2,13 +2,12 @@
 
 mkdir -p Samples~
 echo "Converting samples into UPM samples"
-ls
 # for the package, we need to identify the files listed in the ./package.json's Samples section
-for row in $(jq -r '.samples[]' ./package.json); do
-    _jq() {
-     echo ${row} | jq -r ${1}
-    }
+for row in $(cat ./package.json | jq -r '.samples[] | @base64'); do
 
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
     path=$(_jq '.path')
     sourcePath="${path/SAMPLES_PATH/Samples}"
     dstPath="${path/SAMPLES_PATH/Samples~}"
@@ -17,14 +16,11 @@ for row in $(jq -r '.samples[]' ./package.json); do
     echo "Converting samples - found sound ${sourcePath}"
     echo "Converting samples - found dst ${dstPath}"
 
-    # move the source path to the dst path
+    # # move the source path to the dst path
     mv $sourcePath $dstPath
 
-    # update the package json file
+    # # update the package json file
     sed -i 's,'$path','$dstPath',' package.json
-
-    echo "Converting samples - Preview package.json"
-    cat package.json
 done
 
 # delete the /Samples directory
