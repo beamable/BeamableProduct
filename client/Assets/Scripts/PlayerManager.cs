7a691a494c8400a28f51451948f9b55f7080e4a0
Piotr Siuszko <siuszko@zoho.com>
2023-10-16 10:23:38 +0200

    Scripts for testing

---
+++ client/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,47 @@
+﻿using Beamable;
+using Beamable.Api.CloudSaving;
+using Beamable.Common;
+using System;
+using UnityEngine;
+
+namespace GameExample
+{
+	[Serializable]
+	public struct PlayerData
+	{
+		public int gamePoints;
+		public int gamesStarted;
+		public bool isThatThingEnabled;
+	}
+
+	public class PlayerManager : MonoBehaviour
+	{
+		public BeamContext Context;
+		public PlayerData Data;
+
+		CloudSavingService _cloudSave;
+
+		async void Start()
+		{
+			Context = BeamContext.Default;
+			await Context.OnReady;
+			_cloudSave = Context.ServiceProvider.GetService<CloudSavingService>();
+			_cloudSave.UpdateReceived += HandleUpdateReceived;
+			Data = await _cloudSave.Get<PlayerData>(nameof(PlayerData));
+			Debug.Log($"Init completed: {Data.ToString()}");
+			Data.gamesStarted++;
+			await Send();
+		}
+
+		private void HandleUpdateReceived(ManifestResponse obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		[ContextMenu("DUA")]
+		public async Promise Send()
+		{
+			await _cloudSave.Set(nameof(PlayerData), Data, autoUpload: true);
+		}
+	}
+}
