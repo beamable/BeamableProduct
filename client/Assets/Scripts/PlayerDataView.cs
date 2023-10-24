bd333637060f33e89a4a3fe71802d549d62fb0f2
Piotr Siuszko <siuszko@zoho.com>
2023-10-24 10:55:00 +0200

    tests

---
+++ client/Assets/Scripts/PlayerDataView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameExample
+{
+	public class PlayerDataView : MonoBehaviour
+	{
+		[SerializeField]private TextMeshProUGUI text;
+		public Button Button;
+		
+
+		public void UpdateData(PlayerData data)
+		{
+			text.text = $"{data.gamesStarted} games started\n" +
+			            $"{data.gamePoints} game points\n" +
+			            $"isThatThingEnabled: {data.isThatThingEnabled}" +
+			            $"Updated at {DateTime.Now}";
+		}
+	}
+}
--- client/Assets/Scripts/PlayerManager.cs
+++ client/Assets/Scripts/PlayerManager.cs
@@ -22,24 +22,36 @@ namespace GameExample
 		public PlayerSaves _cloudSave;
 		public CloudSavingService _CloudSavingService;
 		public bool useOldUploadService;
+		public PlayerDataView view;
 
 		async void Start()
 		{
 			Context = BeamContext.Default;
 			await Context.OnReady;
 			_cloudSave = Context.ServiceProvider.GetService<PlayerSaves>();
-			_CloudSavingService = Context.ServiceProvider.GetService<CloudSavingService>();
-			await _CloudSavingService.Init();
+			// _CloudSavingService = Context.ServiceProvider.GetService<CloudSavingService>();
+			// await _CloudSavingService.Init();
 			_cloudSave.OnUpdated += HandleUpdateReceived;
 			await _cloudSave.Refresh();
 			Data = _cloudSave.Get<PlayerData>(nameof(PlayerData));
+			view.UpdateData(Data);
+			view.Button.onClick.AddListener(BtnClicked);
 			Debug.Log($"Init completed: {Data.ToString()}");
 			Data.gamesStarted++;
 			await Send();
 		}
 
+		private void BtnClicked()
+		{
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+			Send();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+		}
+
 		private void HandleUpdateReceived()
 		{
+			Data = _cloudSave.Get<PlayerData>(nameof(PlayerData));
+			view.UpdateData(Data);
 			Debug.Log("Update received!");
 		}
 
