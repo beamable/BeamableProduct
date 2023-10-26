9dc63b6ff746da7772f2ee5da7392af73677d993
cdhanna <cdhanna@users.noreply.github.com>
2023-10-26 20:33:18 +0000

    Formatting: [BEAM-3880] Local dotnet and beam on Windows (#2869)

--- client/Packages/com.beamable/Editor/BeamCli/BeamCliUtil.cs
+++ client/Packages/com.beamable/Editor/BeamCli/BeamCliUtil.cs
@@ -50,7 +50,7 @@ namespace Beamable.Editor.BeamCli
 				if (USE_SRC)
 				{
 					return Path.Combine(DotnetUtil.DotnetHome,
-					                    $"dotnet run -f net6.0 --project {EditorConfiguration.Instance.AdvancedCli.Value.UseFromSource.Value} -- ");
+										$"dotnet run -f net6.0 --project {EditorConfiguration.Instance.AdvancedCli.Value.UseFromSource.Value} -- ");
 				}
 				if (USE_GLOBAL)
 				{
@@ -111,7 +111,7 @@ namespace Beamable.Editor.BeamCli
 			}
 		}
 
-		
+
 		static bool InstallTool()
 		{
 			Directory.CreateDirectory(CLI_VERSIONED_HOME);
