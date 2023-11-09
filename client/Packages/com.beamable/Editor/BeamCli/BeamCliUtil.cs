96615a8dca0ebeb4fc74fba1a3569812df1a8a38
gabrielbeamable <141041673+gabrielbeamable@users.noreply.github.com>
2023-11-09 09:35:00 -0300

    Feature/beam 3894 - Regenerate USAM files (#2891)
    * Revert some files that were accidentally commited before
    
    * Remove quiet from CommandArgs and move it to NewSolutionCommandArgs

--- client/Packages/com.beamable/Editor/BeamCli/BeamCliUtil.cs
+++ client/Packages/com.beamable/Editor/BeamCli/BeamCliUtil.cs
@@ -50,7 +50,7 @@ namespace Beamable.Editor.BeamCli
 				if (USE_SRC)
 				{
 					return Path.Combine(DotnetUtil.DotnetHome,
-										$"dotnet run -f net6.0 --project {EditorConfiguration.Instance.AdvancedCli.Value.UseFromSource.Value} -- ");
+										$"dotnet run --no-build -f net6.0 --project {EditorConfiguration.Instance.AdvancedCli.Value.UseFromSource.Value} -- ");
 				}
 				if (USE_GLOBAL)
 				{
