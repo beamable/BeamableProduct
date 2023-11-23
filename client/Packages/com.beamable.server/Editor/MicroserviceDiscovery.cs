1bf474b59b92a31e787b9f9d870024baaa69426c
Piotr Siuszko <siuszko@zoho.com>
2023-11-23 16:31:02 +0100

    Revert "Test dummy CLI"
    This reverts commit 1d15f75b4919dbfff69d79a5150813a3acc96813.

--- client/Packages/com.beamable.server/Editor/MicroserviceDiscovery.cs
+++ client/Packages/com.beamable.server/Editor/MicroserviceDiscovery.cs
@@ -26,7 +26,7 @@ namespace Beamable.Server.Editor
 		public async Promise Start()
 		{
 			await BeamEditorContext.Default.InitializePromise;
-			var cli = BeamEditorContext.Default.ServiceScope.GetService<IBeamCli>();
+			var cli = BeamEditorContext.Default.ServiceScope.GetService<BeamCli>();
 
 			_command = cli.Command.ProjectPs();
 			_command.OnStreamServiceDiscoveryEvent(Handle);
