b904abf351b5549e377cfabf1c07272e9ffd59e0
Leinnan <Leinnan@users.noreply.github.com>
2023-11-23 15:24:00 +0000

    Formatting: [BEAM-3922] SmallerJSON class serialization fixes (#2922)

--- client/Packages/com.beamable/Tests/Editor/SmallerJSON/JsonSerializeTest.cs
+++ client/Packages/com.beamable/Tests/Editor/SmallerJSON/JsonSerializeTest.cs
@@ -17,9 +17,9 @@ namespace Beamable.Editor.Tests.SmallerJson
 			instance.LastName = "Super";
 			var jsonSerializedUnity = JsonUtility.ToJson(instance);
 			var smallerJsonSerialized = Json.Serialize(instance, new StringBuilder());
-			Assert.AreEqual(jsonSerializedUnity,smallerJsonSerialized);
+			Assert.AreEqual(jsonSerializedUnity, smallerJsonSerialized);
 		}
-		
+
 
 		[Serializable]
 		public class CustomComplexClass
@@ -28,7 +28,7 @@ namespace Beamable.Editor.Tests.SmallerJson
 				!string.IsNullOrWhiteSpace(_displayName) &&
 				!string.IsNullOrWhiteSpace(_firstName) &&
 				!string.IsNullOrWhiteSpace(_lastName);
-        
+
 			public string DisplayName
 			{
 				get => _displayName;
@@ -44,7 +44,7 @@ namespace Beamable.Editor.Tests.SmallerJson
 				get => _lastName;
 				set => _lastName = value;
 			}
-    
+
 			[SerializeField] private string _displayName;
 			[SerializeField] private string _firstName;
 			[SerializeField] private string _lastName;
