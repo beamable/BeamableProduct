48b834331663edad6bd1646e69652bd65224a3e9
Piotr Siuszko <siuszko@zoho.com>
2023-11-29 11:20:35 +0100

    remvoe usings

--- client/Packages/com.beamable.server/Editor/UI/ServiceLogWindow.cs
+++ client/Packages/com.beamable.server/Editor/UI/ServiceLogWindow.cs
@@ -6,13 +6,7 @@ using System;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
-#if UNITY_2018
-using UnityEngine.Experimental.UIElements;
-using UnityEditor.Experimental.UIElements;
-#elif UNITY_2019_1_OR_NEWER
 using UnityEngine.UIElements;
-using UnityEditor.UIElements;
-#endif
 
 namespace Beamable.Editor.Microservice.UI
 {
