d065900d1c85a4f9ac3507c71077f139a752d103
MevLyshkin <piotr@beamable.com>
2024-01-03 20:48:40 +0100

    [BEAM-3965] CLI- Improved error messages  (#2992)
    * Errata PoC
    
    * [BEAM-3963] Fix log output and update serilog deps
    
    * Update CHANGELOG
    
    * Update Spectre deps
    
    * Fixes
    
    * BeamResourceRepository, enhance content error handling
    
    * Add invalid content error code and improve error message
    
    * Import fix

--- cli/cli/CliException.cs
+++ cli/cli/CliException.cs
@@ -1,3 +1,4 @@
+using Errata;
 using System.Diagnostics;
 
 namespace cli;
@@ -12,16 +13,31 @@ public class CliException : Exception
 
 	public bool ReportOnStdOut;
 
+	public List<Diagnostic> Reports;
+
 	public CliException(string message) : base(message)
 	{
 		NonZeroOrOneExitCode = 1;
 		ReportOnStdOut = true;
 	}
 
-	public CliException(string message, int nonZeroOrOneExitCode, bool useStdOut) : base(message)
+	public CliException(string message, int nonZeroOrOneExitCode, bool useStdOut, string additionalNote = null, Diagnostic[] additionalReports = null) : base(message)
 	{
 		NonZeroOrOneExitCode = nonZeroOrOneExitCode;
 		ReportOnStdOut = useStdOut;
+		
+		var baseReport = Diagnostic.Error(message)
+			.WithCode($"{nonZeroOrOneExitCode:0000.##}");
+		if (!string.IsNullOrWhiteSpace(additionalNote))
+		{
+			baseReport.WithNote(additionalNote);
+		}
+		Reports = new List<Diagnostic>(){baseReport};
+		if (additionalReports != null)
+		{
+			Reports.AddRange(additionalReports);
+		}
+		
 		Debug.Assert(NonZeroOrOneExitCode > 1, "NonZeroOrOneExitCode must be > 1 --- 0 is OK and 1 is \"private exception\" (not meant to be exposed to our cli-interface layer). In Engine, we check for these exceptions and ask for a bug report.");
 	}
 }
