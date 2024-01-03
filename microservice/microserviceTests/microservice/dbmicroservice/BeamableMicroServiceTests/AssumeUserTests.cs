0e5f1bb5efd0ea56492076644a35797d57a09890
Chris Hanna <chris@beamable.com>
2024-01-03 13:46:19 -0500

    beam 3919 - fix open api require user endpoints in microservice code

--- microservice/microserviceTests/microservice/dbmicroservice/BeamableMicroServiceTests/AssumeUserTests.cs
+++ microservice/microserviceTests/microservice/dbmicroservice/BeamableMicroServiceTests/AssumeUserTests.cs
@@ -9,7 +9,46 @@ namespace microserviceTests.microservice.dbmicroservice.BeamableMicroServiceTest
 [TestFixture]
 public class AssumeUserTests : CommonTest
 {
+	
+		
+	[TestCase(true)]
+	[TestCase(false)]
+	[NonParallelizable]
+	public async Task AssumeUserAsAdmin_SubCallRequiresUser(bool forceCheck)
+	{
 
+		TestSocket testSocket = null;
+		var ms = new TestSetup(new TestSocketProvider(socket =>
+		{
+			testSocket = socket;
+			socket.AddStandardMessageHandlers()
+				.AddMessageHandler(
+					MessageMatcher
+						.WithReqId(-5)
+						.WithFrom(2)
+					,
+					MessageResponder.Success(new EmptyResponse()),
+					MessageFrequency.OnlyOnce())
+				.AddMessageHandler(
+					MessageMatcher
+						.WithReqId(1)
+						.WithStatus(200),
+					MessageResponder.NoResponse(),
+					MessageFrequency.OnlyOnce()
+				);
+		}));
+
+		await ms.Start<SimpleMicroservice>(new TestArgs());
+		Assert.IsTrue(ms.HasInitialized);
+
+		testSocket.SendToClient(
+			ClientRequest.ClientCallableAsAdmin("micro_simple", nameof(SimpleMicroservice.TestAssumeUser_RequestRequiresUser), 1, 1, 2, forceCheck));
+
+		// simulate shutdown event...
+		await ms.OnShutdown(this, null);
+		Assert.IsTrue(testSocket.AllMocksCalled());
+	}
+		
 	[TestCase(true)]
 	[TestCase(false)]
 	[NonParallelizable]
