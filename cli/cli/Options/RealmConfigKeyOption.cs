5cd9cc719972dea52b4a4c4a322cd7c6ab2703d6
zpatekar <77490300+zpatekar@users.noreply.github.com>
2024-01-11 16:07:57 +0100

    Add realm config CRUD operations to the CLI (#3018)
    * Add realm config CRUD operations to the CLI.
    
    * Add an option to view, set and remove a list of realm config values.

---
+++ cli/cli/Options/RealmConfigKeyOption.cs
@@ -0,0 +1,7 @@
+namespace cli;
+
+public class RealmConfigKeyOption : ConfigurableOptionList
+{
+	public RealmConfigKeyOption() : base("keys", "A list of realm config keys in a namespace|key format")
+	{ }
+}
