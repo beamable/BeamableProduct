03911af1b7615b194220b170251314c6ded16d08
Chris Hanna <chris@beamable.com>
2024-01-22 16:10:02 -0500

    pr comments

--- cli/cli/Options/ShowRawOutput.cs
+++ cli/cli/Options/ShowRawOutput.cs
@@ -5,7 +5,7 @@ namespace cli;
 public class ShowRawOutput : Option<bool>
 {
 	public ShowRawOutput()
-		: base("--raw", "When enabled, machine-readable JSON will be logged to the console. Note, when the command is being piped, this option has no effect.")
+		: base("--raw", "Output raw JSON to standard out. This happens by default when the command is being piped")
 	{
 	}
 }
@@ -13,7 +13,7 @@ public class ShowRawOutput : Option<bool>
 public class ShowPrettyOutput : Option<bool>
 {
 	public ShowPrettyOutput()
-		: base("--pretty", "When enabled, any available higher level console graphics will be displayed on the console. Note, this is the default behaviour, except when the command is being piped.")
+		: base("--pretty", "Output syntax highlighted box text. This happens by default when the command is not piped")
 	{
 		
 	}
