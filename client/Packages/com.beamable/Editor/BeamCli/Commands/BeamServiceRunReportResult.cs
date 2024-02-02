1f7ed1c9d84189d94635440612fbaf5bb4745bc9
gabrielbeamable <gabrielbeamable@users.noreply.github.com>
2024-02-02 20:22:32 +0000

    Formatting: [BEAM-4001] [BEAM-4002] Open microservice main file in current project instead of a new one (#3066)

--- client/Packages/com.beamable/Editor/BeamCli/Commands/BeamServiceRunReportResult.cs
+++ client/Packages/com.beamable/Editor/BeamCli/Commands/BeamServiceRunReportResult.cs
@@ -1,13 +1,13 @@
 
 namespace Beamable.Editor.BeamCli.Commands
 {
-    using Beamable.Common;
-    using Beamable.Common.BeamCli;
-    
-    [System.SerializableAttribute()]
-    public class BeamServiceRunReportResult
-    {
-        public bool Success;
-        public string FailureReason;
-    }
+	using Beamable.Common;
+	using Beamable.Common.BeamCli;
+
+	[System.SerializableAttribute()]
+	public class BeamServiceRunReportResult
+	{
+		public bool Success;
+		public string FailureReason;
+	}
 }
