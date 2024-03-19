caff96722a026c917068870fa45e2a277a985d39
Chris Hanna <chris@beamable.com>
2024-03-19 09:34:14 -0400

    missing code

--- client/Packages/com.beamable.server/SharedRuntime/ServiceDiscoveryEntry.cs
+++ client/Packages/com.beamable.server/SharedRuntime/ServiceDiscoveryEntry.cs
@@ -12,5 +12,45 @@ namespace Beamable.Server
 		public int healthPort;
 		public bool isContainer;
 		public string containerId;
+		public DiscoveryStatus status;
+	}
+	
+	public enum DiscoveryStatus
+	{
+		Starting,
+		AcceptingTraffic,
+		Stopping
+	}
+
+	public static class DiscoveryStatusUtil
+	{
+		public static string GetDisplayString(this DiscoveryStatus status)
+		{
+			switch (status)
+			{
+				case DiscoveryStatus.Starting: return "starting";
+				case DiscoveryStatus.Stopping: return "stopping";
+				case DiscoveryStatus.AcceptingTraffic: return "accepting-traffic";
+				default: throw new ArgumentException();
+			}
+		}
+		public static bool TryGetStatusFromString(string statusString, out DiscoveryStatus status)
+		{
+			switch (statusString)
+			{
+				case "starting": 
+					status = DiscoveryStatus.Starting;
+					return true;
+				case "stopping": 
+					status = DiscoveryStatus.Stopping;
+					return true;
+				case "accepting-traffic": 
+					status = DiscoveryStatus.AcceptingTraffic;
+					return true;
+				default:
+					status = DiscoveryStatus.Starting;
+					return false;
+			}
+		}
 	}
 }
