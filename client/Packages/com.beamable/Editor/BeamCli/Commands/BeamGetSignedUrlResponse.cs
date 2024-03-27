1f2c159ce666f50eb264fde4324c94a75c23251b
gabrielbeamable <141041673+gabrielbeamable@users.noreply.github.com>
2024-03-27 14:57:48 -0300

    [Issue/3178] List non beamo references of services and add those to Unity sln (#3214)
    * Update Unity-CLI generated code
    
    * Add services libraries projs to Unity sln

--- client/Packages/com.beamable/Editor/BeamCli/Commands/BeamGetSignedUrlResponse.cs
+++ client/Packages/com.beamable/Editor/BeamCli/Commands/BeamGetSignedUrlResponse.cs
@@ -1,15 +1,15 @@
 
 namespace Beamable.Editor.BeamCli.Commands
 {
-	using Beamable.Common;
-	using Beamable.Common.BeamCli;
-
-	[System.SerializableAttribute()]
-	public class BeamGetSignedUrlResponse
-	{
-		public System.Collections.Generic.List<BeamGetLogsUrlHeader> headers;
-		public string url;
-		public string body;
-		public string method;
-	}
+    using Beamable.Common;
+    using Beamable.Common.BeamCli;
+    
+    [System.SerializableAttribute()]
+    public class BeamGetSignedUrlResponse
+    {
+        public System.Collections.Generic.List<BeamGetLogsUrlHeader> headers;
+        public string url;
+        public string body;
+        public string method;
+    }
 }
