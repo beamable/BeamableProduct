024f779b24ba529a1821883dcd509edf9407f169
Chris Hanna <chris+gh@beamable.com>
2024-04-01 15:01:03 -0400

    Issue/3164 actually does the trick (#3236)
    * more delay
    
    * maybe
    
    * cleanup
    
    ---------
    
    Co-authored-by: Chris Hanna <chris@beamable.com>

--- client/Packages/com.beamable/Editor/Modules/Content/ContentIO.cs
+++ client/Packages/com.beamable/Editor/Modules/Content/ContentIO.cs
@@ -983,18 +983,12 @@ namespace Beamable.Editor.Content
 			content.SetContentName(nextName);
 			NotifyRenamed(oldId, content, nextAssetpath);
 			content.BroadcastUpdate();
+			
 			var result = AssetDatabase.MoveAsset(existingAssetPath, nextAssetpath);
 			if (!string.IsNullOrEmpty(result))
 			{
 				throw new Exception(result);
 			}
-
-			EditorApplication.delayCall += () =>
-			{
-				EditorUtility.SetDirty(content);
-				AssetDatabase.ForceReserializeAssets(new[] {nextAssetpath},
-				                                     ForceReserializeAssetsOptions.ReserializeAssetsAndMetadata);
-			};
 		}
 
 
