84417b4f2b7bf3c147497e6caf2d47bb634460ba
cdhanna <cdhanna@users.noreply.github.com>
2024-04-01 13:19:43 +0000

    Formatting: fixes 3149 - adds retry to platform requester based on ssl connection issue (#3223)

--- client/Packages/com.beamable/Editor/BeamCli/Commands/BeamBuildProjectCommandOutput.cs
+++ client/Packages/com.beamable/Editor/BeamCli/Commands/BeamBuildProjectCommandOutput.cs
@@ -1,13 +1,13 @@
 
 namespace Beamable.Editor.BeamCli.Commands
 {
-    using Beamable.Common;
-    using Beamable.Common.BeamCli;
-    
-    [System.SerializableAttribute()]
-    public class BeamBuildProjectCommandOutput
-    {
-        public string service;
-        public BeamProjectErrorReport report;
-    }
+	using Beamable.Common;
+	using Beamable.Common.BeamCli;
+
+	[System.SerializableAttribute()]
+	public class BeamBuildProjectCommandOutput
+	{
+		public string service;
+		public BeamProjectErrorReport report;
+	}
 }
