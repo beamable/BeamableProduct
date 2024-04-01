7adae3fc6051115e73c0b8682119eb4366bdc352
Chris Hanna <chris@beamable.com>
2024-04-01 11:06:03 -0400

    fix perf test

--- stress-tests/standalone-microservice/services/standalone-microservice/Program.cs
+++ stress-tests/standalone-microservice/services/standalone-microservice/Program.cs
@@ -15,60 +15,11 @@ namespace Beamable.standalone_microservice
 		public static async Task Main()
 		{
 			Environment.SetEnvironmentVariable("DOTNET_DiagnosticPorts", null);
-			await Prepare<StandaloneMicroservice>();
-			
-			// load environment variables from local file
-			LoadEnvironmentVariables();
+			await MicroserviceBootstrapper.Prepare<StandaloneMicroservice>();
 			
 			// run the Microservice code
 			await MicroserviceBootstrapper.Start<StandaloneMicroservice>();
 		}
 		
-		static void LoadEnvironmentVariables(string filePath=".env")
-		{
-			if (!File.Exists(filePath))
-				throw new Exception($"No environment file found at path=[{filePath}]");
-
-			foreach (var line in File.ReadAllLines(filePath))
-			{
-				var parts = line.Split(
-					'=',
-					StringSplitOptions.RemoveEmptyEntries);
-
-				if (parts.Length != 2)
-					continue;
-
-				Environment.SetEnvironmentVariable(parts[0], parts[1]);
-			}
-		}
-		
-		static async Task Prepare<TMicroservice>() where TMicroservice : Microservice
-		{
-			
-			var inDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
-			if (inDocker) return;
-			
-			MicroserviceAttribute attribute = typeof(TMicroservice).GetCustomAttribute<MicroserviceAttribute>();
-			var serviceName = attribute.MicroserviceName;
-			
-			using var process = new Process();
-
-			process.StartInfo.FileName = "beam";
-			process.StartInfo.Arguments = $"project generate-env {serviceName} . --auto-deploy --include-prefix=false --instance-count=10";
-			process.StartInfo.RedirectStandardOutput = true;
-			process.StartInfo.RedirectStandardError = true;
-			process.StartInfo.CreateNoWindow = true;
-			process.StartInfo.UseShellExecute = false;
-
-			process.Start();
-			await process.WaitForExitAsync();
-			
-			var result = await process.StandardOutput.ReadToEndAsync();
-			// Console.WriteLine(result);
-			if (process.ExitCode != 0)
-			{
-				throw new Exception($"Failed to generate-env message=[{result}]");
-			}
-		}
 	}
 }
