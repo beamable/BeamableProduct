9eb8b73c234719d64eb749892cbd6894c98f7eb9
Chris Hanna <chris@beamable.com>
2024-04-04 16:12:10 -0400

    adds spew logging for cli

--- client/Packages/com.beamable/Common/Runtime/Constants/Implementations/SpewConstants.cs
+++ client/Packages/com.beamable/Common/Runtime/Constants/Implementations/SpewConstants.cs
@@ -7,6 +7,7 @@
 			public static partial class Spew
 			{
 				public const string SPEW_ALL = "SPEW_ALL";
+				public const string SPEW_CLI = "SPEW_CLI";
 				public const string SPEW_NOTIFICATION = "SPEW_NOTIFICATION";
 				public const string SPEW_CHAT = "SPEW_CHAT";
 				public const string SPEW_PUBNUB_SUBSCRIPTION = "SPEW_PUBNUB_SUBSCRIPTION";
