09b8f386721c0189c170454a0580d6651a708b0f
Chris Hanna <chris+gh@beamable.com>
2024-04-05 14:41:47 -0400

    usam nightly fixes (#3273)
    Co-authored-by: Chris Hanna <chris@beamable.com>

---
+++ client/Packages/com.beamable/Common/Runtime/BeamCli/Contracts/Constants.cs
@@ -0,0 +1,11 @@
+namespace Beamable.Common.BeamCli.Contracts
+{
+	public static class CliConstants
+	{
+		/// <summary>
+		/// A magical constant that is used in the 'beam project generate-props` call to signify that the
+		/// $(SolutionDir) should be set to the same place as the Directory.Build.Props file.
+		/// </summary>
+		public const string GENERATE_PROPS_SLN_NEXT_TO_PROPS = "<dir.props>";
+	}
+}
