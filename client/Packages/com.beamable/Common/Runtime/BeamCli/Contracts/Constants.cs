6688806f61fe376151952e8a735bc3e956aafa07
Chris Hanna <chris@beamable.com>
2024-04-05 14:56:56 -0400

    simplier const

--- client/Packages/com.beamable/Common/Runtime/BeamCli/Contracts/Constants.cs
+++ client/Packages/com.beamable/Common/Runtime/BeamCli/Contracts/Constants.cs
@@ -6,6 +6,6 @@ namespace Beamable.Common.BeamCli.Contracts
 		/// A magical constant that is used in the 'beam project generate-props` call to signify that the
 		/// $(SolutionDir) should be set to the same place as the Directory.Build.Props file.
 		/// </summary>
-		public const string GENERATE_PROPS_SLN_NEXT_TO_PROPS = "<dir.props>";
+		public const string GENERATE_PROPS_SLN_NEXT_TO_PROPS = "DIR.PROPS";
 	}
 }
