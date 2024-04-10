e2bf905642419a73113a44fb668490339922a549
Chris Hanna <chris@beamable.com>
2024-04-10 14:21:31 -0400

    update files in place

--- build/bin/modify-unity-files.sh
+++ build/bin/modify-unity-files.sh
@@ -6,25 +6,13 @@ echo "VERSION = ${VERSION}"
 SRC_FILE="client/Packages/com.beamable/Runtime/Environment/Resources/env-$ENVIRONMENT.json"
 DST_FILE="client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json"
 echo "Moving $SRC_FILE to $DST_FILE"
-
-echo current folder
-pwd
-
-echo listing...
-ls
-
-echo srcFile
-cat $SRC_FILE
-
-echo listing dst
-ls client/Packages/com.beamable/Runtime/Environment/Resources
 cp -f $SRC_FILE $DST_FILE
 
 # update the version number in env-defaults
-sed "s/BUILD__SDK__VERSION__STRING/'${VERSION}'/" client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json
+sed -i "s/BUILD__SDK__VERSION__STRING/${VERSION}/" client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json
 
 # update the com.beamable.server dependency on com.beamable
-sed "s/BUILD__SDK__VERSION__STRING/'${VERSION}'/" client/Packages/com.beamable.server/package.json
+sed -i "s/BUILD__SDK__VERSION__STRING/${VERSION}/" client/Packages/com.beamable.server/package.json
     
 # remove old env template files
 rm -f client/Packages/com.beamable/Runtime/Environment/Resources/env-dev.json
