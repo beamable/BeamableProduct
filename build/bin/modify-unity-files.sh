4d2e339cf7c71e87eda10b49864e6358f9571d69
Chris Hanna <chris@beamable.com>
2024-04-10 13:17:59 -0400

    ubuntize

--- build/bin/modify-unity-files.sh
+++ build/bin/modify-unity-files.sh
@@ -6,13 +6,13 @@ echo "VERSION = ${VERISON}"
 SRC_FILE=client/Packages/com.beamable/Runtime/Environment/Resources/env-${ENVIRONMENT}.json
 DST_FILE=client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json
 echo "Moving $SRC_FILE to $DST_FILE"
-cp -f $SRC_FILE $DST_FILE
+mv $SRC_FILE $DST_FILE
 
 # update the version number in env-defaults
-sed -i "s/BUILD__SDK__VERSION__STRING/'${VERISON}'/" "client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json"
+sed "s/BUILD__SDK__VERSION__STRING/'${VERISON}'/" client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json
 
 # update the com.beamable.server dependency on com.beamable
-sed -i "s/BUILD__SDK__VERSION__STRING/'${VERISON}'/" "client/Packages/com.beamable.server/package.json"
+sed "s/BUILD__SDK__VERSION__STRING/'${VERISON}'/" client/Packages/com.beamable.server/package.json
     
 # remove old env template files
 rm -f client/Packages/com.beamable/Runtime/Environment/Resources/env-dev.json
