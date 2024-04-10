f41147fb71923d551dd0468dcb21035519e7c646
Chris Hanna <chris@beamable.com>
2024-04-10 13:25:59 -0400

    force copy

--- build/bin/modify-unity-files.sh
+++ build/bin/modify-unity-files.sh
@@ -1,18 +1,18 @@
 # review inputs
 echo "ENVIRONMENT = ${ENVIRONMENT}"
-echo "VERSION = ${VERISON}"
+echo "VERSION = ${VERSION}"
 
 # select the right .env file for the package and write it as env-defaults
-SRC_FILE=client/Packages/com.beamable/Runtime/Environment/Resources/env-${ENVIRONMENT}.json
-DST_FILE=client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json
+SRC_FILE=./client/Packages/com.beamable/Runtime/Environment/Resources/env-$ENVIRONMENT.json
+DST_FILE=./client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json
 echo "Moving $SRC_FILE to $DST_FILE"
-mv $SRC_FILE $DST_FILE
+cp -f $SRC_FILE $DST_FILE
 
 # update the version number in env-defaults
-sed "s/BUILD__SDK__VERSION__STRING/'${VERISON}'/" client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json
+sed "s/BUILD__SDK__VERSION__STRING/'${VERSION}'/" client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json
 
 # update the com.beamable.server dependency on com.beamable
-sed "s/BUILD__SDK__VERSION__STRING/'${VERISON}'/" client/Packages/com.beamable.server/package.json
+sed "s/BUILD__SDK__VERSION__STRING/'${VERSION}'/" client/Packages/com.beamable.server/package.json
     
 # remove old env template files
 rm -f client/Packages/com.beamable/Runtime/Environment/Resources/env-dev.json
