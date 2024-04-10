5ee10b90f2f57d584027a1a0107a6a92015d481c
Chris Hanna <chris@beamable.com>
2024-04-10 14:30:10 -0400

    map samples

--- build/bin/modify-unity-files.sh
+++ build/bin/modify-unity-files.sh
@@ -10,6 +10,7 @@ cp -f $SRC_FILE $DST_FILE
 
 # update the version number in env-defaults
 sed -i "s/BUILD__SDK__VERSION__STRING/${VERSION}/" client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json
+sed -i "s/UNITY__VSP__UID/false/" client/Packages/com.beamable/Runtime/Environment/Resources/env-default.json
 
 # update the com.beamable.server dependency on com.beamable
 sed -i "s/BUILD__SDK__VERSION__STRING/${VERSION}/" client/Packages/com.beamable.server/package.json
