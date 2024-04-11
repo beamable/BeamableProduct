236adf019d8afc12b3fcb357c3f35c74a197b2d6
Chris Hanna <chris@beamable.com>
2024-04-11 14:14:04 -0400

    more build stuff

---
+++ build/bin/upload-changelogs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo Branch is $BRANCH
+
+git --version
+git init
+git config user.email "chris@beamable.com"
+git config user.name "gh-actions"
+git remote add --fetch origin https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/beamable/Changelogs.git
+git remote set-url origin https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/beamable/Changelogs.git
+git checkout $BRANCH
+git status
+cp -f ../client/Packages/com.beamable/CHANGELOG.md ./com-beamable-changelog.md
+cp -f ../client/Packages/com.beamable.server/CHANGELOG.md ./com-beamable-server-changelog.md
+cp -f ../cli/cli/CHANGELOG.md ./beamable-tools-changelog.md || true
+git add .
+git status
+git diff-index --quiet HEAD || git commit -m "updating changelogs for ' + $VERSION + '"
+sh "git push --set-upstream --force origin $BRANCH"
---
+++ build/bin/upload-docker-base-image.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+export BEAMSERVICE_TAG=${ENVIRONMENT}_${VERSION:-0.0.0}
+export LOCAL_REPO_TAG=beamservice:${BEAMSERVICE_TAG}
+
+echo "Docker Version Info..."
+docker version
+
+echo "Tag Info...."
+echo "beamserviceTag = $BEAMSERVICE_TAG" 
+echo "localRepoTag = $LOCAL_REPO_TAG" 
+echo "version = $VERSION"
+echo "env = $ENVIRONMENT"
+
+echo "Logging into dockerhub"
+docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}
+
+
+export lib_path="../microservice/microservice/lib"
+
+echo "Building microserivce dependencies..."
+cd client
+echo "Building Common Library..."
+dotnet publish ../client/Packages/com.beamable/Common -c release -o $lib_path /p:InformationalVersion=$VERSION
+
+echo "Building Server Library..."
+dotnet publish ../client/Packages/com.beamable.server/SharedRuntime -c release -o $lib_path /p:InformationalVersion=$VERSION
+
+echo "Building Stubs..."
+dotnet publish ../microservice/unityEngineStubs -c release -o $lib_path /p:InformationalVersion=$VERSION
+
+echo "Building Addressable Stubs..."
+dotnet publish ../microservice/unityenginestubs.addressables -c release -o $lib_path /p:InformationalVersion=$VERSION
+
+echo "Building Tools..."
+dotnet publish ../microservice/beamable.tooling.common -c release -o $lib_path /p:InformationalVersion=$VERSION
+
+
+echo "Building Microservice base image..."
+# /usr/libexec/docker/cli-plugins/docker-buildx build --builder beamable-builder --platform linux/arm64,linux/amd64 --push -t beamableinc/${LOCAL_REPO_TAG} ../microservice/microservice --build-arg BEAMABLE_SDK_VERSION=${VERSION:-0.0.0}
