32ac66ba4acc918cc6720ab1308f19c3211bce35
Chris Hanna <chris+gh@beamable.com>
2024-04-19 08:48:00 -0400

    syncing Unity SDK from CLI (#3317)
    Co-authored-by: cdhanna <3848374+cdhanna@users.noreply.github.com>

--- client/Packages/com.beamable/Editor/BeamCli/Commands/BeamContentPublish.cs
+++ client/Packages/com.beamable/Editor/BeamCli/Commands/BeamContentPublish.cs
@@ -4,7 +4,7 @@ namespace Beamable.Editor.BeamCli.Commands
 	using Beamable.Common;
 	using Beamable.Common.BeamCli;
 
-	public class ContentLocalManifestArgs : Beamable.Common.BeamCli.IBeamCommandArgs
+	public class ContentPublishArgs : Beamable.Common.BeamCli.IBeamCommandArgs
 	{
 		/// <summary>Inform a subset of ','-separated manifest ids for which to return data. By default, will return all manifests</summary>
 		public string[] manifestIds;
@@ -27,30 +27,30 @@ namespace Beamable.Editor.BeamCli.Commands
 	}
 	public partial class BeamCommands
 	{
-		public virtual ContentLocalManifestWrapper ContentLocalManifest(ContentLocalManifestArgs localManifestArgs)
+		public virtual ContentPublishWrapper ContentPublish(ContentPublishArgs publishArgs)
 		{
 			// Create a list of arguments for the command
 			System.Collections.Generic.List<string> genBeamCommandArgs = new System.Collections.Generic.List<string>();
 			genBeamCommandArgs.Add("beam");
 			genBeamCommandArgs.Add(defaultBeamArgs.Serialize());
 			genBeamCommandArgs.Add("content");
-			genBeamCommandArgs.Add("local-manifest");
-			genBeamCommandArgs.Add(localManifestArgs.Serialize());
+			genBeamCommandArgs.Add("publish");
+			genBeamCommandArgs.Add(publishArgs.Serialize());
 			// Create an instance of an IBeamCommand
 			Beamable.Common.BeamCli.IBeamCommand command = this._factory.Create();
 			// Join all the command paths and args into one string
 			string genBeamCommandStr = string.Join(" ", genBeamCommandArgs);
 			// Configure the command with the command string
 			command.SetCommand(genBeamCommandStr);
-			ContentLocalManifestWrapper genBeamCommandWrapper = new ContentLocalManifestWrapper();
+			ContentPublishWrapper genBeamCommandWrapper = new ContentPublishWrapper();
 			genBeamCommandWrapper.Command = command;
 			// Return the command!
 			return genBeamCommandWrapper;
 		}
 	}
-	public class ContentLocalManifestWrapper : Beamable.Common.BeamCli.BeamCommandWrapper
+	public class ContentPublishWrapper : Beamable.Common.BeamCli.BeamCommandWrapper
 	{
-		public virtual ContentLocalManifestWrapper OnStreamLocalContentState(System.Action<ReportDataPoint<BeamLocalContentState>> cb)
+		public virtual ContentPublishWrapper OnStreamLocalContentState(System.Action<ReportDataPoint<BeamLocalContentState>> cb)
 		{
 			this.Command.On("stream", cb);
 			return this;
--- client/Packages/com.beamable/Editor/BeamCli/Commands/BeamContentReset.cs
+++ client/Packages/com.beamable/Editor/BeamCli/Commands/BeamContentReset.cs
@@ -4,7 +4,7 @@ namespace Beamable.Editor.BeamCli.Commands
 	using Beamable.Common;
 	using Beamable.Common.BeamCli;
 
-	public class ContentLocalManifestArgs : Beamable.Common.BeamCli.IBeamCommandArgs
+	public class ContentResetArgs : Beamable.Common.BeamCli.IBeamCommandArgs
 	{
 		/// <summary>Inform a subset of ','-separated manifest ids for which to return data. By default, will return all manifests</summary>
 		public string[] manifestIds;
@@ -27,30 +27,30 @@ namespace Beamable.Editor.BeamCli.Commands
 	}
 	public partial class BeamCommands
 	{
-		public virtual ContentLocalManifestWrapper ContentLocalManifest(ContentLocalManifestArgs localManifestArgs)
+		public virtual ContentResetWrapper ContentReset(ContentResetArgs resetArgs)
 		{
 			// Create a list of arguments for the command
 			System.Collections.Generic.List<string> genBeamCommandArgs = new System.Collections.Generic.List<string>();
 			genBeamCommandArgs.Add("beam");
 			genBeamCommandArgs.Add(defaultBeamArgs.Serialize());
 			genBeamCommandArgs.Add("content");
-			genBeamCommandArgs.Add("local-manifest");
-			genBeamCommandArgs.Add(localManifestArgs.Serialize());
+			genBeamCommandArgs.Add("reset");
+			genBeamCommandArgs.Add(resetArgs.Serialize());
 			// Create an instance of an IBeamCommand
 			Beamable.Common.BeamCli.IBeamCommand command = this._factory.Create();
 			// Join all the command paths and args into one string
 			string genBeamCommandStr = string.Join(" ", genBeamCommandArgs);
 			// Configure the command with the command string
 			command.SetCommand(genBeamCommandStr);
-			ContentLocalManifestWrapper genBeamCommandWrapper = new ContentLocalManifestWrapper();
+			ContentResetWrapper genBeamCommandWrapper = new ContentResetWrapper();
 			genBeamCommandWrapper.Command = command;
 			// Return the command!
 			return genBeamCommandWrapper;
 		}
 	}
-	public class ContentLocalManifestWrapper : Beamable.Common.BeamCli.BeamCommandWrapper
+	public class ContentResetWrapper : Beamable.Common.BeamCli.BeamCommandWrapper
 	{
-		public virtual ContentLocalManifestWrapper OnStreamLocalContentState(System.Action<ReportDataPoint<BeamLocalContentState>> cb)
+		public virtual ContentResetWrapper OnStreamLocalContentState(System.Action<ReportDataPoint<BeamLocalContentState>> cb)
 		{
 			this.Command.On("stream", cb);
 			return this;
