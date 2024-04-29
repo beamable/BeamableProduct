b391dbe6a23554875669b5c18774d736ea70d0df
Chris Hanna <chris+gh@beamable.com>
2024-04-29 13:41:42 -0400

    fixes 3335 - usam refresh button broken, and some speed improvements (#3354)
    * logging checks
    
    * fixes 3335 - refresh window fix and perf boosts
    
    * pr comments
    
    ---------
    
    Co-authored-by: Chris Hanna <chris@beamable.com>

--- client/Packages/com.beamable.server/Editor/Usam/Editor/UsamLogger.cs
+++ client/Packages/com.beamable.server/Editor/Usam/Editor/UsamLogger.cs
@@ -8,11 +8,39 @@ namespace Beamable.Server.Editor.Usam
 	[SpewLogger]
 	public class UsamLogger
 	{
+		private static long _previousStopWatchSeconds;
+		private static Stopwatch _stopwatch = new Stopwatch();
+		
 		[Conditional(Constants.Features.Spew.SPEW_ALL), Conditional(Constants.Features.Spew.SPEW_USAM)]
 		public static void Log(params object[] msg)
 		{
-			Logger.DoSimpleSpew("USAM: " + string.Join(",", msg.Select(x => x?.ToString())));
+			if (_stopwatch.IsRunning)
+			{
+				var diff = _stopwatch.ElapsedMilliseconds - _previousStopWatchSeconds;
+				Logger.DoSimpleSpew($"USAM: total=({_stopwatch.ElapsedMilliseconds}) elapsed=({diff})" + string.Join(",", msg.Select(x => x?.ToString())));
+				_previousStopWatchSeconds = _stopwatch.ElapsedMilliseconds;
+
+			}
+			else
+			{			
+				Logger.DoSimpleSpew($"USAM: " + string.Join(",", msg.Select(x => x?.ToString())));
+			}
+		}
+		
+		[Conditional(Constants.Features.Spew.SPEW_ALL), Conditional(Constants.Features.Spew.SPEW_USAM)]
+		public static void ResetLogTimer()
+		{
+			_stopwatch.Restart();
+			_previousStopWatchSeconds = 0;
+		}
+		
+		[Conditional(Constants.Features.Spew.SPEW_ALL), Conditional(Constants.Features.Spew.SPEW_USAM)]
+		public static void StopLogTimer()
+		{
+			_stopwatch.Stop();
+			_previousStopWatchSeconds = 0;
 		}
 	}
+
 }
 
