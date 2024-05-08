c954acc651dda232f4b2a6aea03da0af08e3c3ac
cdhanna <cdhanna@users.noreply.github.com>
2024-05-08 14:09:35 +0000

    Formatting: cli server draft (#3357)

--- cli/cli/App.cs
+++ cli/cli/App.cs
@@ -58,7 +58,7 @@ public class App
 
 	private static LogConfigData ConfigureLogging(App app, Func<LoggerConfiguration, ILogger> configureLogger = null)
 	{
-	
+
 		var tempFile = Path.Combine(Path.GetTempPath(), "beamCliLog.txt");
 		var shouldUseTempFile = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("BEAM_CLI_NO_FILE_LOG"));
 		try
@@ -148,7 +148,7 @@ public class App
 		services.AddSingleton<IDataReporterService, DataReporterService>();
 		services.AddSingleton<ServerService>();
 		services.AddSingleton<AppLifecycle>();
-		
+
 		OpenApiRegistration.RegisterOpenApis(services);
 
 		_serviceConfigurator?.Invoke(services);
@@ -158,7 +158,7 @@ public class App
 		Action<IDependencyBuilder> serviceConfigurator = null,
 		Action<IDependencyBuilder> commandConfigurator = null,
 		Func<LoggerConfiguration, ILogger> configureLogger = null,
-		LogConfigData prebuiltLogger=null
+		LogConfigData prebuiltLogger = null
 		)
 	{
 		if (IsBuilt)
@@ -321,7 +321,7 @@ public class App
 
 
 		// content commands
-		
+
 		Commands.AddRootCommand<ContentCommand>();
 		Commands.AddSubCommandWithHandler<ContentPullCommand, ContentPullCommandArgs, ContentCommand>();
 		Commands.AddSubCommandWithHandler<ContentStatusCommand, ContentStatusCommandArgs, ContentCommand>();
@@ -431,7 +431,7 @@ public class App
 		});
 
 		var commandLineBuilder = new CommandLineBuilder(root);
-		
+
 		// this middleware is responsible for catching parse errors and putting them on the data-out raw channel
 		commandLineBuilder.AddMiddleware((ctx, next) =>
 		{
@@ -445,7 +445,7 @@ public class App
 			var appContext = provider.GetService<IAppContext>();
 			var reporter = provider.GetService<IDataReporterService>();
 			var isPiping = appContext.UsePipeOutput || appContext.ShowRawOutput;
-			
+
 			if (!isPiping)
 			{
 				// we aren't using raw output, so this middleware has nothing to do.
@@ -457,9 +457,9 @@ public class App
 			reporter.Exception(ex, ctx.ExitCode, ctx.BindingContext.ParseResult.Diagram());
 			// don't call the next task, because we have "handled" the error by posting it to the error channel
 			return Task.CompletedTask;
-			
+
 		}, MiddlewareOrder.ErrorReporting);
-		
+
 		commandLineBuilder.AddMiddleware(consoleContext =>
 		{
 			// create a scope for the execution of the command
