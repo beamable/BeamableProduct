247d463825254ef199480166fb4da6e1e205aa28
Gabriel Ramos <gabriel@beamable.com>
2024-06-19 11:41:56 -0300

    Some more comments addressed

--- cli/cli/App.cs
+++ cli/cli/App.cs
@@ -242,7 +242,6 @@ public class App
 
 		// add commands
 		Commands.AddRootCommand<CliInterfaceGeneratorCommand, CliInterfaceGeneratorCommandArgs>();
-
 		Commands.AddRootCommand<ServerGroupCommand>();
 		Commands.AddSubCommand<ServeCliCommand, ServeCliCommandArgs, ServerGroupCommand>();
 		Commands.AddSubCommand<RequestCliCommand, RequestCliCommandArgs, ServerGroupCommand>();
@@ -272,6 +271,7 @@ public class App
 		Commands.AddSubCommand<ProjectVersionCommand, ProjectVersionCommandArgs, ProjectCommand>();
 		Commands.AddSubCommand<ShareCodeCommand, ShareCodeCommandArgs, ProjectCommand>();
 		Commands.AddSubCommand<CheckStatusCommand, CheckStatusCommandArgs, ProjectCommand>();
+		Commands.AddSubCommand<ShowRemoteManifestCommand, ShowRemoteManifestCommandArgs, ProjectCommand>();
 		Commands.AddRootCommand<AccountMeCommand, AccountMeCommandArgs>();
 		Commands.AddRootCommand<BaseRequestGetCommand, BaseRequestArgs>();
 		Commands.AddRootCommand<BaseRequestPutCommand, BaseRequestArgs>();
---
+++ cli/cli/Commands/Project/ShowRemoteManifestCommand.cs
@@ -0,0 +1,25 @@
+using cli.Services;
+
+namespace cli.Commands.Project;
+
+public class ShowRemoteManifestCommandArgs : CommandArgs
+{
+
+}
+
+public class ShowRemoteManifestCommand : AtomicCommand<ShowRemoteManifestCommandArgs, ServiceManifest>
+{
+	public ShowRemoteManifestCommand() : base("remote-manifest", "Returns the remote manifest in json format")
+	{
+	}
+
+	public override void Configure()
+	{
+	}
+
+	public override async Task<ServiceManifest> GetResult(ShowRemoteManifestCommandArgs args)
+	{
+		var manifest = await args.BeamoService.GetCurrentManifest();
+		return manifest;
+	}
+}
