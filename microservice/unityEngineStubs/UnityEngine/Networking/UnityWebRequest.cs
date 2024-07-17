84d0e458e68f91a93c91c89cae39f53c7cbb793e
Chris Hanna <chris@beamable.com>
2024-07-17 14:49:30 -0400

    more stub info

--- microservice/unityEngineStubs/UnityEngine/Networking/UnityWebRequest.cs
+++ microservice/unityEngineStubs/UnityEngine/Networking/UnityWebRequest.cs
@@ -3,5 +3,8 @@ namespace UnityEngine.Networking
 	public class UnityWebRequest
 	{
 		// stub type.
+		public string error { get; }
+		public long responseCode { get; }
+		public string url { get; }
 	}
 }
