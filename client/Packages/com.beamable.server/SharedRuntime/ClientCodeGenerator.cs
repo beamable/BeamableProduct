3e94ea5199ccf81f075a81f87a85b4d6b363872b
gabrielbeamable <141041673+gabrielbeamable@users.noreply.github.com>
2024-07-22 11:40:48 -0300

    [Issue/3480] Services Deploy cancellation (#3548)
    * Make it possible to cancel deployment of services
    
    * Commit common copy to Unity SDK
    
    * Add memory cleanup and push new version

--- client/Packages/com.beamable.server/SharedRuntime/ClientCodeGenerator.cs
+++ client/Packages/com.beamable.server/SharedRuntime/ClientCodeGenerator.cs
@@ -1,5 +1,5 @@
-// this file was copied from nuget package Beamable.Server.Common@0.0.0-PREVIEW.NIGHTLY-202405141737
-// https://www.nuget.org/packages/Beamable.Server.Common/0.0.0-PREVIEW.NIGHTLY-202405141737
+// this file was copied from nuget package Beamable.Server.Common@0.0.0-PREVIEW.NIGHTLY-202407161549
+// https://www.nuget.org/packages/Beamable.Server.Common/0.0.0-PREVIEW.NIGHTLY-202407161549
 
 using Beamable.Common;
 using Beamable.Common.Api.Auth;
