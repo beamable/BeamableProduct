0ef352352a036498f1d15ed55856375bf23590ae
Allister MacLeod <allister@beamable.com>
2024-09-24 16:38:59 -0400

    SendCurrency XMLDoc expansion to clarify withdrawal and deposit behavior.

--- client/Packages/com.beamable.server/SharedRuntime/Api/Inventory/IMicroserviceInventoryApi.cs
+++ client/Packages/com.beamable.server/SharedRuntime/Api/Inventory/IMicroserviceInventoryApi.cs
@@ -6,9 +6,9 @@ namespace Beamable.Server.Api.Inventory
 {
 	/// <summary>
 	/// This type defines the %Microservices main entry point for the %Inventory feature.
-	/// 
+	///
 	/// [img beamable-logo]: https://landen.imgix.net/7udgo2lvquge/assets/xgh89bz1.png?w=400 "Beamable Logo"
-	/// 
+	///
 	/// #### Related Links
 	/// - See the <a target="_blank" href="https://docs.beamable.com/docs/inventory-feature-overview">Inventory</a> feature documentation
 	/// - See Beamable.Server.IBeamableServices script reference
@@ -19,11 +19,21 @@ namespace Beamable.Server.Api.Inventory
 	public interface IMicroserviceInventoryApi : IInventoryApi
 	{
 		/// <summary>
-		/// Send multiple currencies to different user.
+		/// Send one or more currencies to different user. The currencies will
+		/// be withdrawn from the current player and deposited to the recipient
+		/// player. If the donor player does not have enough of any of the
+		/// specified currencies, the whole transaction will be terminated with
+		/// no effect.
+		/// The recipient player must be a different player from the donor (the
+		/// current player according to the Beamable context); trying to transfer
+		/// currencies to the same player who is donating is an error.
+		/// Transaction IDs are normally assigned by Beamable services internally;
+		/// omit the transaction argument unless you have a need to manage the
+		/// transaction IDs externally.
 		/// </summary>
 		/// <param name="currencies">A dictionary where the keys are content IDs of the currency, and the values are the amount of currency to send</param>
 		/// <param name="recipientPlayer">Target user identifier.</param>
-		/// <param name="transaction">An inventory transaction ID. Leave this argument empty.</param>
+		/// <param name="transaction">(optional) An inventory transaction ID. In the vast majority of cases you should leave this argument empty.</param>
 		/// <returns>A <see cref="Promise{T}"/> representing the network call.</returns>
 		Promise SendCurrency(Dictionary<string, long> currencies, long recipientPlayer, string transaction = null);
 	}
