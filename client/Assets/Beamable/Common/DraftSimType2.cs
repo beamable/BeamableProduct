6a627882367ae2c00de41021652439bb6399309b
Chris Hanna <chris@beamable.com>
2024-10-31 13:51:26 -0400

    merge

--- client/Assets/Beamable/Common/DraftSimType2.cs
+++
@@ -1,9 +0,0 @@
-using Beamable.Common.Content;
-
-namespace Beamable.EasyFeature.GameSpecificPlayerSystemArchitecture
-{
-	/// <summary>
-	/// A <see cref="SimGameType"/> content that defines the data for the draft system. Only supports same-size teams.  
-	/// </summary>
-	public class DraftSimType2 : DraftSimType { }
-}
