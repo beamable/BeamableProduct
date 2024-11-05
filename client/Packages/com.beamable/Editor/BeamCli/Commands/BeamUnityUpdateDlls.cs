3d93ec95f128018c1b8b708e4c8e196c879b225d
Gabriel Ramos <gabriel@beamable.com>
2024-11-05 16:53:23 -0300

    cli generated code

--- client/Packages/com.beamable/Editor/BeamCli/Commands/BeamUnityUpdateDlls.cs
+++ client/Packages/com.beamable/Editor/BeamCli/Commands/BeamUnityUpdateDlls.cs
@@ -4,29 +4,26 @@ namespace Beamable.Editor.BeamCli.Commands
     using Beamable.Common;
     using Beamable.Common.BeamCli;
     
-    public partial class ProjectBuildArgs : Beamable.Common.BeamCli.IBeamCommandArgs
+    public partial class UnityUpdateDllsArgs : Beamable.Common.BeamCli.IBeamCommandArgs
     {
-        /// <summary>When true, the command will run forever and watch the state of the program</summary>
-        public bool watch;
-        /// <summary>The list of services to include, defaults to all local services (separated by whitespace)</summary>
-        public string[] ids;
+        /// <summary>The name of the service to update the dlls</summary>
+        public string service;
+        /// <summary>The path of the dll that will be referenced</summary>
+        public string[] paths;
         /// <summary>Serializes the arguments for command line usage.</summary>
         public virtual string Serialize()
         {
             // Create a list of arguments for the command
             System.Collections.Generic.List<string> genBeamCommandArgs = new System.Collections.Generic.List<string>();
-            // If the watch value was not default, then add it to the list of args.
-            if ((this.watch != default(bool)))
+            // Add the service value to the list of args.
+            genBeamCommandArgs.Add(this.service.ToString());
+            // If the paths value was not default, then add it to the list of args.
+            if ((this.paths != default(string[])))
             {
-                genBeamCommandArgs.Add(("--watch=" + this.watch));
-            }
-            // If the ids value was not default, then add it to the list of args.
-            if ((this.ids != default(string[])))
-            {
-                for (int i = 0; (i < this.ids.Length); i = (i + 1))
+                for (int i = 0; (i < this.paths.Length); i = (i + 1))
                 {
                     // The parameter allows multiple values
-                    genBeamCommandArgs.Add(("--ids=" + this.ids[i]));
+                    genBeamCommandArgs.Add(("--paths=" + this.paths[i]));
                 }
             }
             string genBeamCommandStr = "";
@@ -37,33 +34,28 @@ namespace Beamable.Editor.BeamCli.Commands
     }
     public partial class BeamCommands
     {
-        public virtual ProjectBuildWrapper ProjectBuild(ProjectBuildArgs buildArgs)
+        public virtual UnityUpdateDllsWrapper UnityUpdateDlls(UnityUpdateDllsArgs updateDllsArgs)
         {
             // Create a list of arguments for the command
             System.Collections.Generic.List<string> genBeamCommandArgs = new System.Collections.Generic.List<string>();
             genBeamCommandArgs.Add("beam");
             genBeamCommandArgs.Add(defaultBeamArgs.Serialize());
-            genBeamCommandArgs.Add("project");
-            genBeamCommandArgs.Add("build");
-            genBeamCommandArgs.Add(buildArgs.Serialize());
+            genBeamCommandArgs.Add("unity");
+            genBeamCommandArgs.Add("update-dlls");
+            genBeamCommandArgs.Add(updateDllsArgs.Serialize());
             // Create an instance of an IBeamCommand
             Beamable.Common.BeamCli.IBeamCommand command = this._factory.Create();
             // Join all the command paths and args into one string
             string genBeamCommandStr = string.Join(" ", genBeamCommandArgs);
             // Configure the command with the command string
             command.SetCommand(genBeamCommandStr);
-            ProjectBuildWrapper genBeamCommandWrapper = new ProjectBuildWrapper();
+            UnityUpdateDllsWrapper genBeamCommandWrapper = new UnityUpdateDllsWrapper();
             genBeamCommandWrapper.Command = command;
             // Return the command!
             return genBeamCommandWrapper;
         }
     }
-    public partial class ProjectBuildWrapper : Beamable.Common.BeamCli.BeamCommandWrapper
+    public partial class UnityUpdateDllsWrapper : Beamable.Common.BeamCli.BeamCommandWrapper
     {
-        public virtual ProjectBuildWrapper OnStreamBuildProjectCommandOutput(System.Action<ReportDataPoint<BeamBuildProjectCommandOutput>> cb)
-        {
-            this.Command.On("stream", cb);
-            return this;
-        }
     }
 }
