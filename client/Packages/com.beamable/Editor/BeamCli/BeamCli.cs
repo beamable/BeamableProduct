75e117154e9ee0ffe34ad9b4980a994bae64166f
Chris Hanna <chris+gh@beamable.com>
2024-11-08 14:26:59 -0500

    switch up creation of deps (#3769)
    * switch up creation of deps
    
    * test
    
    * test2
    
    * beep boop
    
    * eg
    
    * halp
    
    * ordering is important
    
    * finally
    
    * open solution
    
    ---------
    
    Co-authored-by: Chris Hanna <chris@beamable.com>

--- client/Packages/com.beamable/Editor/BeamCli/BeamCli.cs
+++ client/Packages/com.beamable/Editor/BeamCli/BeamCli.cs
@@ -41,13 +41,9 @@ namespace Beamable.Editor.BeamCli
 			}
 		}
 
-		async Promise Init(EditorAccountInfo _) => await Init();
-
 		public async Promise Init()
 		{
 			await _ctx.OnReady;
-
-			var extraPaths = BeamablePackages.GetManifestFileReferences();
 			
 			var args = new InitArgs
 			{
@@ -55,9 +51,17 @@ namespace Beamable.Editor.BeamCli
 				noTokenSave = true,
 				cid = _ctx.Requester.Cid,
 				pid = _ctx.Requester.Pid,
-				host = BeamableEnvironment.ApiUrl,
-				saveExtraPaths = extraPaths.ToArray()
+				host = BeamableEnvironment.ApiUrl
 			};
+
+			if (string.IsNullOrEmpty(args.cid) || string.IsNullOrEmpty(args.pid))
+			{
+				// cannot call the init command with a blank cid/pid. 
+				return;
+			}
+			
+			var extraPaths = BeamablePackages.GetManifestFileReferences();
+			args.saveExtraPaths = extraPaths.ToArray();
 			
 			var token = _ctx.Requester.Token;
 			if (token == null)
@@ -85,7 +89,6 @@ namespace Beamable.Editor.BeamCli
 				Debug.LogError("Unable to register Unity project with local CLI project." + cb.data.message);
 			});
 			var _ = linkCommand.Run();
-			// await linkCommand.Run();
 		}
 	}
 }
