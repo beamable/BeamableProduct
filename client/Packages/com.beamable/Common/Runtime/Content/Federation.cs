abb9a7eeca9813b8d47cc461cd662f96e0589658
Gabriel Ramos <gabriel@beamable.com>
2024-12-19 17:44:26 -0300

    SimGameType custom property drawer

--- client/Packages/com.beamable/Common/Runtime/Content/Federation.cs
+++ client/Packages/com.beamable/Common/Runtime/Content/Federation.cs
@@ -17,5 +17,17 @@ namespace Beamable.Common.Content
 
 	[System.Serializable]
 	[Agnostic]
+	public class Namespace
+	{
+		[ContentField("name")]
+		public string Name;
+	}
+
+	[System.Serializable]
+	[Agnostic]
 	public class OptionalFederation : Optional<Federation> { }
+
+	[System.Serializable]
+	[Agnostic]
+	public class OptionalNamespace : Optional<Namespace>{}
 }
