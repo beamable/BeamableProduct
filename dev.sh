eab282ccb153c585beec4b5592917f81eac7bddf
Chris Hanna <chris@beamable.com>
2025-01-14 09:14:36 -0500

    opt in again

--- dev.sh
+++ dev.sh
@@ -34,14 +34,14 @@ PREVIOUS_VERSION=0.0.123.$PREVIOUS_BUILD_NUMBER
 SOLUTION=./build/LocalBuild/LocalBuild.sln
 TMP_BUILD_OUTPUT="TempBuild"
 
-BUILD_ARGS="--configuration Release -p:PackageVersion=$VERSION -p:CombinedVersion=$VERSION -p:InformationalVersion=$VERSION -p:Warn=0 -p:BEAM_PREVENT_COPY_CODE_TO_UNITY=true" #-
+BUILD_ARGS="--configuration Release -p:PackageVersion=$VERSION -p:CombinedVersion=$VERSION -p:InformationalVersion=$VERSION -p:Warn=0" #-
 PACK_ARGS="--configuration Release --no-build -o $TMP_BUILD_OUTPUT -p:PackageVersion=$VERSION -p:CombinedVersion=$VERSION -p:InformationalVersion=$VERSION -p:SKIP_GENERATION=true"
 PUSH_ARGS="--source $FEED_NAME"
 
 dotnet restore $SOLUTION
 dotnet build $SOLUTION $BUILD_ARGS
-dotnet build cli/beamable.common -t:CopyCodeToUnity 
-dotnet build cli/beamable.server.common -t:CopyCodeToUnity 
+dotnet build cli/beamable.common -t:CopyCodeToUnity -p:BEAM_COPY_CODE_TO_UNITY=true
+dotnet build cli/beamable.server.common -t:CopyCodeToUnity -p:BEAM_COPY_CODE_TO_UNITY=true
 dotnet pack $SOLUTION $PACK_ARGS
 dotnet nuget push $TMP_BUILD_OUTPUT/*.$VERSION.nupkg $PUSH_ARGS
 
