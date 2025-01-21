2928dba7591967d80d9e2aca118d5dc54edd4abe
Chris Hanna <chris+gh@beamable.com>
2025-01-21 11:49:14 -0500

    fix nuget release (#3853)
    * test
    
    * confused
    
    * setup
    
    * ignore
    
    * reset
    
    * huh
    
    * confused
    
    * no symbols
    
    ---------
    
    Co-authored-by: Chris Hanna <chris@beamable.com>

--- setup.sh
+++ setup.sh
@@ -51,6 +51,29 @@ dotnet nuget add source $SOURCE_FOLDER --name $FEED_NAME
 echo "Creating build number file"
 echo 0 > build-number.txt
 
+# restore cli tools
+dotnet tool restore --tool-manifest ./cli/cli/.config/dotnet-tools.json
+
+# reset the template projects to reference the base version number
+TEMPLATE_DOTNET_CONFIG_PATH="./cli/beamable.templates/.config/dotnet-tools.json"
+ls -a "./cli/beamable.templates/"
+mkdir -p "./cli/beamable.templates/.config"
+cat > $TEMPLATE_DOTNET_CONFIG_PATH << EOF
+{
+  "version": 1,
+  "isRoot": true,
+  "tools": {
+    "beamable.tools": {
+      "version": "0.0.123.0",
+      "commands": [
+        "beam"
+      ],
+      "rollForward": false
+    }
+  }
+}
+EOF
+
 
 # TODO: Consider running this as part of a post-pull git action
 # TODO: Should this run the `sync-rider-run-settings.sh` script? (probably)
