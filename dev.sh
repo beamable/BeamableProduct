3eb9aa9a6c37ac7c6943e4837b65c6388b2f1041
Chris Hanna <chris@beamable.com>
2025-01-22 14:21:06 -0500

    ah

--- dev.sh
+++ dev.sh
@@ -74,8 +74,8 @@ rm -rf $HOME/.nuget/packages/beamable.*/$PREVIOUS_VERSION
 dotnet tool install Beamable.Tools --version $VERSION --global --allow-downgrade --no-cache
 
 # generate unity CLI
-beam generate-interface --engine unity --output=./client/Packages/com.beamable/Editor/BeamCli/Commands
-dotnet tool run dotnet-format ./client/Packages/com.beamable/Editor/BeamCli/Commands
+#beam generate-interface --engine unity --output=./client/Packages/com.beamable/Editor/BeamCli/Commands
+#dotnet tool run dotnet-format ./client/Packages/com.beamable/Editor/BeamCli/Commands
 
 # restore the nuget packages (and CLI) for a sample project, thus restoring the
 # nuget-cache for all projects.
