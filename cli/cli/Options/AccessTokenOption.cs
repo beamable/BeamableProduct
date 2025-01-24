9e8f8413246b4b0537d753c4bfed49d5427655d1
Pedro Rauiz Estigarribia Gestal <pedro@beamable.com>
2025-01-24 12:35:59 -0300

    Improved help strings for CLI global option

--- cli/cli/Options/AccessTokenOption.cs
+++ cli/cli/Options/AccessTokenOption.cs
@@ -2,9 +2,8 @@ namespace cli;
 
 public class AccessTokenOption : ConfigurableOption
 {
-	public AccessTokenOption() : base(
-		Constants.CONFIG_ACCESS_TOKEN,
-		"The access token to use for the requests")
+	public AccessTokenOption() : base(Constants.CONFIG_ACCESS_TOKEN, "The access token to use for the requests." +
+	                                                                 $" It overwrites the logged in user stored in {Constants.CONFIG_TOKEN_FILE_NAME} for THIS INVOCATION ONLY")
 	{
 	}
 }
