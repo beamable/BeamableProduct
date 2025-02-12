900dc178c04a2a86da6a0184ee4776521690c8f7
Chris Hanna <chris@beamable.com>
2025-02-12 14:57:30 -0500

    clear disk space

--- build/customUnityDockerImage/build-images.sh
+++ build/customUnityDockerImage/build-images.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
 
-
-UNITY_VERSION=6000.0.37f1
 GAME_CI_VERSION=3.1.0
 MY_USERNAME=beamable
 
-# don't hesitate to remove unused components from this list
-declare -a components=("android" "ios" "mac-mono" "webgl" "windows-mono")
+declare -a components=("android" "ios" "mac-mono" "webgl" "windows-mono" "base")
+declare -a versions=("2021.3.29f1" "2022.3.7f1" "6000.0.37f1")
 
-for component in "${components[@]}"
+for version in "${versions[@]}"
 do
-  GAME_CI_UNITY_EDITOR_IMAGE=unityci/editor:ubuntu-${UNITY_VERSION}-${component}-${GAME_CI_VERSION}
-  IMAGE_TO_PUBLISH=${CUSTOM_REGISTRY}${MY_USERNAME}/editor:ubuntu-${UNITY_VERSION}-${component}-${GAME_CI_VERSION}
-  #editor-ubuntu-6000.0.37f1-android-3.1.0
-  #echo $GAME_CI_UNITY_EDITOR_IMAGE
-  docker build --build-arg GAME_CI_UNITY_EDITOR_IMAGE=${GAME_CI_UNITY_EDITOR_IMAGE} . -t ${IMAGE_TO_PUBLISH} --platform linux/amd64
-  docker push ${IMAGE_TO_PUBLISH}
+    for component in "${components[@]}"
+    do
+        # a valid image resembles something like this,
+        #  editor:ubuntu-6000.0.37f1-android-3.1.0
+
+        GAME_CI_UNITY_EDITOR_IMAGE=unityci/editor:ubuntu-${version}-${component}-${GAME_CI_VERSION}
+        IMAGE_TO_PUBLISH=${CUSTOM_REGISTRY}${MY_USERNAME}/editor:ubuntu-${version}-${component}-${GAME_CI_VERSION}
+
+        echo "cleaning docker files"
+        docker system prune -a --volumes --force
+        docker rmi -f $(docker images -aq)
+
+        echo "checking docker clean status"
+        docker images -a  # Should be empty
+        docker ps -a      # Should be empty
+        docker volume ls  # Should be empty
+
+        echo "building: ${IMAGE_TO_PUBLISH}"
+        docker build --build-arg GAME_CI_UNITY_EDITOR_IMAGE=${GAME_CI_UNITY_EDITOR_IMAGE} . -t ${IMAGE_TO_PUBLISH} --platform linux/amd64
+        
+        echo "pushing: ${IMAGE_TO_PUBLISH}"
+        docker push ${IMAGE_TO_PUBLISH}
+    done
 done
