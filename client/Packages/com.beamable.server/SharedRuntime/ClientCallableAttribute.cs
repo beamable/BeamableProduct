03af1480e96c1e999e41baea88a38c00a030df92
Chris Hanna <chris@beamable.com>
2025-04-01 16:22:41 -0400

    upgrade cli to rc5

--- client/Packages/com.beamable.server/SharedRuntime/ClientCallableAttribute.cs
+++ client/Packages/com.beamable.server/SharedRuntime/ClientCallableAttribute.cs
@@ -1,5 +1,5 @@
-// this file was copied from nuget package Beamable.Server.Common@4.2.0-PREVIEW.RC4
-// https://www.nuget.org/packages/Beamable.Server.Common/4.2.0-PREVIEW.RC4
+// this file was copied from nuget package Beamable.Server.Common@4.2.0-PREVIEW.RC5
+// https://www.nuget.org/packages/Beamable.Server.Common/4.2.0-PREVIEW.RC5
 
 using Beamable.Common;
 using Beamable.Common.Reflection;
