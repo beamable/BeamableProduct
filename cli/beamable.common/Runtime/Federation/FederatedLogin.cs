8effea344e921f69942adecd791b55cc39ed5eea
Piotr Siuszko <siuszko@zoho.com>
2025-04-15 11:12:12 +0200

    IFederatedLogin method docs with examples

--- cli/beamable.common/Runtime/Federation/FederatedLogin.cs
+++ cli/beamable.common/Runtime/Federation/FederatedLogin.cs
@@ -9,6 +9,62 @@ namespace Beamable.Common
 	/// </summary>
 	public interface IFederatedLogin<in T> : IFederation where T : IFederationId, new()
 	{
+	    /// <summary>
+        /// Authenticates a user using the given token, challenge, and solution.
+        /// This interface covers both the login and signup flows.
+        /// </summary>
+        /// <param name="token">The token to authenticate with.</param>
+        /// <param name="challenge">The challenge to authenticate with.</param>
+        /// <param name="solution">The solution to authenticate with.</param>
+        /// <returns>A promise that resolves to a federated authentication response.</returns>
+        /// <remarks>
+        /// When it implements the 2-step authentication flow it is called two times, first with the token having any value only.
+        /// The second time it is called, the token will be the token returned from the first call,
+        /// the challenge will be the challenge returned from the first call, and the solution will be the solution provided by the user.
+        /// </remarks>
+        /// <example>
+        /// Simplified 1-step authentication flow implementation example:
+        /// <code>
+        ///    public async Promise&lt;FederatedAuthenticationResponse&gt; Authenticate(string token, string challenge,
+        ///        string solution)
+        ///    {
+        ///        var externalId = await CallExternalAuthProvider(token);
+        ///        if (!string.IsNullOrWhiteSpace(externalId))
+        ///        {
+        ///            throw new MicroserviceException(500, "Authentication failed", "Authentication failed.");
+        ///        }
+        ///         return new FederatedAuthenticationResponse()
+        ///        {
+        ///            user_id = externalId
+        ///        };
+        ///    }
+        /// </code>
+        /// Simplified 2-step authentication flow implementation example:
+        /// <code>
+        ///    public Promise&lt;FederatedAuthenticationResponse&gt; Authenticate(string token, string challenge,
+        ///        string solution)
+        ///    {
+        ///        if (string.IsNullOrWhiteSpace(solution))
+        ///        {
+        ///               var (challengeText, challengeTime) = await StartThirdPartyVerification(token);
+        ///            return new FederatedAuthenticationResponse()
+        ///            {
+        ///                challenge = challengeText,
+        ///                challenge_ttl = challengeTime
+        ///            };
+        ///        }
+        ///        var externalId = await ValidateThirdParty(token, solution);
+        ///        if (string.IsNullOrWhiteSpace(externalId))
+        ///        {
+        ///          throw new MicroserviceException(500, "Authentication failed", "Authentication failed.");
+        ///        }
+        ///        return new FederatedAuthenticationResponse()
+        ///        {
+        ///          user_id = externalId
+        ///        };
+        /// }
+        /// </code>
+        /// </example>
 		Promise<FederatedAuthenticationResponse> Authenticate(string token, string challenge, string solution);
 	}
 
