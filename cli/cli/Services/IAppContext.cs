7605304c6e9f7c3f1ff25cab51366806115e416e
Chris Hanna <chris@beamable.com>
2025-06-03 09:51:33 -0400

    revert

--- cli/cli/Services/IAppContext.cs
+++ cli/cli/Services/IAppContext.cs
@@ -336,16 +336,15 @@ public class DefaultAppContext : IAppContext
 
 	public async Task Set(string cid, string pid, string host)
 	{
-		// if (!string.IsNullOrEmpty(cid))
-		// {
-		// 	var aliasResolve = await _aliasService.Resolve(cid);
-		// 	_cid = aliasResolve.Cid;
-		// }
-		// else
-		// {
-		// 	_cid = null;
-		// }
-		_cid = cid;
+		if (!string.IsNullOrEmpty(cid))
+		{
+			var aliasResolve = await _aliasService.Resolve(cid);
+			_cid = aliasResolve.Cid;
+		}
+		else
+		{
+			_cid = cid;
+		}
 		_pid = pid;
 		_host = host;
 		_token.Cid = _cid;
