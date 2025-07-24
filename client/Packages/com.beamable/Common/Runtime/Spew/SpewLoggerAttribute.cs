b69fc184f8fde5de96532648247237ceaeab3175
Gabriel Dias <gmoraes@beamable.com>
2025-07-24 11:52:35 -0300

    change: update cli auto generated code.

--- client/Packages/com.beamable/Common/Runtime/Spew/SpewLoggerAttribute.cs
+++ client/Packages/com.beamable/Common/Runtime/Spew/SpewLoggerAttribute.cs
@@ -1,5 +1,5 @@
-// this file was copied from nuget package Beamable.Common@5.1.0-PREVIEW.RC1
-// https://www.nuget.org/packages/Beamable.Common/5.1.0-PREVIEW.RC1
+// this file was copied from nuget package Beamable.Common@5.1.0
+// https://www.nuget.org/packages/Beamable.Common/5.1.0
 
 using System;
 using System.Diagnostics;
