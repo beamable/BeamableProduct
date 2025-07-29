c543543bfaeeab5736240f233588f0546529a0e2
Gabriel Dias <gmoraes@beamable.com>
2025-07-29 17:04:30 -0300

    fix: Check for Serializable Attribute on base type as well
    change: run nullable and genericTypes validation only for project with property EnableUnrealBlueprintCompability as true.
    
    change: Added support to MapOfString subtype as a valid Dictionary subtype.
    
    fix: Use the correct path on Generated Client code for Microservices.

--- cli/cli/Services/UnityOpenApiSourceGenerator/OpenApiClientCodeGenerator.cs
+++ cli/cli/Services/UnityOpenApiSourceGenerator/OpenApiClientCodeGenerator.cs
@@ -197,6 +197,7 @@ namespace Beamable.Server.Generator
 				}
 				
 				string methodName = path.Replace("/", string.Empty);
+				string pathName = path[0] == '/' ? path.Substring(1) : path;
 				if (item.Extensions.TryGetValue(COMPONENT_METHOD_NAME_KEY, out var methodNameExt) && methodNameExt is OpenApiString methodNameExtStr)
 				{
 					methodName = methodNameExtStr.Value;
@@ -236,7 +237,7 @@ namespace Beamable.Server.Generator
 							itemValue => itemValue.Value.GetEffective(document));
 					}
 					
-					AddCallableMethod(targetClass, methodName, parameters, responseType, addedParameters);
+					AddCallableMethod(targetClass, methodName, pathName, parameters, responseType, addedParameters);
 				}
 			}
 
@@ -314,7 +315,7 @@ namespace Beamable.Server.Generator
 			parameterClass.Members.Add(wrapper);
 		}
 
-		private void AddCallableMethod(CodeTypeDeclaration targetClass, string methodName,
+		private void AddCallableMethod(CodeTypeDeclaration targetClass, string methodName, string servicePath,
 			IDictionary<string, OpenApiSchema> parameters, OpenApiMediaType returnMediaType,
 			Dictionary<string, string> paramsTypeName)
 		{
@@ -374,8 +375,6 @@ namespace Beamable.Server.Generator
 
 			// Declaring a return statement for method ToString.
 			var returnStatement = new CodeMethodReturnStatement();
-
-			string servicePath = methodName;
 
 			// servicePath = $"micro_{Descriptor.Name}/{servicePath}"; // micro is the feature name, so we don't accidentally stop out an existing service.
 
