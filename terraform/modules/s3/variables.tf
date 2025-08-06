9e36ca0a6785e78f5d940a7936a147bdcafd84cc
Chris Hanna <chris@beamable.com>
2025-08-06 10:52:47 -0400

    fix websdkdocs_price_class

--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -47,7 +47,7 @@ variable "websdkdocs_price_class" {
   type        = string
   description = "CloudFront price class: PriceClass_Any (global), PriceClass_200 (most regions), or PriceClass_100 (US, Canada, Europe)"
   validation {
-    condition     = contains(["PriceClass_All", "PriceClass_200", "PriceClass_100"], var.collector_price_class)
+    condition     = contains(["PriceClass_All", "PriceClass_200", "PriceClass_100"], var.websdkdocs_price_class)
     error_message = "Price class must be one of [PriceClass_All, PriceClass_200, PriceClass_100]."
   }
   default = "PriceClass_100"
