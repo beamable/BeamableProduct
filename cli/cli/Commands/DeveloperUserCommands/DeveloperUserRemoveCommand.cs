56d21624fc7da2924c040a953515aedb7fc1371e
felipe-beamable <felipe@beamable.com>
2025-08-19 14:42:57 -0300

    Change - Added a batch remove for the developer user to clean up a specific type of developer user

--- cli/cli/Commands/DeveloperUserCommands/DeveloperUserRemoveCommand.cs
+++ cli/cli/Commands/DeveloperUserCommands/DeveloperUserRemoveCommand.cs
@@ -5,34 +5,42 @@ namespace cli.DeveloperUserCommands;
 
 public class DeveloperUserRemoveCommand : AtomicCommand<DeveloperUserRemoveArgs, DeveloperUserResult>, ISkipManifest
 {
-	public DeveloperUserRemoveCommand() : base("remove-user", "Remove a user from the local files, it will not remove it from the portal")
+	public DeveloperUserRemoveCommand() : base("remove-user", "Remove a user or a list from the local files, it will not remove it from the portal")
 	{
 	}
 
 	public override void Configure()
 	{
-		AddOption(new ConfigurableOption("gamer-tag", "The gamer tag of the player that you would like to remove, it will not remove from the portal"), (args, s) => { args.GamerTag = s; });
+		AddOption(new ConfigurableOptionList("gamer-tag", "The gamer tag of the player that you would like to remove, it will not remove from the portal"), (args, enumerable) =>
+		{
+			args.GamerTags = new List<string>();
+			foreach (string gamerTag in enumerable)
+			{
+				args.GamerTags.Add(gamerTag);
+			}
+		});
 	}
 	
 
 	public override Task<DeveloperUserResult> GetResult(DeveloperUserRemoveArgs args)
 	{
-		args.DeveloperUserManagerService.DeleteUser(args.GamerTag);
+		args.DeveloperUserManagerService.DeleteUsers(args.GamerTags);
+
+		var deletedUsers = new List<DeveloperUserData>();
+
+		foreach (string gamerTag in args.GamerTags)
+		{
+			deletedUsers.Add(new DeveloperUserData() { GamerTag = long.Parse(gamerTag) });
+		}
 		
 		return Task.FromResult(new DeveloperUserResult
 		{
-			DeletedUsers = new List<DeveloperUserData>()
-			{
-				new DeveloperUserData()
-				{
-					GamerTag = long.Parse(args.GamerTag)
-				}
-			}
+			DeletedUsers = deletedUsers
 		});
 	}
 }
 
 public class DeveloperUserRemoveArgs : ContentCommandArgs
 {
-	public string GamerTag;
+	public List<string> GamerTags;
 }
