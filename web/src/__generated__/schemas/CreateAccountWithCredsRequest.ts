85fbedfe18157112362a1686e5ab0caecd9c0cba
Mohammed Agboola <moh.agboola@gmail.com>
2025-08-21 21:04:26 +0100

    autogen schema and api updates

---
+++ web/src/__generated__/schemas/CreateAccountWithCredsRequest.ts
@@ -0,0 +1,16 @@
+/**
+ * ⚠️ THIS FILE IS AUTO-GENERATED. DO NOT EDIT MANUALLY.
+ * All manual edits will be lost when this file is regenerated.
+ */
+
+import type { ChallengeSolution } from './ChallengeSolution';
+
+export type CreateAccountWithCredsRequest = { 
+  challenge_solution?: ChallengeSolution; 
+  external_token?: string; 
+  initProperties?: Record<string, string>; 
+  password?: string; 
+  provider_namespace?: string; 
+  provider_service?: string; 
+  username?: string; 
+};
