cff82b0eecd3eca550df8a80c73bbdd63894fe12
Chris Hanna <chris@beamable.com>
2025-09-11 14:51:14 -0400

    remove comment

--- cli/beamable.templates/templates/BeamService/Program.cs
+++ cli/beamable.templates/templates/BeamService/Program.cs
@@ -10,9 +10,6 @@ namespace Beamable.BeamService
 		/// </summary>
 		public static async Task Main()
 		{
-			//  Note: Any code you run after this method will run on every autoscaled
-			//        instance of the microservice running in the Beamable cloud.
-			
 			// inject data from the CLI.
 			await MicroserviceBootstrapper.Prepare<BeamService>();
 			
