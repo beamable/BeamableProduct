6f6484d72658219022abc90835dfe851f6062e8f
Gabriel Dias <gmoraes@beamable.com>
2025-10-08 16:48:57 -0300

    fix: Fixed an issue where button to create a new snapshot was not displayed when no snapshots existed.

--- client/Packages/com.beamable/Editor/UI/ContentWindow/ContentWindow_SnapshotManager.cs
+++ client/Packages/com.beamable/Editor/UI/ContentWindow/ContentWindow_SnapshotManager.cs
@@ -104,8 +104,10 @@ namespace Beamable.Editor.UI.ContentWindow
 		private void DrawSnapshots()
 		{
 			BeamManifestSnapshotItem manifestWithBiggerName = _allSnapshots.Values.Where(FilterSnapshot).OrderByDescending(item => item.Name.Length).FirstOrDefault();
-			float biggerNameSize = EditorStyles.label.CalcSize(new GUIContent(manifestWithBiggerName?.Name ?? string.Empty)).x;
 			float snapshotListAreaWidth = _snapshotSplitter.cellNormalizedSizes[0] * EditorGUIUtility.currentViewWidth;
+			float biggerNameSize = manifestWithBiggerName != null
+				? EditorStyles.label.CalcSize(new GUIContent(manifestWithBiggerName.Name)).x
+				: snapshotListAreaWidth;
 			List<GUILayoutOption> scrollOptions = new List<GUILayoutOption>() {GUILayout.ExpandWidth(true)};
 			if (biggerNameSize > snapshotListAreaWidth)
 			{
