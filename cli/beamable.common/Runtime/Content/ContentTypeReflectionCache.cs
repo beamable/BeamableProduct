128fa0fada5bd44025facac1f69f2d28722af769
Piotr Siuszko <siuszko@zoho.com>
2025-11-21 11:47:37 +0100

    using cleanup

--- cli/beamable.common/Runtime/Content/ContentTypeReflectionCache.cs
+++ cli/beamable.common/Runtime/Content/ContentTypeReflectionCache.cs
@@ -1,10 +1,8 @@
 using Beamable.Common.Reflection;
 using Beamable.Content;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
