203f5d8843bf4eef1e2d5b7a33a7e4f60ccd4f43
MevLyshkin <piotr@beamable.com>
2025-12-04 16:56:32 +0100

    Content force UTF8 no bom (#4426)
    * Content deserialization exception- add extra log
    
    * Setup content files to save in UTF8 format without BOM
    
    ---------
    
    Co-authored-by: Gabriel Moraes <gmoraes@beamable.com>

--- cli/cli/Utils/DirectoryUtils.cs
+++ cli/cli/Utils/DirectoryUtils.cs
@@ -1,4 +1,5 @@
 using Beamable.Server;
+using System.Text;
 
 namespace cli.Utils;
 
@@ -64,5 +65,31 @@ public static class DirectoryUtils
 			len = len / 1024;
 		}
 		return $"{len:0.##} {sizes[order]}";
+	}
+
+	private static readonly UTF8Encoding UTF8NoBom = new UTF8Encoding(false);
+	
+	/// <summary>
+	/// Writes a UTF8 file without a BOM asynchronously.
+	/// </summary>
+	/// <remarks>It can be useful, especially in cases when the file would be created on Windows and accessed on other OS.
+	/// On Windows by default, the file can have a BOM, which will cause problems when reading it on Linux or macOS.
+	/// </remarks>
+	/// <param name="path"></param>
+	/// <param name="content"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns>Task to await</returns>
+	public static Task WriteUtf8FileAsync(string path, string content, CancellationToken cancellationToken = default)
+	{
+		return File.WriteAllTextAsync(path, content, UTF8NoBom, cancellationToken);
+	}
+	/// <summary>
+	/// Writes a UTF8 file without a BOM.
+	/// </summary>
+	/// <param name="path"></param>
+	/// <param name="content"></param>
+	public static void WriteUtf8File(string path, string content)
+	{
+		File.WriteAllText(path, content, UTF8NoBom);
 	}
 }
