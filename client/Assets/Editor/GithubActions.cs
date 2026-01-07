5259d69373a44aff06de7d6bc254f81c00517cf3
Chris Hanna <chris@beamable.com>
2026-01-07 09:16:15 -0500

    remove undeeded build method

--- client/Assets/Editor/GithubActions.cs
+++ client/Assets/Editor/GithubActions.cs
@@ -7,42 +7,6 @@ using UnityEngine;
 
 public static class GithubActions
 {
-	public static string Get(string name)
-    {
-        var args = Environment.GetCommandLineArgs();
-        var index = Array.IndexOf(args, name);
-        return index >= 0 && index < args.Length - 1
-            ? args[index + 1]
-            : null;
-    }
-
-#if UNITY_6000_3_OR_NEWER
-	[MenuItem("Build/BundleSignedPackage")]
-	public static void BundlePackage()
-	{
-		var BeamableOrgId = Environment.GetEnvironmentVariable("BEAM_ORG_ID");
-		if (string.IsNullOrEmpty(BeamableOrgId)){
-			BeamableOrgId = Get("-beamableOrgId");
-		}
-		Debug.Log($"About to pack. org=[{BeamableOrgId}]");
-		var packRequest = Client.Pack("Packages/com.beamable", "build/package_dist", BeamableOrgId);
-		Debug.Log("Packing...");
-		while (packRequest.Status == StatusCode.InProgress)
-		{
-			Thread.Sleep(100);
-		}
-
-		if (packRequest.Status == StatusCode.Success)
-		{
-			Debug.Log("packed tarball: " + packRequest.Result.tarballPath);
-			return;
-		}
-		else
-		{
-			Debug.LogError($"failed to pack. code=[{packRequest.Error.errorCode}] message=[{packRequest.Error.message}]");
-		}
-	}
-#endif
 	public static void OnBuild()
 	{
 		Debug.Log("ON BUILD TEST XYZ");
