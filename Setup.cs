9ee8d14394aee1f75396aac7f3da0da0c1c4e209
Piotr Siuszko <siuszko@zoho.com>
2026-01-16 19:05:18 +0100

    Update Setup.cs

--- Setup.cs
+++ Setup.cs
@@ -353,7 +353,7 @@ static (int Previous, int Next) GetNextBuildNumber()
     if (!File.Exists(buildNumberPath))
     {
         AnsiConsole.MarkupLine("[bold][red]Error[/][/] No [bold]build-number.txt[/] file found.");
-        AnsiConsole.MarkupLine("Call [bold]./Setup.cs setup[/] first.");
+        AnsiConsole.MarkupLine("Call [bold]dotnet run Setup.cs setup[/] first.");
         Environment.Exit(1);
     }
     int current = int.Parse(File.ReadAllText(buildNumberPath).Trim());
@@ -587,6 +587,32 @@ static async Task<(bool Success, string GoPath)> InstallGo()
         catch
         {
             AnsiConsole.Write($"Failed to install GO using Chocolatey!");
+        }
+    }
+    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    {
+        try
+        {
+            await RunProcessAsync("apt-get", "--version");
+            AnsiConsole.WriteLine("apt-get is available");
+        }
+        catch
+        {
+            AnsiConsole.WriteLine("apt-get is not available, trying with sudo...");
+        }
+
+        try
+        {
+            await RunProcessAsync("wget", $"https://go.dev/dl/go{GO_VERSION}.linux-amd64.tar.gz");
+            AnsiConsole.WriteLine($"GO with version {GO_VERSION} was successfully downloaded!");
+            await RunProcessAsync("sudo", $"rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go{GO_VERSION}.linux-amd64.tar.gz");
+            await RunProcessAsync("rm", $"go{GO_VERSION}.linux-amd64.tar.gz");
+            AnsiConsole.WriteLine($"GO with version {GO_VERSION} was successfully installed!");
+            return (true, "/usr/local/go/bin/go");
+        }
+        catch
+        {
+            AnsiConsole.WriteLine($"Failed to install GO using wget!");
         }
     }
     else
