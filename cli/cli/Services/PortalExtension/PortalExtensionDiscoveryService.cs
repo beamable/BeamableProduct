b0d2f53049b3a7376a52b707c64f694761637a12
Chris Hanna <chris@beamable.com>
2026-03-25 13:47:56 -0400

    merge issue

--- cli/cli/Services/PortalExtension/PortalExtensionDiscoveryService.cs
+++ cli/cli/Services/PortalExtension/PortalExtensionDiscoveryService.cs
@@ -136,8 +136,8 @@ public class PortalExtensionObserver
 	{
 		var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 		StartProcessResult result = isWindows
-			? StartProcessUtil.Run("cmd.exe", "/c npm run beam-build", workingDirectoryPath: _appPath)
-			: StartProcessUtil.Run("npm", "run beam-build", workingDirectoryPath: _appPath);
+			? StartProcessUtil.Run("cmd.exe", "/c npm run beam-build", workingDirectoryPath: AppFilesPath)
+			: StartProcessUtil.Run("npm", "run beam-build", workingDirectoryPath: AppFilesPath);
 		if (result.exit != 0)
 		{
 			Log.Error($"Failed to generate portal extension build. \nCheck errors: \n{result.stderr} \nAll logs: {result.stdout}"
