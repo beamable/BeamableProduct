c3e3afd4a436b83e8c0d776fc29b9c4dcf4f5b5a
Justin <jstnlefebvre@gmail.com>
2026-04-10 14:10:19 -0400

    fix up some issues with the web engine codegen

--- web/src/__generated__/schemas/AuthV2JsonWebKey.ts
+++ web/src/__generated__/schemas/AuthV2JsonWebKey.ts
@@ -23,7 +23,7 @@ export type AuthV2JsonWebKey = {
   x?: string | null; 
   x5c?: string[] | null; 
   x5t?: string | null; 
-  x5t#S256?: string | null; 
+  "x5t#S256"?: string | null; 
   x5u?: string | null; 
   y?: string | null; 
 };
